d3c35b621e6d770188f8755e2d55aa8083b59e87
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-01 20:18:31 -0800

    1938

---
+++ 1938/1938.ts
@@ -0,0 +1,4 @@
+function maximumGeneticDifferenceQuery(root: TreeNode | null): number {
+    if (!root) return 0;
+    return 1 + Math.max(maximumGeneticDifferenceQuery(root.left), maximumGeneticDifferenceQuery(root.right));
+}
