5cb1c3df956410271d8dca94d5825825e5ec1614
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-03 09:19:33 -0800

    1443

---
+++ 1443/1443.go
@@ -0,0 +1,11 @@
+func minimumTimeToCollectAllApplesI(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := minimumTimeToCollectAllApplesI(root.Left)
+	r := minimumTimeToCollectAllApplesI(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
