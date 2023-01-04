1c4ab9807a6330a3787fb950908fe018c5a3617d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-03 19:52:48 -0800

    405

---
+++ 0405/405.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convert_a_number_to_hexadecimal(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
