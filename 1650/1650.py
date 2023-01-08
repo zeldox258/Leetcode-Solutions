4d4e9c2f521d73ab0e2803f3b26c00264cef5e58
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-08 09:26:04 -0800

    1650

---
+++ 1650/1650.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lowest_common_ancestor_of_a_binary_tree_(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
