bccf914d328ea7d5a9bc5bc26a73d5d763714efe
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-10 11:32:06 -0800

    412

---
+++ 0412/412.py
@@ -0,0 +1,6 @@
+class Solution:
+    def fizz_buzz(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
