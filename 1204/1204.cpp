f54c63f2148c297c76c92cda428ecd3b96e75a50
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-09 19:41:58 -0800

    1204

---
+++ 1204/1204.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lastPersonToFitInTheBus(vector<int>& nums) {
+        int cur=nums[0],mx=nums[0];
+        for(int i=1;i<nums.size();i++){
+            cur=max(nums[i],cur+nums[i]);
+            mx=max(mx,cur);
+        }
+        return mx;
+    }
+};
