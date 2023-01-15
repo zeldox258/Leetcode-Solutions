8bd99bce33480dd70d30f0d9fdba046ffd7b1bbb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-15 13:14:40 -0800

    1657

---
+++ 1657/1657.py
@@ -0,0 +1,6 @@
+class Solution:
+    def determine_if_two_strings_are_close(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
