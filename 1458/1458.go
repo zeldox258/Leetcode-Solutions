f789788bca7c00e615e519914e51da6931f0f92a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-18 10:40:57 -0800

    1458

---
+++ 1458/1458.go
@@ -0,0 +1,11 @@
+func maxDotProductOfTwoSubsequences(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := maxDotProductOfTwoSubsequences(root.Left)
+	r := maxDotProductOfTwoSubsequences(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
