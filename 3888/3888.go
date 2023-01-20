9b455995a64d983539711c32efeac7c3ba5b9cf1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-20 08:28:50 -0800

    3888

---
+++ 3888/3888.go
@@ -0,0 +1,11 @@
+func minimumOperationsToMakeAllGrid(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := minimumOperationsToMakeAllGrid(root.Left)
+	r := minimumOperationsToMakeAllGrid(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
