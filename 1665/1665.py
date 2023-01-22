ef34110cd505f0c1e6fd943ba7e9c86683993d1f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-22 15:26:26 -0800

    1665

---
+++ 1665/1665.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_initial_energy_to_finish_tasks(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
