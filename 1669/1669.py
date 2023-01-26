2347e290987567e460062d4470e868b0dfa28cd6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-26 11:54:19 -0800

    1669

---
+++ 1669/1669.py
@@ -0,0 +1,7 @@
+class Solution:
+    def merge_in_between_linked_lists(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
