2ed5c6953e64c4f69e1ed41abf7eebf023516add
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-29 10:57:55 -0800

    431

---
+++ 0431/431.py
@@ -0,0 +1,8 @@
+class Solution:
+    def encode_nary_tree_to_binary_tree(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
