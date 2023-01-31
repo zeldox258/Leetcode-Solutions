f6d8acf3be077d7034938457b959ad81e5bfb122
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-01-31 08:40:31 -0800

    2687

---
+++ 2687/2687.go
@@ -0,0 +1,14 @@
+func bikesLastTimeUsed(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return m
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+	return -1
+}
