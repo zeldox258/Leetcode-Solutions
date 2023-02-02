5bce48f21b39a7fb2fc99f3aea4caae708816f72
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-02 12:41:52 -0800

    435

---
+++ 0435/435.py
@@ -0,0 +1,8 @@
+class Solution:
+    def nonoverlapping_intervals(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
