ec504c26546a3560c7ba4d513ff27ccada528047
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-02 12:00:00 -0800

    1287

---
+++ 1287/1287.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n;i++) if(arr[i]==arr[i+n/4]) return arr[i];
+        return -1;
+    }
+}
