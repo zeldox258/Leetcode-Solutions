80504e644e2dfc62b33c26f53a6da7c8ef175e29
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-01 22:47:17 -0800

    3900

---
+++ 3900/3900.go
@@ -0,0 +1,10 @@
+func longestBalancedSubstringAfterO(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, x := range nums {
+		if j, ok := m[target-x]; ok {
+			return []int{j, i}
+		}
+		m[x] = i
+	}
+	return nil
+}
