311fbe0b00d99b2d5369308bda1ac47a0a8280b0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-03 21:22:26 -0800

    2910

---
+++ 2910/2910.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_number_of_groups_to_create_a_val(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
