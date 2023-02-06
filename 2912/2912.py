30f8dba401124a0839c7c38ab79a1f3bf8f2c1e3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-05 22:23:47 -0800

    2912

---
+++ 2912/2912.py
@@ -0,0 +1,12 @@
+class Solution:
+    def number_of_ways_to_reach_destination_in_t(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
