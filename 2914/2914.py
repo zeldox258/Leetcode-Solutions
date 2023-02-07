5de72e6d9490ebd29bf1c7edfdb9a31ce1145516
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-07 08:59:55 -0800

    2914

---
+++ 2914/2914.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_number_of_changes_to_make_binary(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
