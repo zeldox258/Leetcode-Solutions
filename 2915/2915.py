08d8f8b55d68347f1b1d59b4d414fb85b5ba755a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-08 10:27:42 -0800

    2915

---
+++ 2915/2915.py
@@ -0,0 +1,10 @@
+class Solution:
+    def length_of_the_longest_subsequence_that_s(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
