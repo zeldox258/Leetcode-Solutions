7b3b361ff26696aea87e10fec3d7abc428f61dc7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-09 22:10:52 -0800

    442

---
+++ 0442/442.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_all_duplicates_in_an_array(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
