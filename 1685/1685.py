909e3547f7e76a847d2aad81b522324f7198383a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-10 19:35:00 -0800

    1685

---
+++ 1685/1685.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sum_of_absolute_differences_in_a_sorted_(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
