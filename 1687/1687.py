bf366062302c2df458a434c41f3958a8c646c42b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-12 22:05:26 -0800

    1687

---
+++ 1687/1687.py
@@ -0,0 +1,6 @@
+class Solution:
+    def delivering_boxes_from_storage_to_ports(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
