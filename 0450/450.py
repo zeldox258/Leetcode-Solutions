046260db0301de7551f19ae61602998c59806ffb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-17 12:22:20 -0800

    450

---
+++ 0450/450.py
@@ -0,0 +1,8 @@
+class Solution:
+    def delete_node_in_a_bst(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
