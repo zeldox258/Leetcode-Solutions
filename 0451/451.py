7e1bc6d79a45a7aab11619e1355bb9bb098bdf4d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-18 09:20:19 -0800

    451

---
+++ 0451/451.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sort_characters_by_frequency(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
