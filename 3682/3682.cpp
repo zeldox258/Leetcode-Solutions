e66eb995d21e6975ba124105816ac518e2c17e44
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-20 22:21:22 -0800

    3682

---
+++ 3682/3682.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* minimumIndexSumOfCommonElement(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* cur=head;
+        while(cur){
+            ListNode* next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+};
