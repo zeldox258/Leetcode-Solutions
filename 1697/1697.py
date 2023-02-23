25d0b0b50f10c930bb47b93e5c55ff9209f011ca
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-22 21:05:36 -0800

    1697

---
+++ 1697/1697.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checking_existence_of_edge_length_limite(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
