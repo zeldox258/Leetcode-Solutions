71d8184485271cd16ff3b293fa4497a59f4fa237
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-23 21:09:03 -0800

    456

---
+++ 0456/456.py
@@ -0,0 +1,11 @@
+class Solution:
+    def 132_pattern(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
