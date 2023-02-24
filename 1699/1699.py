239c60c0345f2cb8a75ece2344b5405c09ded7ab
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-24 12:40:30 -0800

    1699

---
+++ 1699/1699.py
@@ -0,0 +1,7 @@
+class Solution:
+    def number_of_calls_between_two_persons(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
