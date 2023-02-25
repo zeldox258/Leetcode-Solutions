b0cf52d650a4200ace0c53d9a9b780ae24cac8ee
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-25 09:56:20 -0800

    2932

---
+++ 2932/2932.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_strong_pair_xor_i(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
