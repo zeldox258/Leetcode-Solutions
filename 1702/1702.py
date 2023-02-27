42da2c1432ea7a3079d01be921508dfc102a651a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-02-27 09:24:44 -0800

    1702

---
+++ 1702/1702.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_binary_string_after_change(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
