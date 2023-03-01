06ac0990481b4de44d210caba7160b9875e51359
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-01 15:27:21 -0800

    462

---
+++ 0462/462.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimum_moves_to_equal_array_elements_ii(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
