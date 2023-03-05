d7fa9558944144770d658818eae601e66ba33adb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-05 12:50:13 -0800

    2940

---
+++ 2940/2940.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_building_where_alice_and_bob_can_me(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
