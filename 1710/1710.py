a7c6fb60cf452d78182de913b1a929dd2c9b0772
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-07 10:24:35 -0800

    1710

---
+++ 1710/1710.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_units_on_a_truck(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
