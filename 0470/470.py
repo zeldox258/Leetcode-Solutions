f0d8db94fe905985854db086d28d1da42c2d9e58
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-09 10:43:47 -0800

    470

---
+++ 0470/470.py
@@ -0,0 +1,10 @@
+class Solution:
+    def implement_rand10_using_rand7(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
