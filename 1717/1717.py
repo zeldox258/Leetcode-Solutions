0461cd5a67faa626af5cc888d86e6988c5e25c81
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-14 09:09:44 -0700

    1717

---
+++ 1717/1717.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_score_from_removing_substrings(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
