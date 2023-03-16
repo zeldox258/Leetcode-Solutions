b85385035926616ff087aaadae96fc07368134cb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-16 15:47:23 -0700

    3647

---
+++ 3647/3647.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solve3647(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int x:nums){
+            pq.add(x);
+            if(pq.size()>k) pq.poll();
+        }
+        return pq.peek();
+    }
+}
---
+++ 3647/README.md
@@ -0,0 +1,5 @@
+# 3647. Maximum Weight in Two Bags
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/maximum-weight-in-two-bags/)
