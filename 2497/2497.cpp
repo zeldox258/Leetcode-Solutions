c950341806ca028609a14f2e045835a894385056
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-21 12:24:09 -0700

    2497

---
+++ 2497/2497.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* maximumStarSumOfAGraph(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* cur=head;
+        while(cur){
+            ListNode* next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+};
