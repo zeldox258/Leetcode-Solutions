43d5b8cefd0246b4ddbb42eb094659876c83f9a0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-22 14:50:23 -0700

    1725

---
+++ 1725/1725.py
@@ -0,0 +1,8 @@
+class Solution:
+    def number_of_rectangles_that_can_form_the_l(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
