47e8b825ec8af320e698d40ae8a623e4a7431c7a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-24 10:28:23 -0700

    2959

---
+++ 2959/2959.py
@@ -0,0 +1,7 @@
+class Solution:
+    def number_of_possible_sets_of_closing_branc(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
