2e120d640bebb2a30ad13083887a2fa70d58f405
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-27 12:41:07 -0700

    2962

---
+++ 2962/2962.py
@@ -0,0 +1,6 @@
+class Solution:
+    def count_subarrays_where_max_element_appear(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
