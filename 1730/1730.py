54bd84a0ef73e8ad8c02302973a879d582bad672
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-27 21:57:00 -0700

    1730

---
+++ 1730/1730.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shortest_path_to_get_food(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
