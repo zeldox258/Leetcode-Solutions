21c6d37e178e0656beb8d9d6b9d20ee2f8a288b3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-30 14:19:39 -0700

    1940

---
+++ 1940/1940.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool LongestCommonSubsequenceBetwee(string s) {
+        var st = new Stack<char>();
+        foreach (char c in s) {
+            if (c == '(' || c == '[' || c == '{') st.Push(c);
+            else {
+                if (st.Count == 0) return false;
+                char p = st.Pop();
+                if (c == ')' && p != '(' || c == ']' && p != '[' || c == '}' && p != '{') return false;
+            }
+        }
+        return st.Count == 0;
+    }
+}
