2128c01c56a6566fde3ddedaa96a549d425d1aab
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-03-30 12:45:14 -0700

    2965

---
+++ 2965/2965.py
@@ -0,0 +1,15 @@
+class Solution:
+    def find_missing_and_repeated_values(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
