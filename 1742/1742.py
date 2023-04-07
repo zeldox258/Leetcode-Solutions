8fdc7cf21109958bdd1791f85776c975a7fa4340
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-07 16:21:29 -0700

    1742

---
+++ 1742/1742.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum_number_of_balls_in_a_box(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
