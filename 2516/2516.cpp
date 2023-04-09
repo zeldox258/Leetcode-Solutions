59c7df50de3af0b38da7ae8dd446c2898a4c82ef
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-09 10:33:47 -0700

    2516

---
+++ 2516/2516.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> takeKOfEachCharacterFromLeftAn(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> res(n,-1);
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&nums[st.top()]<nums[i]){res[st.top()]=nums[i];st.pop();}
+            st.push(i);
+        }
+        return res;
+    }
+};
