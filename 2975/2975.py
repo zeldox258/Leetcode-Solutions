4b7d86967798e580d8f1ebb7373857fc3734f0b8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-09 13:18:34 -0700

    2975

---
+++ 2975/2975.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximum_square_area_by_removing_fences_f(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
