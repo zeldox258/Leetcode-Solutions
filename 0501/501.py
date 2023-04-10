4b3bee89342f04f4cff063fa49140524d38b95dc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-09 18:35:40 -0700

    501

---
+++ 0501/501.py
@@ -0,0 +1,11 @@
+class Solution:
+    def find_mode_in_binary_search_tree(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
