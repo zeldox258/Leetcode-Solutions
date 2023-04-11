d06d5c05766a39b1e1916c15bc08561de26ce0e7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-11 14:07:26 -0700

    2977

---
+++ 2977/2977.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_cost_to_convert_string_ii(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
