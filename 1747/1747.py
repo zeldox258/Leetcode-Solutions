2ec0eaf377691ed76c6cbf87a0c32758febd0d5a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-12 10:53:26 -0700

    1747

---
+++ 1747/1747.py
@@ -0,0 +1,6 @@
+class Solution:
+    def leetflex_banned_accounts(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
