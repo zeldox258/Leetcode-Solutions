ded9d4d50d812734a1df7e3e97c765c1deac1719
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-25 18:12:51 -0700

    517

---
+++ 0517/517.py
@@ -0,0 +1,6 @@
+class Solution:
+    def super_washing_machines(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
