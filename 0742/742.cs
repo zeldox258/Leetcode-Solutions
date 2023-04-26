aa1f249855da51c701900eb867897f5d5bd5e49f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-25 18:46:08 -0700

    742

---
+++ 0742/742.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode ClosestLeafInABinaryTree(ListNode head) {
+        ListNode prev = null, cur = head;
+        while (cur != null) {
+            var next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
