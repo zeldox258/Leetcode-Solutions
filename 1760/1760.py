6c0e4682284fa016724cd5c33de6a394398da3cd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-04-25 18:21:22 -0700

    1760

---
+++ 1760/1760.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimum_limit_of_balls_in_a_bag(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
