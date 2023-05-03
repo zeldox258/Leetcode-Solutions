467cd5b2d13dba2e0cfeaed678c0545a873db7a8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-03 08:29:09 -0700

    525

---
+++ 0525/525.py
@@ -0,0 +1,8 @@
+class Solution:
+    def contiguous_array(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
