d12b98aa2beaf3340a38ba03edd596c87a184e3f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-04 17:05:56 -0700

    3000

---
+++ 3000/3000.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_area_of_longest_diagonal_rectang(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
