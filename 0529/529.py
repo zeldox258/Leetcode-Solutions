e15a60ea5accca983ee00dc03a71f29b1ef0b856
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-07 19:34:46 -0700

    529

---
+++ 0529/529.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minesweeper(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
