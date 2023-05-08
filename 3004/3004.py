a396a963ded2d5366e1ab45f178248bbb525fa0b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-08 10:02:17 -0700

    3004

---
+++ 3004/3004.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum_subtree_of_the_same_color(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
