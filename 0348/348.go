9e454ad97c1b7c07fb6213f329c1dca8c21a71d4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-09 17:09:50 -0700

    348

---
+++ 0348/348.go
@@ -0,0 +1,11 @@
+func designTictactoe(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := designTictactoe(root.Left)
+	r := designTictactoe(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
