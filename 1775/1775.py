27d71b5d6e9f97639faeaedac3b39acd91865ba3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-10 12:05:23 -0700

    1775

---
+++ 1775/1775.py
@@ -0,0 +1,10 @@
+class Solution:
+    def equal_sum_arrays_with_minimum_number_of_(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
