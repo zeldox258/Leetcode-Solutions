4454d85b33ead9848dc588030d55640d6688040a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-10 16:37:32 -0700

    532

---
+++ 0532/532.py
@@ -0,0 +1,6 @@
+class Solution:
+    def kdiff_pairs_in_an_array(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
