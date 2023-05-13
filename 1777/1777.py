772070fd49d89b1e27f8f91cf8a019f25c64816c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-12 22:50:03 -0700

    1777

---
+++ 1777/1777.py
@@ -0,0 +1,6 @@
+class Solution:
+    def products_price_for_each_store(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
