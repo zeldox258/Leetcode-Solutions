76c1b7989588f78de8a7937f1dbf6dc0a81603f0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-18 20:15:17 -0700

    540

---
+++ 0540/540.py
@@ -0,0 +1,8 @@
+class Solution:
+    def single_element_in_a_sorted_array(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
