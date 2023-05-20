3fb019d46dc236a038bc4545dd8247a7b2b4a531
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-19 18:15:16 -0700

    3015

---
+++ 3015/3015.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_the_number_of_houses_at_a_certain_(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
