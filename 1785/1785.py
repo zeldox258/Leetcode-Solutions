f1fc43ebf10633edcba304dc7f9d1046dac9c9f9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-20 13:22:13 -0700

    1785

---
+++ 1785/1785.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_elements_to_add_to_form_a_given_(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
