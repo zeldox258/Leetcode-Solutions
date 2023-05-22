eb1d0450d5ff7e768dd7539f048f0245f2622abc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-22 13:37:09 -0700

    1787

---
+++ 1787/1787.py
@@ -0,0 +1,12 @@
+class Solution:
+    def make_the_xor_of_all_segments_equal_to_ze(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
