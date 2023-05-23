bd78171772ad7f3c7a5f6e1025567d86e1ad0d8d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-23 12:07:58 -0700

    3019

---
+++ 3019/3019.py
@@ -0,0 +1,7 @@
+class Solution:
+    def number_of_changing_keys(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
