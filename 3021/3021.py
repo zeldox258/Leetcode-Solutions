bc5e450574b8febc041a5a5b5cd10af70627058e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-25 10:04:50 -0700

    3021

---
+++ 3021/3021.py
@@ -0,0 +1,11 @@
+class Solution:
+    def alice_and_bob_playing_flower_game(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
