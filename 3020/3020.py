6f669503e2a04ba02671d3a9a6aae5bea0c6a395
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-24 20:53:03 -0700

    3020

---
+++ 3020/3020.py
@@ -0,0 +1,10 @@
+class Solution:
+    def find_the_maximum_number_of_elements_in_s(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
