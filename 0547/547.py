886578d03a0f68fe980f339bf4f55f990ac5d264
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-25 10:03:49 -0700

    547

---
+++ 0547/547.py
@@ -0,0 +1,6 @@
+class Solution:
+    def number_of_provinces(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
