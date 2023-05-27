5637f210207891c43ece4865ef4da8a849c3095c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-26 18:23:23 -0700

    3022

---
+++ 3022/3022.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimize_or_of_remaining_elements_using_(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
