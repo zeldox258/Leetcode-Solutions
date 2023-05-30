a1e818fe02f187e392dd8d6b0d5f012039b19dc0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-05-29 17:14:59 -0700

    3025

---
+++ 3025/3025.py
@@ -0,0 +1,15 @@
+class Solution:
+    def find_the_number_of_ways_to_place_people_(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
