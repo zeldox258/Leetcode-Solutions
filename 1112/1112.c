23bc6572c56c4b2f8e3f8e3c777bef68ea14f34c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-03 19:33:10 -0700

    1112

---
+++ 1112/1112.c
@@ -0,0 +1,9 @@
+int highestGradeForEachStudent(int* nums, int numsSize, int target) {
+    int l = 0, r = numsSize - 1;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (nums[m] == target) return m;
+        if (nums[m] < target) l = m + 1; else r = m - 1;
+    }
+    return -1;
+}
