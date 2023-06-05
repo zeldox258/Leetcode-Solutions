d14b927f82238063e21aadbc5ff47bce05c0121b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-04 21:03:03 -0700

    557

---
+++ 0557/557.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse_words_in_a_string_iii(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
