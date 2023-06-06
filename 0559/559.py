a305f401f653743ed48f1a785bfab6dc2c24a242
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-06 11:30:22 -0700

    559

---
+++ 0559/559.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum_depth_of_nary_tree(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
