3555ded718fd018a2ce138ae66fed1b67a9d01c8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-07 22:23:55 -0700

    560

---
+++ 0560/560.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subarray_sum_equals_k(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
