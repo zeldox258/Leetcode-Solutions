9719e1d1886f0e194bbaee389e656ea1d8850256
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-08 16:03:37 -0700

    1805

---
+++ 1805/1805.py
@@ -0,0 +1,10 @@
+class Solution:
+    def number_of_different_integers_in_a_string(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
