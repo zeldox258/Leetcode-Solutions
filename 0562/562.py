18af0d0b313b4d6c1a4a2218b196471e042e5027
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-09 19:01:20 -0700

    562

---
+++ 0562/562.py
@@ -0,0 +1,6 @@
+class Solution:
+    def longest_line_of_consecutive_one_in_matri(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
