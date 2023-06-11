156be773f4b69a8a7284f3898aeac16367f63f5a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-10 18:22:01 -0700

    1807

---
+++ 1807/1807.py
@@ -0,0 +1,6 @@
+class Solution:
+    def evaluate_the_bracket_pairs_of_a_string(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
