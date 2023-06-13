8d06c76c066fd6ad6fcf5d31316a49754cd21fa7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-13 10:02:29 -0700

    566

---
+++ 0566/566.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reshape_the_matrix(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
