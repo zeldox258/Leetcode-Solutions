6c89a7d6f4cb9ba31b1cbec6f94fe240c3e3ebb3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-17 10:11:26 -0700

    570

---
+++ 0570/570.py
@@ -0,0 +1,8 @@
+class Solution:
+    def managers_with_at_least_5_direct_reports(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
