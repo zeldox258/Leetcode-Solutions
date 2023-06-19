461e04200a41c82d22b4d4158d949ac0fd699510
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-18 18:18:03 -0700

    571

---
+++ 0571/571.py
@@ -0,0 +1,10 @@
+class Solution:
+    def find_median_given_frequency_of_numbers(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
