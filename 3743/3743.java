b8d076f48d02c0e0d1c237d596d7ce886c30c629
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-20 09:27:52 -0700

    3743

---
+++ 3743/3743.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solve3743(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int x:nums){
+            pq.add(x);
+            if(pq.size()>k) pq.poll();
+        }
+        return pq.peek();
+    }
+}
---
+++ 3743/README.md
@@ -0,0 +1,5 @@
+# 3743. Maximize Cyclic Partition Score
+
+**Difficulty:** Hard
+
+[LeetCode Link](https://leetcode.com/problems/maximize-cyclic-partition-score/)
