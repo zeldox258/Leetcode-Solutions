19b94e587c9781ee374fa3ebe3174142286cd652
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-22 10:26:25 -0700

    3049

---
+++ 3049/3049.py
@@ -0,0 +1,7 @@
+class Solution:
+    def earliest_second_to_mark_indices_ii(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
