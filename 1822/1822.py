eb9e6d732c2a608ab67beb5399ec7e6f62170272
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-25 22:23:37 -0700

    1822

---
+++ 1822/1822.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sign_of_the_product_of_an_array(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
