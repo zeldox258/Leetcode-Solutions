34792ee1f23f33d7f7018c26ad6ab2724817f107
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-25 21:14:31 -0700

    3052

---
+++ 3052/3052.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximize_items(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
