e154d54af1741ef686e5cb5c2b2859a70c9553e8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-26 20:28:31 -0700

    3477

---
+++ 3477/3477.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var fruitsIntoBasketsIi = function(nums) {
+    const n = nums.length;
+    const dp = new Array(n);
+    dp[0] = nums[0];
+    for (let i = 1; i < n; i++) dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    return Math.max(...dp);
+};
