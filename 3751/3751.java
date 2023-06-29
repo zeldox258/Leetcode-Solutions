49ba9ce6790d527ee572f9147b9ada959c5dafa2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-06-28 21:05:07 -0700

    3751

---
+++ 3751/3751.java
@@ -0,0 +1,11 @@
+class Solution {
+    public ListNode solve3751(ListNode head) {
+        if(head==null||head.next==null) return head;
+        ListNode prev=null,cur=head;
+        while(cur!=null){
+            ListNode next=cur.next;
+            cur.next=prev;prev=cur;cur=next;
+        }
+        return prev;
+    }
+}
---
+++ 3751/README.md
@@ -0,0 +1,5 @@
+# 3751. Total Waviness of Numbers in Range I
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/total-waviness-of-numbers-in-range-i/)
