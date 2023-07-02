ae81035ff5510300772be6993a232fb339626edb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-02 12:45:35 -0700

    585

---
+++ 0585/585.py
@@ -0,0 +1,8 @@
+class Solution:
+    def investments_in_2016(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
