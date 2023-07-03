53d820d2ab438abdec2b1d6266dfdc931c016595
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-03 11:08:53 -0700

    3060

---
+++ 3060/3060.py
@@ -0,0 +1,8 @@
+class Solution:
+    def user_activities_within_time_bounds(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
