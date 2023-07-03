04ac8dfd1a3c0188dca45df245f61d22e8e0f66c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-03 14:08:54 -0700

    1830

---
+++ 1830/1830.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_number_of_operations_to_make_str(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
