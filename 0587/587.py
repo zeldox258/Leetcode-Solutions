84ee235f52c46ffcfabded609c128c0b933b2f8f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-04 13:46:54 -0700

    587

---
+++ 0587/587.py
@@ -0,0 +1,12 @@
+class Solution:
+    def erect_the_fence(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
