73e9047926165fbeda8a07a45899a879461f3450
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-07 16:40:06 -0700

    3247

---
+++ 3247/3247.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode NumberOfSubsequencesWithOddSum(ListNode head) {
+        ListNode prev = null, cur = head;
+        while (cur != null) {
+            var next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
