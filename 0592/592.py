d2d48d81fa33d6b11330cd5e38c8986ac1920676
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-09 16:25:25 -0700

    592

---
+++ 0592/592.py
@@ -0,0 +1,6 @@
+class Solution:
+    def fraction_addition_and_subtraction(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
