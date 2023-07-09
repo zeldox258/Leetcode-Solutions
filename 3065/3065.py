4ed79a905747038421afeb13bd26adf127da0df3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-08 21:46:25 -0700

    3065

---
+++ 3065/3065.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimum_operations_to_exceed_threshold_v(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
