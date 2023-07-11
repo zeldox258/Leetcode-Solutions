fec9aafe761e1044c0ea953e83e3ad53798c9028
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-10 19:07:17 -0700

    1837

---
+++ 1837/1837.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sum_of_digits_in_base_k(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
