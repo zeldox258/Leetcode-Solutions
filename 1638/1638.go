d4ba7be5ed10936277b03419e06a5f12ba80c585
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-15 09:11:05 -0700

    1638

---
+++ 1638/1638.go
@@ -0,0 +1,11 @@
+func countSubstringsThatDifferByOne(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := countSubstringsThatDifferByOne(root.Left)
+	r := countSubstringsThatDifferByOne(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
