19b4c5921460280194c2c93a4c9ddf8640734acf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-17 12:27:50 -0700

    3770

---
+++ 3770/3770.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solve3770(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]<target) l=m+1; else r=m-1;
+        }
+        return -1;
+    }
+}
---
+++ 3770/README.md
@@ -0,0 +1,5 @@
+# 3770. Largest Prime from Consecutive Prime Sum
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/largest-prime-from-consecutive-prime-sum/)
