357388fb3797dc4ddefd8aef52ce75e36dfadcc7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-18 19:20:46 -0700

    3075

---
+++ 3075/3075.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximize_happiness_of_selected_children(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
