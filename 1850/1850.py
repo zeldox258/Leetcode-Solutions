4ad617c91ed3b67e14c29a2730e07c9c91181e87
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-23 09:48:51 -0700

    1850

---
+++ 1850/1850.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimum_adjacent_swaps_to_reach_the_kth_(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
