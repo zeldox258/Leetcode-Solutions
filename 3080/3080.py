a81d2e52ad334e4b58b1185d5124b16d39b930cd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-23 21:49:23 -0700

    3080

---
+++ 3080/3080.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mark_elements_on_array_by_performing_que(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
