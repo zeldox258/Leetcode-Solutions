495fffa2a480c715f3fa2ee13e885d2f1009b064
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-24 18:38:22 -0700

    607

---
+++ 0607/607.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sales_person(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
