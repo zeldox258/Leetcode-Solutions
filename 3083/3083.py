231657b872c776ac5ca6df3fef9dccc261c3815d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-26 08:05:17 -0700

    3083

---
+++ 3083/3083.py
@@ -0,0 +1,9 @@
+class Solution:
+    def existence_of_a_substring_in_a_string_and(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
