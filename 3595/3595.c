d8bda6fcd114d773dbac7b72aa2d7e8ecb4626e6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-07-27 13:44:34 -0700

    3595

---
+++ 3595/3595.c
@@ -0,0 +1,14 @@
+int parent[10001];
+int find(int x) {
+    if (parent[x] != x) parent[x] = find(parent[x]);
+    return parent[x];
+}
+int onceTwice(int n, int** edges, int edgesSize, int* edgesColSize) {
+    for (int i = 0; i < n; i++) parent[i] = i;
+    int comp = n;
+    for (int i = 0; i < edgesSize; i++) {
+        int a = find(edges[i][0]), b = find(edges[i][1]);
+        if (a != b) { parent[a] = b; comp--; }
+    }
+    return comp;
+}
