1b994fd1df031806f237563c52b2a7584e5fba7b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-01 16:40:29 -0700

    615

---
+++ 0615/615.py
@@ -0,0 +1,8 @@
+class Solution:
+    def average_salary_departments_vs_company(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
