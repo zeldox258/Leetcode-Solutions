7d3306463dc44c67c62786c4a6216b7d48ef69a8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-02 13:05:49 -0700

    3090

---
+++ 3090/3090.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_length_substring_with_two_occurr(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
