449eef8185d096802a4274ebb77df833f55d0a6d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-04 12:00:00 -0700

    1015

---
+++ 1015/1015.java
@@ -0,0 +1,6 @@
+
+class Solution {
+    public int smallestRepunitDivByK(int K) {
+        if(K%2==0||K%5==0)return -1; int r=0; for(int i=1;i<=K;i++){r=(r*10+1)%K;if(r==0)return i;} return -1;
+    }
+}
