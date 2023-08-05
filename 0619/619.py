d44e2699bc0ee4b5360e68b15d2188a2fa1b0005
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-05 12:20:47 -0700

    619

---
+++ 0619/619.py
@@ -0,0 +1,7 @@
+class Solution:
+    def biggest_single_number(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
