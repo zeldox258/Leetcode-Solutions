a26be98576d44e5ecca266f6933c68e592c5ce07
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-06 14:15:57 -0700

    1864

---
+++ 1864/1864.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_number_of_swaps_to_make_the_bina(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
