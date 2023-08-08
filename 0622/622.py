925b2d3af38f79795c1651444d3269aaa0d12cbf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-08 15:53:51 -0700

    622

---
+++ 0622/622.py
@@ -0,0 +1,6 @@
+class Solution:
+    def design_circular_queue(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
