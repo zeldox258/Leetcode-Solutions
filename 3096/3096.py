1e3db5ed9fb71a7f147f641289753b96e88126a7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-08 14:54:01 -0700

    3096

---
+++ 3096/3096.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimum_levels_to_gain_more_points(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
