6a81c8000395626c69476b91965788a015589bfd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-08 12:46:17 -0700

    2072

---
+++ 2072/2072.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int TheWinnerUniversity(int[] nums, int target) {
+        int l = 0, r = nums.Length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            if (nums[m] < target) l = m + 1; else r = m - 1;
+        }
+        return -1;
+    }
+}
