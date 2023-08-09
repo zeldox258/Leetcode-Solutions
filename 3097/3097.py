7a63111741b84a02a63581ea74104859483fcf53
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-09 14:55:59 -0700

    3097

---
+++ 3097/3097.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shortest_subarray_with_or_at_least_k_ii(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
