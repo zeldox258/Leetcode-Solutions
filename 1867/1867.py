48e471f091e75c2fe7f19c3dc24d1f7bbaf52bed
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-09 19:34:34 -0700

    1867

---
+++ 1867/1867.py
@@ -0,0 +1,6 @@
+class Solution:
+    def orders_with_maximum_quantity_above_avera(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
