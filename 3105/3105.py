e1d1ff3118a277f03018f6a75db6b83fedfa4be0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-17 12:48:43 -0700

    3105

---
+++ 3105/3105.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longest_strictly_increasing_or_strictly_(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
