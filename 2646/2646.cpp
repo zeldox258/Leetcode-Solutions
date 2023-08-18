1df7cd9ef6beb28c56d50a27668350600a21fb0a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-17 21:02:07 -0700

    2646

---
+++ 2646/2646.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimizeTheTotalPriceOfTheTrip(string s) {
+        unordered_set<char> seen;
+        int l=0,res=0;
+        for(int r=0;r<s.size();r++){
+            while(seen.count(s[r])) seen.erase(s[l++]);
+            seen.insert(s[r]);
+            res=max(res,r-l+1);
+        }
+        return res;
+    }
+};
