35b50b8b967ad204d7316a9adfc03815fbfed8a0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-18 18:35:31 -0700

    632

---
+++ 0632/632.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallest_range_covering_elements_from_k_(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
