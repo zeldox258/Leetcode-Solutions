abb19f4f6fdb5e14a7d979aa53d366bca401328a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-21 19:46:53 -0700

    635

---
+++ 0635/635.py
@@ -0,0 +1,10 @@
+class Solution:
+    def design_log_storage_system(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
