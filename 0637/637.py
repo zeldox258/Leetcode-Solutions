0f0598c56b9b109a886d6d657426110cbdabeb61
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-23 14:46:30 -0700

    637

---
+++ 0637/637.py
@@ -0,0 +1,6 @@
+class Solution:
+    def average_of_levels_in_binary_tree(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
