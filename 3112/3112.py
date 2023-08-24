bb796340c4fd5cd952bd6d5beae436f7064f42c8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-24 16:31:12 -0700

    3112

---
+++ 3112/3112.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_time_to_visit_disappearing_nodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
