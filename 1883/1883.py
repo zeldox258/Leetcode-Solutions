d9666f89ed8955c9c51b8960d55d5c4bb0de01a9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-25 20:20:49 -0700

    1883

---
+++ 1883/1883.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimum_skips_to_arrive_at_meeting_on_ti(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
