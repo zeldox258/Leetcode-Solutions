ba6922e9cc3c09d33f027e8e0b72365dd5c52d31
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-26 15:01:23 -0700

    1464

---
+++ 1464/1464.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solve1464(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);int sum=0,count=0;
+        for(int x:nums){
+            sum+=x;
+            count+=map.getOrDefault(sum-k,0);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
---
+++ 1464/README.md
@@ -0,0 +1,5 @@
+# 1464. Maximum Product of Two Elements in an Array
+
+**Difficulty:** Easy
+
+[LeetCode Link](https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/)
