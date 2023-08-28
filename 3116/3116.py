982ebce5942a60ef794fb7ea300eb1d0b1dda4c5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-28 11:49:13 -0700

    3116

---
+++ 3116/3116.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kth_smallest_amount_with_single_denomina(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
