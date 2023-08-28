f8578e42d470ba2fc7a331060a002126af0f37f3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-28 16:40:03 -0700

    2178

---
+++ 2178/2178.ts
@@ -0,0 +1,4 @@
+function maximumSplitOfPositiveEvenInte(root: TreeNode | null): number {
+    if (!root) return 0;
+    return 1 + Math.max(maximumSplitOfPositiveEvenInte(root.left), maximumSplitOfPositiveEvenInte(root.right));
+}
