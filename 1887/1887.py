ab95b6e93561b9431a6edda1506225e1520e4d2d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-08-29 16:21:43 -0700

    1887

---
+++ 1887/1887.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reduction_operations_to_make_the_array_e(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
