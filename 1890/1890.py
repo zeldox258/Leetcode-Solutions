a82fd23d1935b14350a2ad6c122f9f8e050225c3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-01 21:34:25 -0700

    1890

---
+++ 1890/1890.py
@@ -0,0 +1,8 @@
+class Solution:
+    def the_latest_login_in_2020(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
