a9376d8f9e166083a8975636791968f2c1fb8ab7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-05 21:52:46 -0700

    1894

---
+++ 1894/1894.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_the_student_that_will_replace_the_c(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
