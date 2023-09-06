56f732ac7e61d65065628205b2737058d760afc6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-06 11:12:02 -0700

    3549

---
+++ 3549/3549.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var multiplyTwoPolynomials = function(n) {
+    let count = 0;
+    while (n) {
+        count += n & 1;
+        n >>>= 1;
+    }
+    return count;
+};
