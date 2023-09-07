aa3c2cb516a00e17d9c8f7fe37f7af3492a24741
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-07 08:26:51 -0700

    652

---
+++ 0652/652.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_duplicate_subtrees(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
