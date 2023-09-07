655438382332978f77256cb4ae472b5316d85150
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-07 09:10:48 -0700

    1896

---
+++ 1896/1896.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimum_cost_to_change_the_final_value_o(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
