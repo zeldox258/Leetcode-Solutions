0b2ad27e0f35ac8db434b7bcc45ae700c5994781
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-08 08:55:22 -0700

    1897

---
+++ 1897/1897.py
@@ -0,0 +1,6 @@
+class Solution:
+    def redistribute_characters_to_make_all_stri(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
