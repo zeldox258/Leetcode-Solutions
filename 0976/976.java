0d8d09d592589c0c1ba8df049d617a74a06d3bd6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-09 12:00:00 -0700

    976

---
+++ 0976/976.java
@@ -0,0 +1,6 @@
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        java.util.Arrays.sort(nums); for(int i=nums.length-1;i>=2;i--) if(nums[i-2]+nums[i-1]>nums[i]) return nums[i-2]+nums[i-1]+nums[i]; return 0;
+    }
+}
