dfde8d78419b0ff29969d77aaf7adc1650df5357
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-08 19:08:07 -0700

    3127

---
+++ 3127/3127.py
@@ -0,0 +1,6 @@
+class Solution:
+    def make_a_square_with_the_same_color(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
