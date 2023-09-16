650536345f730234c78b1813a3dc298335754de1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-16 16:37:38 -0700

    3135

---
+++ 3135/3135.py
@@ -0,0 +1,8 @@
+class Solution:
+    def equalize_strings_by_adding_or_removing_c(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
