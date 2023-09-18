36ee569dc0f79709b7489b40b8aac767389b99e4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-17 20:17:04 -0700

    662

---
+++ 0662/662.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum_width_of_binary_tree(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
