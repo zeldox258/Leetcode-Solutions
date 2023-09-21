6949163f391183853d0fcd673f7d08c36de2fd92
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-21 11:26:01 -0700

    3323

---
+++ 3323/3323.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MinimizeConnectedGroupsByInser(int[] nums, int k) {
+        var map = new Dictionary<int, int>();
+        map[0] = 1;
+        int sum = 0, count = 0;
+        foreach (int x in nums) {
+            sum += x;
+            if (map.ContainsKey(sum - k)) count += map[sum - k];
+            map[sum] = map.GetValueOrDefault(sum) + 1;
+        }
+        return count;
+    }
+}
