67bfef2f4408299ae1699b9d24ea89fd6721106c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-22 10:50:06 -0700

    667

---
+++ 0667/667.py
@@ -0,0 +1,6 @@
+class Solution:
+    def beautiful_arrangement_ii(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
