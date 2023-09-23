a1d124097e5e88090162bcba2300802ac25e84fa
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-23 09:54:03 -0700

    3142

---
+++ 3142/3142.py
@@ -0,0 +1,6 @@
+class Solution:
+    def check_if_grid_satisfies_conditions(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
