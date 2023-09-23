b07aa09646e1c4cc53ed1b79674c6fb294bb9449
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-23 10:48:34 -0700

    1912

---
+++ 1912/1912.py
@@ -0,0 +1,6 @@
+class Solution:
+    def design_movie_rental_system(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
