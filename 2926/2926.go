b856a24e4f7af06a92905a0024f6b023d0b4ddcb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-26 08:02:31 -0700

    2926

---
+++ 2926/2926.go
@@ -0,0 +1,15 @@
+func maximumBalancedSubsequenceSum(nums []int) int {
+	l, r, res := 0, len(nums)-1, 0
+	for l < r {
+		area := min(nums[l], nums[r]) * (r - l)
+		if area > res {
+			res = area
+		}
+		if nums[l] < nums[r] {
+			l++
+		} else {
+			r--
+		}
+	}
+	return res
+}
