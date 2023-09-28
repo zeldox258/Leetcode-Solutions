448aa91ec2368dfc6fed9844daf3496b704d2d1e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-27 19:02:19 -0700

    1916

---
+++ 1916/1916.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_ways_to_build_rooms_in_an_ant_colo(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
