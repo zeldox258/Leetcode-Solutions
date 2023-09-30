888a14ed84f8607e05a095c8b92fa25dd14e7b2a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-09-30 09:15:40 -0700

    675

---
+++ 0675/675.py
@@ -0,0 +1,8 @@
+class Solution:
+    def cut_off_trees_for_golf_event(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
