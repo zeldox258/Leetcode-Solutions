01dc442575452be67e5bf020f7c875a6185a5578
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-01 14:59:34 -0700

    1920

---
+++ 1920/1920.py
@@ -0,0 +1,8 @@
+class Solution:
+    def build_array_from_permutation(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
