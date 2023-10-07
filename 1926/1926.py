4aee18aa31738263852d2caf3c4fcdf0ea8c7d1a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-07 16:30:56 -0700

    1926

---
+++ 1926/1926.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nearest_exit_from_entrance_in_maze(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
