132d73254671f7ed89414ca982ce4dcf9dc01ead
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-12 21:51:44 -0700

    1931

---
+++ 1931/1931.py
@@ -0,0 +1,8 @@
+class Solution:
+    def painting_a_grid_with_three_different_col(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
