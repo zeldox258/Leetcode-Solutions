2d1ff049a8daa7d820309ad425863d91c9dd0770
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-13 16:03:56 -0700

    1932

---
+++ 1932/1932.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge_bsts_to_create_single_bst(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
