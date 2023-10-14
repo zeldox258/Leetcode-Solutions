3d20a57c280605bb8b7dab7d18c6c3b81e8e650a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-14 15:35:53 -0700

    689

---
+++ 0689/689.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximum_sum_of_3_nonoverlapping_subarray(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
