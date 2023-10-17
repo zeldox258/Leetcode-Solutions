567faf959a0304f8d113470d4be898318d094fff
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-16 17:05:30 -0700

    3165

---
+++ 3165/3165.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_sum_of_subsequence_with_nonadjac(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
