297ca6164d88d74038c3d8070b5a5745a44bd547
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-16 17:22:43 -0700

    1935

---
+++ 1935/1935.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_number_of_words_you_can_type(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
