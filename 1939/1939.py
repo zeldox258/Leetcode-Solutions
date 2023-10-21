78cbbce72d3a6f7369135b6f044efa7e7a9b24fa
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-20 22:24:15 -0700

    1939

---
+++ 1939/1939.py
@@ -0,0 +1,7 @@
+class Solution:
+    def users_that_actively_request_confirmation(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
