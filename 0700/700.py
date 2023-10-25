8422d054e3a87962f162b3e382698fca3981d3d6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-25 09:25:49 -0700

    700

---
+++ 0700/700.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search_in_a_binary_search_tree(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
