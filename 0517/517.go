5ef1ca14765069ee15885f5db94c9ad8175ce069
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-25 17:42:49 -0700

    517

---
+++ 0517/517.go
@@ -0,0 +1,11 @@
+func superWashingMachines(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
