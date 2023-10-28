a415ccedf62f18f20f71a13844b1bf7501efec12
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-28 09:58:46 -0700

    2378

---
+++ 2378/2378.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var chooseEdgesToMaximizeScoreInAT = function(nums, k) {
+    const map = new Map();
+    map.set(0, 1);
+    let sum = 0, count = 0;
+    for (const x of nums) {
+        sum += x;
+        count += map.get(sum - k) || 0;
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+    return count;
+};
