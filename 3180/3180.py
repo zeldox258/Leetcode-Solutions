ecc6db29ca257349c5c30b963f35a635a240fca2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-31 12:37:34 -0700

    3180

---
+++ 3180/3180.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_total_reward_using_operations_i(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
