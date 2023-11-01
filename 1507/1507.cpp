2a7a474e227411b94d895d34d793eafc4a34cd63
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-10-31 20:47:42 -0700

    1507

---
+++ 1507/1507.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* reformatDate(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* cur=head;
+        while(cur){
+            ListNode* next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+};
