bca228b9fa66f275567f68a961470053591c3c7a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-05 08:43:28 -0800

    711

---
+++ 0711/711.py
@@ -0,0 +1,11 @@
+class Solution:
+    def number_of_distinct_islands_ii(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
