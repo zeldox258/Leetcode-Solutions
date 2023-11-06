4d319c080167d4e5c7eb5e024a02a1705c8d2286
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-06 09:04:14 -0800

    712

---
+++ 0712/712.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_ascii_delete_sum_for_two_strings(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
