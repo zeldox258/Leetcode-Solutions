96d2d6139632646d15374a2f97ae96c92d1246d6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-07 09:43:10 -0800

    1957

---
+++ 1957/1957.py
@@ -0,0 +1,6 @@
+class Solution:
+    def delete_characters_to_make_fancy_string(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
