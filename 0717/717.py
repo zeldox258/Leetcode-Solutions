24895f0046d23fc83396f4e853da37def372c776
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-11 20:22:17 -0800

    717

---
+++ 0717/717.py
@@ -0,0 +1,9 @@
+class Solution:
+    def 1bit_and_2bit_characters(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
