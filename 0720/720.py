46e35ef427e41e0492cc9999d6b256d460a0374f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-14 18:34:49 -0800

    720

---
+++ 0720/720.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longest_word_in_dictionary(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
