50de3dbd8b3f2a82fca5663ba9115b78c37006e3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-15 16:44:49 -0800

    3195

---
+++ 3195/3195.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_the_minimum_area_to_cover_all_ones_(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
