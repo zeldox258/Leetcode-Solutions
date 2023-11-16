35bc717d9269e5c685ac659b789f9baa51082e80
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-15 16:23:51 -0800

    1965

---
+++ 1965/1965.py
@@ -0,0 +1,8 @@
+class Solution:
+    def employees_with_missing_information(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
