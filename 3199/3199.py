4167b3d559fa087cc1bafd2e796356c4b1b18ba8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-19 12:04:05 -0800

    3199

---
+++ 3199/3199.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_triplets_with_even_xor_set_bits_i(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
