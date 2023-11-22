a74722d3efafecfc1c0447f1f75426fd4faf05d7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-22 08:49:41 -0800

    3202

---
+++ 3202/3202.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_the_maximum_length_of_valid_subsequ(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
