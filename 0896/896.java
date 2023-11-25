66ef35e52a4de7b9f35fab9ea5a278d623cc3c65
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-25 12:00:00 -0800

    896

---
+++ 0896/896.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc = true, dec = true;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i-1]) dec = false;
+            if (nums[i] < nums[i-1]) inc = false;
+        }
+        return inc || dec;
+    }
+}
