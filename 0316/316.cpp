797915d7abe13f5ad956c86ed67e0a498e51768e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-29 14:31:26 -0800

    316

---
+++ 0316/316.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeDuplicateLetters(vector<int>& nums) {
+        int l=0,r=nums.size()-1,res=0;
+        while(l<r){
+            res=max(res,min(nums[l],nums[r])*(r-l));
+            if(nums[l]<nums[r]) l++; else r--;
+        }
+        return res;
+    }
+};
