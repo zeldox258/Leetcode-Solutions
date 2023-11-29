80ed5132043206580e5d7bc016351abcc3315c7e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-11-29 13:22:59 -0800

    1980

---
+++ 1980/1980.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_unique_binary_string(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
