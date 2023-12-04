5f9d5a18d8c847028369c01cbd4a272aada63e66
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-04 09:43:44 -0800

    740

---
+++ 0740/740.py
@@ -0,0 +1,10 @@
+class Solution:
+    def delete_and_earn(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
