fa19a15a19a72ba31c06e3338beefe5a6ed4db85
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-04 18:49:14 -0800

    3214

---
+++ 3214/3214.py
@@ -0,0 +1,7 @@
+class Solution:
+    def year_on_year_growth_rate(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
