0cb578bd502a4dfa2c45a992b53afb08b03a5c27
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-05 11:11:24 -0800

    3215

---
+++ 3215/3215.py
@@ -0,0 +1,10 @@
+class Solution:
+    def count_triplets_with_even_xor_set_bits_ii(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
