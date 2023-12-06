095a35f1e38dd10b8960cfdb7f2f1a3132480947
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-05 20:12:14 -0800

    558

---
+++ 0558/558.go
@@ -0,0 +1,11 @@
+func logicalOrOfTwoBinaryGridsRepre(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := logicalOrOfTwoBinaryGridsRepre(root.Left)
+	r := logicalOrOfTwoBinaryGridsRepre(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
