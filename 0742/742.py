2f6056568236a0c6992f7ac7148d9d59936b4772
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-06 11:02:48 -0800

    742

---
+++ 0742/742.py
@@ -0,0 +1,6 @@
+class Solution:
+    def closest_leaf_in_a_binary_tree(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
