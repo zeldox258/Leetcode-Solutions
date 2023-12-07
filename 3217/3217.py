1ef1fc3bb038ea7a4f461081371ce29e85affc65
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-07 10:16:23 -0800

    3217

---
+++ 3217/3217.py
@@ -0,0 +1,6 @@
+class Solution:
+    def delete_nodes_from_linked_list_present_in(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
