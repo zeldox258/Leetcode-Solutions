3034b4bd75c6caab1c5a40a9ee385af9760724cb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-11 13:31:35 -0800

    2505

---
+++ 2505/2505.py
@@ -0,0 +1,8 @@
+class Solution:
+    def bitwise_or_of_all_subsequence_sums(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
