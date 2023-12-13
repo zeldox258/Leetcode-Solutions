678c35f4f122f8c48ca2daf7d432289b9f5daf82
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-13 13:06:08 -0800

    3004

---
+++ 3004/3004.go
@@ -0,0 +1,14 @@
+func maximumSubtreeOfTheSameColor(nums []int) int {
+	cur, mx := nums[0], nums[0]
+	for _, x := range nums[1:] {
+		if x > cur+x {
+			cur = x
+		} else {
+			cur = cur + x
+		}
+		if cur > mx {
+			mx = cur
+		}
+	}
+	return mx
+}
