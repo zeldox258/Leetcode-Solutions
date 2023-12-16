2944a522cc30b02c343f52b0bd411be299931082
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-16 13:00:02 -0800

    2482

---
+++ 2482/2482.py
@@ -0,0 +1,6 @@
+class Solution:
+    def difference_between_ones_and_zeros_in_row(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
