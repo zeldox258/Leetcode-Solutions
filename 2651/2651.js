34e6a3f5dd7ec465358c99c56a3930e9264aea10
Omer <hsynmrgry@gmail.com>
2023-12-17 09:44:01 -0800

    2651

---
+++ 2651/2651.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} arrivalTime
+ * @param {number} delayedTime
+ * @return {number}
+ */
+var findDelayedArrivalTime = function(arrivalTime, delayedTime) {
+    return (arrivalTime + delayedTime) % 24;
+    
+};
