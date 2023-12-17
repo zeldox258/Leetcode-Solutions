64786000f4e1526d61d42eb5a232dab70595d4c9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-16 19:54:07 -0800

    1250

---
+++ 1250/1250.py
@@ -0,0 +1,10 @@
+class Solution:
+    def check_if_it_is_a_good_array(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
