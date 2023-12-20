31864d867029cc3a640b59a3864e71063f0673a4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-19 18:36:52 -0800

    2772

---
+++ 2772/2772.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int applyOperationsToMakeAllArrayE(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](auto& a,auto& b){return a[1]<b[1];});
+        int count=1,end=intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]>=end){count++;end=intervals[i][1];}
+        }
+        return count;
+    }
+};
