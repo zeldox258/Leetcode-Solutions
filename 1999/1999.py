4a4a8abe943f115b7356d85a536a3aa864d0fedb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-22 16:07:42 -0800

    1999

---
+++ 1999/1999.py
@@ -0,0 +1,7 @@
+class Solution:
+    def smallest_greater_multiple_made_of_two_di(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
