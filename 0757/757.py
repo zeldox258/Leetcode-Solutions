69ded64b096d8ceca20abd6a346094afbcda911a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-25 08:49:39 -0800

    757

---
+++ 0757/757.py
@@ -0,0 +1,6 @@
+class Solution:
+    def set_intersection_size_at_least_two(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
