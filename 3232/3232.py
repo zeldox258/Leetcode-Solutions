e7aba446afb73098377722ed0b687041bec45b12
huseyinguray_microsoft <huseyinguray@microsoft.com>
2023-12-26 09:01:19 -0800

    3232

---
+++ 3232/3232.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_if_digit_game_can_be_won(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
