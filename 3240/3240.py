ce6dd593f4d119287f1540b21db7c45bfe08b88a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-03 18:14:16 -0800

    3240

---
+++ 3240/3240.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_number_of_flips_to_make_binary_g(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
