d7c096fbe4d09b53b2ffc6af0fba773ee1a27b0b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-07 13:59:25 -0800

    2015

---
+++ 2015/2015.py
@@ -0,0 +1,10 @@
+class Solution:
+    def average_height_of_buildings_in_each_segm(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
