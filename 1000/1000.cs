42c35251b79fc6b21c51f353c343413ef93c76b3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-12 16:39:51 -0800

    1000

---
+++ 1000/1000.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    private int[] parent;
+    public int MinimumCostToMergeStones(int n, int[][] edges) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        int comp = n;
+        foreach (var e in edges) {
+            int a = Find(e[0]), b = Find(e[1]);
+            if (a != b) { parent[a] = b; comp--; }
+        }
+        return comp;
+    }
+    private int Find(int x) => parent[x] == x ? x : parent[x] = Find(parent[x]);
+}
