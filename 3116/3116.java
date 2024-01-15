4c2ab582249c19fc7ec43f62963f4b45f3d0d025
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-15 09:05:17 -0800

    3116

---
+++ 3116/3116.java
@@ -0,0 +1,15 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> solve3116(int[] nums) {
+        Arrays.sort(nums);
+        bt(nums,0,new ArrayList<>());
+        return res;
+    }
+    void bt(int[] nums,int start,List<Integer> path){
+        res.add(new ArrayList<>(path));
+        for(int i=start;i<nums.length;i++){
+            if(i>start&&nums[i]==nums[i-1]) continue;
+            path.add(nums[i]);bt(nums,i+1,path);path.remove(path.size()-1);
+        }
+    }
+}
