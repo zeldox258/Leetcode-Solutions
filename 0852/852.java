bdab55bf850e0bfd63e3e85513c5f3eb4fc71f3d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-18 12:00:00 -0800

    852

---
+++ 0852/852.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) { int mid = (l + r) / 2; if (arr[mid] < arr[mid+1]) l = mid + 1; else r = mid; }
+        return l;
+    }
+}
