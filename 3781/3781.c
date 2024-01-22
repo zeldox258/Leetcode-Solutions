51f0a27b661ec10ea6a32a8621e9b2f8baf826aa
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-22 09:17:12 -0800

    3781

---
+++ 3781/3781.c
@@ -0,0 +1,9 @@
+int maximumScoreAfterBinarySwaps(int* nums, int numsSize) {
+    int l = 0, r = numsSize - 1, res = 0;
+    while (l < r) {
+        int area = (nums[l] < nums[r] ? nums[l] : nums[r]) * (r - l);
+        if (area > res) res = area;
+        if (nums[l] < nums[r]) l++; else r--;
+    }
+    return res;
+}
