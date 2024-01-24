c22fc53d7742818509b959ef35bddde18ff31153
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-23 21:37:26 -0800

    3260

---
+++ 3260/3260.py
@@ -0,0 +1,10 @@
+class Solution:
+    def find_the_largest_palindrome_divisible_by(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
