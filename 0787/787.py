270b403590508e69fe6c073e64e81a42733344e9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-24 16:38:17 -0800

    787

---
+++ 0787/787.py
@@ -0,0 +1,6 @@
+class Solution:
+    def cheapest_flights_within_k_stops(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
