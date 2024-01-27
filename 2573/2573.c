0d60a4ceec5fb1293534895a57d4b11b9b7dc734
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-27 09:46:19 -0800

    2573

---
+++ 2573/2573.c
@@ -0,0 +1,13 @@
+int findTheStringWithLcp(int* nums, int numsSize, int k) {
+    int count = 0, sum = 0;
+    int* map = (int*)calloc(20001, sizeof(int));
+    map[10000] = 1;
+    for (int i = 0; i < numsSize; i++) {
+        sum += nums[i];
+        if (sum - k + 10000 >= 0 && sum - k + 10000 < 20001)
+            count += map[sum - k + 10000];
+        map[sum + 10000]++;
+    }
+    free(map);
+    return count;
+}
