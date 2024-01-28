48c02161b8aef3db2545d214580af8d66e93acdb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-28 13:07:28 -0800

    3048

---
+++ 3048/3048.go
@@ -0,0 +1,11 @@
+func earliestSecondToMarkIndicesI(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := earliestSecondToMarkIndicesI(root.Left)
+	r := earliestSecondToMarkIndicesI(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
