2b2779a4019351a7829c73f9aaa6f6af1df48878
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-01-28 13:43:15 -0800

    791

---
+++ 0791/791.py
@@ -0,0 +1,8 @@
+class Solution:
+    def custom_sort_string(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
