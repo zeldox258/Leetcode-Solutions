cfa16677d77806f8b79d5095760c5346d4664b0b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-02 09:40:33 -0800

    3270

---
+++ 3270/3270.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_the_key_of_the_numbers(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
