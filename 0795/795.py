5dcca09003ea69f6ba89112c32df35f129ae6818
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-01 17:44:59 -0800

    795

---
+++ 0795/795.py
@@ -0,0 +1,8 @@
+class Solution:
+    def number_of_subarrays_with_bounded_maximum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
