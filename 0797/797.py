c088741907677fb23396c7a8792e16a025c5ce35
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-03 14:08:00 -0800

    797

---
+++ 0797/797.py
@@ -0,0 +1,12 @@
+class Solution:
+    def all_paths_from_source_to_target(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
