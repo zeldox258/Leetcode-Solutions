4bf18971ab85ee8b95fe9b0a2a5f4c88fe967d4c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-05 11:25:19 -0800

    2044

---
+++ 2044/2044.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_number_of_maximum_bitwiseor_subset(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
