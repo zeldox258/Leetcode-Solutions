a7a7da12121cb2b573ca80a3a60ffc73f1500825
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-08 09:02:03 -0800

    3276

---
+++ 3276/3276.py
@@ -0,0 +1,11 @@
+class Solution:
+    def select_cells_in_grid_with_maximum_score(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
