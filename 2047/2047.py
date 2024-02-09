a2074d93ae934cfd7e7e9f583327d0a607636bdf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-08 16:13:31 -0800

    2047

---
+++ 2047/2047.py
@@ -0,0 +1,6 @@
+class Solution:
+    def number_of_valid_words_in_a_sentence(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
