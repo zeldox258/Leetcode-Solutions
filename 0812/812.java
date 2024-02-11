bb1f041304a306b8fa66a7ae7f5a449d2741fcc7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-11 12:00:00 -0800

    812

---
+++ 0812/812.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double res = 0;
+        int n = points.length;
+        for (int i = 0; i < n; i++) for (int j = i+1; j < n; j++) for (int k = j+1; k < n; k++) {
+            res = Math.max(res, area(points[i], points[j], points[k]));
+        }
+        return res;
+    }
+    double area(int[] a, int[] b, int[] c) {
+        return 0.5 * Math.abs(a[0]*(b[1]-c[1]) + b[0]*(c[1]-a[1]) + c[0]*(a[1]-b[1]));
+    }
+}
