485ef9d23e69d90b6b91f04f0848f7293c8aeb63
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-13 14:10:39 -0800

    3281

---
+++ 3281/3281.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximize_score_of_numbers_in_ranges(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
