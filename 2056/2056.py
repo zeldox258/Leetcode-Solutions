0d5d5604614a4c2d14ac867ddf64b6d6ed1f696a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-17 20:30:03 -0800

    2056

---
+++ 2056/2056.py
@@ -0,0 +1,10 @@
+class Solution:
+    def number_of_valid_move_combinations_on_che(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
