66c0721a4ba4c1349cf707723b727dfce9f539f7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-21 10:21:01 -0800

    2060

---
+++ 2060/2060.py
@@ -0,0 +1,10 @@
+class Solution:
+    def check_if_an_original_string_exists_given(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
