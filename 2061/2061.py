1cdc480c7ffe61a2c98f39afb6dcb8d69ebc5ff4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-22 20:57:53 -0800

    2061

---
+++ 2061/2061.py
@@ -0,0 +1,11 @@
+class Solution:
+    def number_of_spaces_cleaning_robot_cleaned(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
