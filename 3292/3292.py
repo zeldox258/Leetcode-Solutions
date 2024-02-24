bac8dffc7561e5f7ab73361cbe96a395547f1054
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-24 10:42:31 -0800

    3292

---
+++ 3292/3292.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_number_of_valid_strings_to_form_(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
