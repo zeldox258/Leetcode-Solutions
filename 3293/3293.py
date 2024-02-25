150abe7e2b589af7e17c4bb70c30a0df0a8f93d1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-02-25 12:01:34 -0800

    3293

---
+++ 3293/3293.py
@@ -0,0 +1,9 @@
+class Solution:
+    def calculate_product_final_price(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
