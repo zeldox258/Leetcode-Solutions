47aabd19ea2346d99d6c38ad1e3f1936928e2e57
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-02 09:17:10 -0800

    3082

---
+++ 3082/3082.go
@@ -0,0 +1,11 @@
+func findTheSumOfThePowerOfAllSubse(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
