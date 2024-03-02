089a53b6df6dcf9cec525fc745d8bacfdef78857
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-02 13:07:49 -0800

    825

---
+++ 0825/825.py
@@ -0,0 +1,8 @@
+class Solution:
+    def friends_of_appropriate_ages(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
