5312ccd7cac825f6c3dea3ea4d1fa008de0f37cd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-06 14:20:00 -0800

    2074

---
+++ 2074/2074.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverse_nodes_in_even_length_groups(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
