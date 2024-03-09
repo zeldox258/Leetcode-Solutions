f978a110e2494ce7f4a7810b4557111d5839b483
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-09 08:32:08 -0800

    832

---
+++ 0832/832.py
@@ -0,0 +1,6 @@
+class Solution:
+    def flipping_an_image(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
