7b92a7ae917bbafb132eeaf35038ebf502fd1774
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-10 11:05:57 -0700

    2078

---
+++ 2078/2078.py
@@ -0,0 +1,9 @@
+class Solution:
+    def two_furthest_houses_with_different_color(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
