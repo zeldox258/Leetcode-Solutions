89049809abc1c7042221ecd843ca3938516c5d67
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-13 18:35:55 -0700

    3093

---
+++ 3093/3093.go
@@ -0,0 +1,11 @@
+func longestCommonSuffixQueries(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := longestCommonSuffixQueries(root.Left)
+	r := longestCommonSuffixQueries(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
