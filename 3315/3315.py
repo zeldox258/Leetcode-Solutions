27f10d62c31429f92a61cbb30e92884ac9182fd3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-18 18:22:44 -0700

    3315

---
+++ 3315/3315.py
@@ -0,0 +1,8 @@
+class Solution:
+    def construct_the_minimum_bitwise_array_ii(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
