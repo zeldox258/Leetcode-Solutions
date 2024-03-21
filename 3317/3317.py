c9913ed58c3a49cf29a2923c0d4295295ca48a83
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-20 17:26:52 -0700

    3317

---
+++ 3317/3317.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find_the_number_of_possible_ways_for_an_(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
