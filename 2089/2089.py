08fefa509cdb331824c70feb871fe73fb2e054af
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-21 11:46:59 -0700

    2089

---
+++ 2089/2089.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_target_indices_after_sorting_array(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
