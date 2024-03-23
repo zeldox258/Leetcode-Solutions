51f7c377eac762e108de0115cc317537ef08609b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-23 13:44:09 -0700

    3320

---
+++ 3320/3320.py
@@ -0,0 +1,10 @@
+class Solution:
+    def count_the_number_of_winning_sequences(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
