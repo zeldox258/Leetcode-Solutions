54e9982d760e9cc9812cd84898bdb8d9b55fedd2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-24 15:05:21 -0700

    847

---
+++ 0847/847.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shortest_path_visiting_all_nodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
