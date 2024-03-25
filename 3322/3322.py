c32b2309d9aa3b9e6e81de55fa3d63502437833f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-25 12:01:12 -0700

    3322

---
+++ 3322/3322.py
@@ -0,0 +1,6 @@
+class Solution:
+    def premier_league_table_ranking_iii(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
