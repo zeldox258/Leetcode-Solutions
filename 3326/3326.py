9129342f3ce19df075414c234a6fd33285c70a0d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-29 08:29:10 -0700

    3326

---
+++ 3326/3326.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_division_operations_to_make_arra(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
