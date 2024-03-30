b12db659ec3c4ccf2acb4b42be3644f36cce1bb4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-30 12:00:00 -0700

    706

---
+++ 0706/706.java
@@ -0,0 +1,8 @@
+
+class MyHashMap {
+    int[] data = new int[1000001];
+    public MyHashMap() { Arrays.fill(data, -1); }
+    public void put(int key, int value) { data[key] = value; }
+    public int get(int key) { return data[key]; }
+    public void remove(int key) { data[key] = -1; }
+}
