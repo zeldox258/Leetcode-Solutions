1b0455b2956921c7272fc862f71a6ae7f00fb54f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-03-31 16:22:05 -0700

    2099

---
+++ 2099/2099.py
@@ -0,0 +1,14 @@
+class Solution:
+    def find_subsequence_of_length_k_with_the_la(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
