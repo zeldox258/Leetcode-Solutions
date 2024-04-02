22caf7179288f460c39c7a30df1b0831a1af6e4b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-01 21:07:57 -0700

    2100

---
+++ 2100/2100.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_good_days_to_rob_the_bank(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
