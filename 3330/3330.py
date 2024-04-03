1fd8ed33ca4d8b66786ead8d2e290c0b4cdc2808
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-02 19:02:31 -0700

    3330

---
+++ 3330/3330.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_the_original_typed_string_i(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
