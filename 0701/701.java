c5412eaa31ca6830d16ad07aa3ed510d54b1bdea
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-03 12:00:00 -0700

    701

---
+++ 0701/701.java
@@ -0,0 +1,9 @@
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val < root.val) root.left = insertIntoBST(root.left, val);
+        else root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+}
