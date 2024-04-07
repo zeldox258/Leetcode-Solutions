bf17932dff94c1864c6f9c6c47e2e9d2ab31450a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-06 20:04:17 -0700

    3334

---
+++ 3334/3334.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_the_maximum_factor_score_of_array(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
