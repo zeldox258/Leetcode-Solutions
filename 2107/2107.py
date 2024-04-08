441cd4459c90fd859cbd9b9e8d7e84f900b25cb7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-08 15:21:22 -0700

    2107

---
+++ 2107/2107.py
@@ -0,0 +1,6 @@
+class Solution:
+    def number_of_unique_flavors_after_sharing_k(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
