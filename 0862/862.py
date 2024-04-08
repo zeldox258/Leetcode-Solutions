223e04c383e2207292c5eab577ce414b393b2a38
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-08 13:40:57 -0700

    862

---
+++ 0862/862.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shortest_subarray_with_sum_at_least_k(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
