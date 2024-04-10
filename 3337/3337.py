6a0c2eb3ef002dd053f267cc03df398a509a8bad
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-09 22:12:19 -0700

    3337

---
+++ 3337/3337.py
@@ -0,0 +1,6 @@
+class Solution:
+    def total_characters_in_string_after_transfo(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
