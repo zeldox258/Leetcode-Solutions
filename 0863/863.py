a57cfa2923bcf2513eab147bc48b83531b444bb4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-09 20:04:54 -0700

    863

---
+++ 0863/863.py
@@ -0,0 +1,9 @@
+class Solution:
+    def all_nodes_distance_k_in_binary_tree(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
