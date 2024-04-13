684b4fb9e2451939a7b05f423d1caa78cd2d7a54
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-13 15:25:01 -0700

    3341

---
+++ 3341/3341.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_minimum_time_to_reach_last_room_i(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
