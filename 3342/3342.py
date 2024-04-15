b03543b65bb61720e74afc61e98d99392d308b46
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-14 20:30:39 -0700

    3342

---
+++ 3342/3342.py
@@ -0,0 +1,9 @@
+class Solution:
+    def find_minimum_time_to_reach_last_room_ii(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
