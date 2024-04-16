5ce61a084c5de9da5db1b356aab878d653d7036a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-16 09:25:58 -0700

    2115

---
+++ 2115/2115.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_all_possible_recipes_from_given_sup(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
