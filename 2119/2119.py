b90246d740727036e87e9c739368d75602fd1350
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-20 15:39:16 -0700

    2119

---
+++ 2119/2119.py
@@ -0,0 +1,7 @@
+class Solution:
+    def a_number_after_a_double_reversal(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
