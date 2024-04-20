a1f2af1bf2df07bb7503a2963623a33cddd58296
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-19 19:09:17 -0700

    3347

---
+++ 3347/3347.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum_frequency_of_an_element_after_pe(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
