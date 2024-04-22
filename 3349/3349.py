fefb2fad00ef0f76c8c4ea69c64c77839ded0a90
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-21 21:46:51 -0700

    3349

---
+++ 3349/3349.py
@@ -0,0 +1,7 @@
+class Solution:
+    def adjacent_increasing_subarrays_detection_(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
