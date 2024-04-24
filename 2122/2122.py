98db108e536a069e4d082e8f754106a464aa73e2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-23 21:12:09 -0700

    2122

---
+++ 2122/2122.py
@@ -0,0 +1,6 @@
+class Solution:
+    def recover_the_original_array(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
