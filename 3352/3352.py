d9170f4e70abf86274866a7d2c424ca723a5631c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-24 17:15:53 -0700

    3352

---
+++ 3352/3352.py
@@ -0,0 +1,6 @@
+class Solution:
+    def count_kreducible_numbers_less_than_n(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
