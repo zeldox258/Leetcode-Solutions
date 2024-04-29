6a3e4965ea0d1878e63565bbb03832f8ced05b29
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-28 21:07:57 -0700

    2127

---
+++ 2127/2127.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum_employees_to_be_invited_to_a_mee(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
