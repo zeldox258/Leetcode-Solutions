5cce1ca3effe4cd66701217c94bfee29baa5bc68
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-04-30 17:59:50 -0700

    884

---
+++ 0884/884.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uncommon_words_from_two_sentences(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
