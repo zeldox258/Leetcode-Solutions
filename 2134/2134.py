6d27f65538550ea2b94d9baa771c7fbf98dafe1f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-05 18:13:57 -0700

    2134

---
+++ 2134/2134.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_swaps_to_group_all_1s_together_i(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
