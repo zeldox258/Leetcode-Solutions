3fccc84339b075b7a5f36e1c22f488f21b6a7a3d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-07 12:00:00 -0700

    651

---
+++ 0651/651.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int minSteps(int n) {
+        int res = 0;
+        for (int d = 2; d <= n; d++) {
+            while (n % d == 0) {
+                res += d;
+                n /= d;
+            }
+        }
+        return res;
+    }
+}
