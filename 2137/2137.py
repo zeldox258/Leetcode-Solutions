37f2b313f4e3741596388a1b56458d03f7fe16d3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-08 13:04:57 -0700

    2137

---
+++ 2137/2137.py
@@ -0,0 +1,6 @@
+class Solution:
+    def pour_water_between_buckets_to_make_water(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
