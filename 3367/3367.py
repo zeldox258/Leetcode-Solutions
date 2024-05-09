9021fc5c09fa5030f8dd2fae6c3eb0da4b1b657e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-09 13:14:47 -0700

    3367

---
+++ 3367/3367.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximize_sum_of_weights_after_edge_remov(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
