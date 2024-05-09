70e6579765a86b6283ded2815d390e97674b0131
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-08 17:59:33 -0700

    892

---
+++ 0892/892.py
@@ -0,0 +1,6 @@
+class Solution:
+    def surface_area_of_3d_shapes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
