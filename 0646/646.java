8f2589c93493fdc6af780acefd62e393adca3849
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-12 12:00:00 -0700

    646

---
+++ 0646/646.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a,b) -> a[1] - b[1]);
+        int count = 1, end = pairs[0][1];
+        for (int i = 1; i < pairs.length; i++) {
+            if (pairs[i][0] > end) { count++; end = pairs[i][1]; }
+        }
+        return count;
+    }
+}
