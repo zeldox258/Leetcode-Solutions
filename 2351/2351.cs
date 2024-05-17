bad53cce8ee9b466507064dd53f40d8878328947
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-17 11:24:43 -0700

    2351

---
+++ 2351/2351.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] FirstLetterToAppearTwice(int[] nums) {
+        int n = nums.Length;
+        var res = new int[n];
+        Array.Fill(res, -1);
+        var st = new Stack<int>();
+        for (int i = 0; i < n; i++) {
+            while (st.Count > 0 && nums[st.Peek()] < nums[i]) res[st.Pop()] = nums[i];
+            st.Push(i);
+        }
+        return res;
+    }
+}
