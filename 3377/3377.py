ab59a94b5ef80cdddddabe3b01ccb4095823dec3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-19 13:16:52 -0700

    3377

---
+++ 3377/3377.py
@@ -0,0 +1,12 @@
+class Solution:
+    def digit_operations_to_make_two_integers_eq(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
