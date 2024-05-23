14f68e7477ac6502050ae93077a3c73190960cd1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-23 12:01:10 -0700

    907

---
+++ 0907/907.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sum_of_subarray_minimums(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
