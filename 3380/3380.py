37eaf0b0d9364ba4c33b8ac0ea955d9c5c11db07
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-22 20:55:50 -0700

    3380

---
+++ 3380/3380.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximum_area_rectangle_with_point_constr(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
