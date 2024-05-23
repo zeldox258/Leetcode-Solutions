bcd0ed83d7df9259461d50469a3060db355687d0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-23 12:00:00 -0700

    628

---
+++ 0628/628.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1]);
+    }
+}
