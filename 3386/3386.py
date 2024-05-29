561b85727382cef8af6f8eee4cad228ffabdc087
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-28 19:57:08 -0700

    3386

---
+++ 3386/3386.py
@@ -0,0 +1,8 @@
+class Solution:
+    def button_with_longest_push_time(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
