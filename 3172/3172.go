e3ed622e5500a98ecd428fe0eaee4573069bb894
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-31 16:56:52 -0700

    3172

---
+++ 3172/3172.go
@@ -0,0 +1,11 @@
+func secondDayVerification(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
