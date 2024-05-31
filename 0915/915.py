532c126b15759c20f20bd821c519cce19d06aa28
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-31 13:55:49 -0700

    915

---
+++ 0915/915.py
@@ -0,0 +1,8 @@
+class Solution:
+    def partition_array_into_disjoint_intervals(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
