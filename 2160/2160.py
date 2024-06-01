9b24bdc7e191ac2c5093ecf41625ea53b3d23990
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-05-31 22:01:42 -0700

    2160

---
+++ 2160/2160.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_sum_of_four_digit_number_after_s(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
