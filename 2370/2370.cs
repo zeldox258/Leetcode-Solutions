edcd409ab6244c9bc0ebdd89c962e5430e492cf2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-04 13:58:54 -0700

    2370

---
+++ 2370/2370.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] LongestIdealSubsequence(int[] nums, int target) {
+        var map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (map.TryGetValue(target - nums[i], out int j)) return new[] {j, i};
+            map[nums[i]] = i;
+        }
+        return Array.Empty<int>();
+    }
+}
