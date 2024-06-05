930e855cbb6fa319f6f1c76ca36ff6032dd056f5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-05 11:01:49 -0700

    2165

---
+++ 2165/2165.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallest_value_of_the_rearranged_number(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
