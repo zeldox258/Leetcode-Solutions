10f77bf736111dde00b543ec6ff8c440d2fe77ee
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-05 21:41:19 -0700

    3394

---
+++ 3394/3394.py
@@ -0,0 +1,7 @@
+class Solution:
+    def check_if_grid_can_be_cut_into_sections(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
