2872777c981bdb713f2d60510beb0ce98ca9f85d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-07 12:16:47 -0700

    2167

---
+++ 2167/2167.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_time_to_remove_all_cars_containi(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
