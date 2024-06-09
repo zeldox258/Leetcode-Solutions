5023df902bbd5f879d1bb380a12080b1ebaf6a6a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-08 22:33:05 -0700

    3397

---
+++ 3397/3397.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_number_of_distinct_elements_afte(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
