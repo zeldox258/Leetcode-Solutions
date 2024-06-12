a5722dab8e6cf4bc5de1ea97f0de1789cd539620
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-12 14:22:59 -0700

    2711

---
+++ 2711/2711.c
@@ -0,0 +1,11 @@
+int* differenceOfNumberOfDistinctVa(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+    int* res = (int*)malloc(numsSize * sizeof(int));
+    int st[numsSize], top = -1;
+    for (int i = 0; i < numsSize; i++) res[i] = -1;
+    for (int i = 0; i < numsSize; i++) {
+        while (top >= 0 && nums[st[top]] < nums[i]) res[st[top--]] = nums[i];
+        st[++top] = i;
+    }
+    return res;
+}
