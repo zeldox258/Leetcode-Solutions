a95b94c4eeba83acb76196407bba474212926f0e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-14 12:00:00 -0700

    572

---
+++ 0572/572.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSame(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    boolean isSame(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        return a.val == b.val && isSame(a.left, b.left) && isSame(a.right, b.right);
+    }
+}
