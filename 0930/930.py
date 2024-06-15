0acf6b68469eecb64a24f4ac982c2a98a674298c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-15 11:06:46 -0700

    930

---
+++ 0930/930.py
@@ -0,0 +1,8 @@
+class Solution:
+    def binary_subarrays_with_sum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
