70114ce52b2f07ccc0c63326e17eaed4dfe05af1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-15 19:39:29 -0700

    2175

---
+++ 2175/2175.py
@@ -0,0 +1,8 @@
+class Solution:
+    def the_change_in_global_rankings(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
