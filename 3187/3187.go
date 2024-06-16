9d3c8244a14b4e67d68f63382929fa8bd4e6f37d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-15 21:36:28 -0700

    3187

---
+++ 3187/3187.go
@@ -0,0 +1,11 @@
+func peaksInArray(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
