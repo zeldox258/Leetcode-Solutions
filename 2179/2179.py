cea0e80ca70031d74fc86ad6a42228c933840f00
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-19 09:18:56 -0700

    2179

---
+++ 2179/2179.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_good_triplets_in_an_array(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
