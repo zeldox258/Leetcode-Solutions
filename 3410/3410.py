3cc4ac359bb5373d936085c698077a59afc6defb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-21 22:30:28 -0700

    3410

---
+++ 3410/3410.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximize_subarray_sum_after_removing_all(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
