2587e4872be2b6a90d01ee9f8d1e9bbe00e02cce
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-22 19:59:03 -0700

    2182

---
+++ 2182/2182.py
@@ -0,0 +1,6 @@
+class Solution:
+    def construct_string_with_repeat_limit(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
