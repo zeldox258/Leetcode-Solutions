00bfa98b8f07c0060c363fde197f8c56df8b921a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-22 18:59:56 -0700

    937

---
+++ 0937/937.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reorder_data_in_log_files(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
