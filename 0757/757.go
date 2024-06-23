a349086cffd0df75a06ec0bdb8f7b64db17d96b6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-23 13:13:08 -0700

    757

---
+++ 0757/757.go
@@ -0,0 +1,11 @@
+func setIntersectionSizeAtLeastTwo(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
