6de7a45c7061e1db6ba7ea66d20e621be9a655df
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-30 14:09:41 -0700

    2190

---
+++ 2190/2190.py
@@ -0,0 +1,8 @@
+class Solution:
+    def most_frequent_number_following_key_in_an(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
