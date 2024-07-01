7d0d0d6b647ccba0294579fb736b6df1aaa23226
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-06-30 22:45:44 -0700

    945

---
+++ 0945/945.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_increment_to_make_array_unique(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
