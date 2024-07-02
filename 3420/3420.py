e79184d02145d1ebc8e1c6f10c2f292cdf2490b6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-01 20:54:50 -0700

    3420

---
+++ 3420/3420.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_nondecreasing_subarrays_after_k_op(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
