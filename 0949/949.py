c9dc563cfc396aad3dfd268e3b2e22f4a7264dc9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-04 13:45:13 -0700

    949

---
+++ 0949/949.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largest_time_for_given_digits(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
