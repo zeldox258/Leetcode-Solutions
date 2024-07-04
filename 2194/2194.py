d07eb1c2d8afeb00ed118758d7ec57e8d9c9098f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-04 08:10:33 -0700

    2194

---
+++ 2194/2194.py
@@ -0,0 +1,7 @@
+class Solution:
+    def cells_in_a_range_on_an_excel_sheet(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
