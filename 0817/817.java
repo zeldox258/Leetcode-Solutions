ea91cca3646a808961e692204a36cceb94168f0c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-05 16:34:54 -0700

    817

---
+++ 0817/817.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solve817(String s) {
+        Set<Character> set=new HashSet<>();
+        int l=0,res=0;
+        for(int r=0;r<s.length();r++){
+            while(set.contains(s.charAt(r))) set.remove(s.charAt(l++));
+            set.add(s.charAt(r));
+            res=Math.max(res,r-l+1);
+        }
+        return res;
+    }
+}
---
+++ 0817/README.md
@@ -0,0 +1,5 @@
+# 817. Linked List Components
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/linked-list-components/)
