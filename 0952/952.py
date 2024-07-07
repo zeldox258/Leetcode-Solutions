ffe4dc2a78eb315e21a249c8a4239009cbfcf199
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-07 14:20:22 -0700

    952

---
+++ 0952/952.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largest_component_size_by_common_factor(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
