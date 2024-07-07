f8b841a014820e4b8a358d3276da9667f2d765d9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-07 09:49:07 -0700

    2197

---
+++ 2197/2197.py
@@ -0,0 +1,6 @@
+class Solution:
+    def replace_noncoprime_numbers_in_array(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
