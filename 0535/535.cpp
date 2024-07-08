f2e282ad0472411d8e06f9720fc3647867078e3d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-07 18:55:21 -0700

    535

---
+++ 0535/535.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> parent;
+    int find(int x){return parent[x]==x?x:parent[x]=find(parent[x]);}
+    int encodeAndDecodeTinyurl(int n, vector<vector<int>>& edges) {
+        parent.resize(n);
+        iota(parent.begin(),parent.end(),0);
+        int comp=n;
+        for(auto& e:edges){
+            int a=find(e[0]),b=find(e[1]);
+            if(a!=b){parent[a]=b;comp--;}
+        }
+        return comp;
+    }
+};
