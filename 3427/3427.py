5d6060f524018e62da06a1f01e3aeca1e3a9082a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-08 22:00:28 -0700

    3427

---
+++ 3427/3427.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sum_of_variable_length_subarrays(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
