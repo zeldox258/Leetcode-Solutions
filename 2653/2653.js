ef64fdabae5d0a4bb3215156e9057a4716a2f863
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-09 11:20:37 -0700

    2653

---
+++ 2653/2653.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var slidingSubarrayBeauty = function(nums, k) {
+    const sorted = [...nums].sort((a, b) => a - b);
+    return sorted[sorted.length - k];
+};
