2f529f4e92b9fdd75206a80c3463074e72754913
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-10 17:56:54 -0700

    2200

---
+++ 2200/2200.py
@@ -0,0 +1,15 @@
+class Solution:
+    def find_all_kdistant_indices_in_an_array(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
