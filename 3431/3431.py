5ebd205571febb26f8a1bbf4cd43189a635aaadf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-12 12:45:53 -0700

    3431

---
+++ 3431/3431.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_unlocked_indices_to_sort_nums(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
