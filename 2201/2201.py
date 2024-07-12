8b588060b11455bf29cabaf04674f1d350baa41c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-11 22:18:51 -0700

    2201

---
+++ 2201/2201.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_artifacts_that_can_be_extracted(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
