f2da88eecc723bd2f83a4afed8f270d1313b1122
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-15 16:36:07 -0700

    59

---
+++ 0059/59.ts
@@ -0,0 +1,13 @@
+function spiralMatrixIi(nums: number[]): number[][] {
+    const res: number[][] = [];
+    nums.sort((a, b) => a - b);
+    const bt = (s: number, path: number[]) => {
+        res.push([...path]);
+        for (let i = s; i < nums.length; i++) {
+            if (i > s && nums[i] === nums[i - 1]) continue;
+            path.push(nums[i]); bt(i + 1, path); path.pop();
+        }
+    };
+    bt(0, []);
+    return res;
+}
