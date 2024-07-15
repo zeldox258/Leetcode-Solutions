c8beca549825d5504ccc2d3f8dd23ef1e00a7c09
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-15 09:31:49 -0700

    2205

---
+++ 2205/2205.py
@@ -0,0 +1,8 @@
+class Solution:
+    def the_number_of_users_that_are_eligible_fo(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
