ca7c446fd2997e1029485399481f17f3a3fbc30f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-19 08:29:55 -0700

    2209

---
+++ 2209/2209.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_white_tiles_after_covering_with_(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
