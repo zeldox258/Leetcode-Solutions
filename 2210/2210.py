a64940cad07d514a41637f194a1a033744e5e246
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-20 18:22:16 -0700

    2210

---
+++ 2210/2210.py
@@ -0,0 +1,10 @@
+class Solution:
+    def count_hills_and_valleys_in_an_array(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
