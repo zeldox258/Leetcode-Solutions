52adb0c2bf537bf5638097871249a1bc905c94bf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-22 11:47:23 -0700

    3442

---
+++ 3442/3442.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_difference_between_even_and_odd_(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
