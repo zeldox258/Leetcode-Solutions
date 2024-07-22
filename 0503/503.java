a645d1ccf04d3f21d54a6b25dcec774c65eb4537
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-22 12:00:00 -0700

    503

---
+++ 0503/503.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < 2 * n; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n])
+                res[stack.pop()] = nums[i % n];
+            if (i < n) stack.push(i);
+        }
+        return res;
+    }
+}
