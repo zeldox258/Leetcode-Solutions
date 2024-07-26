843f8ac2ed794d7c808077c058e7307725b60cd1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-26 10:43:32 -0700

    2422

---
+++ 2422/2422.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode MergeOperationsToTurnArrayInto(ListNode head) {
+        ListNode prev = null, cur = head;
+        while (cur != null) {
+            var next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
