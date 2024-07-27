ebc07e2932cee9f2681b1818586f4d3e3fe5d6ff
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-27 13:40:26 -0700

    319

---
+++ 0319/319.c
@@ -0,0 +1,8 @@
+int bulbSwitcher(int* nums, int numsSize) {
+    int cur = nums[0], mx = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        cur = nums[i] > cur + nums[i] ? nums[i] : cur + nums[i];
+        if (cur > mx) mx = cur;
+    }
+    return mx;
+}
