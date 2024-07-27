82f86a5158f9504f27acff71535d89c5b141b723
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-26 20:38:02 -0700

    971

---
+++ 0971/971.py
@@ -0,0 +1,8 @@
+class Solution:
+    def flip_binary_tree_to_match_preorder_trave(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
