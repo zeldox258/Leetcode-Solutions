8a3401257eb108c9f3a8218b3549009fb40f5b0e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-27 20:03:37 -0700

    1459

---
+++ 1459/1459.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rectanglesArea = function(nums) {
+    let max = nums[0], cur = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        cur = Math.max(nums[i], cur + nums[i]);
+        max = Math.max(max, cur);
+    }
+    return max;
+};
