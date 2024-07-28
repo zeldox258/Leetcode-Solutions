a40febce1f3ffa4068caedd8b2a777d6408ecb4a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-27 20:25:43 -0700

    2675

---
+++ 2675/2675.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var arrayOfObjectsToMatrix = function(s) {
+    const st = [];
+    const pairs = {'(': ')', '[': ']', '{': '}'};
+    for (const c of s) {
+        if (pairs[c]) st.push(c);
+        else {
+            if (!st.length || pairs[st.pop()] !== c) return false;
+        }
+    }
+    return st.length === 0;
+};
