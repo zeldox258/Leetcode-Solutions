b7d7306d4fc07213b99c754ab69a4fbeae80a1a9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-30 15:12:54 -0700

    3450

---
+++ 3450/3450.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_students_on_a_single_bench(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
