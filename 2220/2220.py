c38b0f5fd325aa659a42da8e7cdfa3e5ecc83f26
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-07-30 20:11:37 -0700

    2220

---
+++ 2220/2220.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_bit_flips_to_convert_number(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
