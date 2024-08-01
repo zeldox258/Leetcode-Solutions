3f2870d98af4eb56a956eef1032e473513fa7801
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-01 13:47:15 -0700

    3742

---
+++ 3742/3742.ts
@@ -0,0 +1,10 @@
+function maximumPathScoreInAGrid(head: ListNode | null): ListNode | null {
+    let prev: ListNode | null = null, cur = head;
+    while (cur) {
+        const next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
