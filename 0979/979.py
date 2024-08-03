b7c361211fecce76ef787355789ae59c094fa120
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-03 08:47:04 -0700

    979

---
+++ 0979/979.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distribute_coins_in_binary_tree(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
