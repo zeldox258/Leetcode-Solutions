62318b2ec167ead2daf66950cf3a6772d6ae4799
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-06 16:07:57 -0700

    2227

---
+++ 2227/2227.py
@@ -0,0 +1,6 @@
+class Solution:
+    def encrypt_and_decrypt_strings(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
