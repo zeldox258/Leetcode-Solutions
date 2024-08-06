4518de63985452a92322ec11b9173fda11afe204
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-05 22:37:15 -0700

    981

---
+++ 0981/981.py
@@ -0,0 +1,11 @@
+class Solution:
+    def time_based_keyvalue_store(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
