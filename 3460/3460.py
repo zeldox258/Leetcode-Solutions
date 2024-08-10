ed6d513da96f2bca40f2d1afc3b382c43997a7c1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-09 17:25:25 -0700

    3460

---
+++ 3460/3460.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longest_common_prefix_after_at_most_one_(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
