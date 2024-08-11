e4898dba923af73b7974c6bbb20d1a1a16b4d90e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-10 21:53:56 -0700

    3461

---
+++ 3461/3461.py
@@ -0,0 +1,8 @@
+class Solution:
+    def check_if_digits_are_equal_in_string_afte(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
