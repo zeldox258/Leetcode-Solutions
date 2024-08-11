74ca0642551a45705f6e2bbc8237ea195592f0a1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-11 08:48:05 -0700

    3008

---
+++ 3008/3008.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findBeautifulIndicesInTheGiven(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        m[0]=1;
+        int sum=0,count=0;
+        for(int x:nums){
+            sum+=x;
+            if(m.count(sum-k)) count+=m[sum-k];
+            m[sum]++;
+        }
+        return count;
+    }
+};
