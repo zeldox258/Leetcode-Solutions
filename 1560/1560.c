574a3de6d0723b9e07d55f0abfe51774b6e2fc04
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-15 21:41:46 -0700

    1560

---
+++ 1560/1560.c
@@ -0,0 +1,13 @@
+int* mostVisitedSectorInACircularTr(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int* res = (int*)malloc(2 * sizeof(int));
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                res[0] = i; res[1] = j;
+                return res;
+            }
+        }
+    }
+    return res;
+}
