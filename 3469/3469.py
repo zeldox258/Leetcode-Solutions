f7c6d1313ccb412bfdac90de5238651cfe87f53a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-17 16:22:57 -0700

    3469

---
+++ 3469/3469.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_minimum_cost_to_remove_array_elemen(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
