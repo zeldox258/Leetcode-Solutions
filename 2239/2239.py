ea552770517e5ea09c4877ee46a4912b05bd2a4a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-18 14:42:40 -0700

    2239

---
+++ 2239/2239.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_closest_number_to_zero(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
