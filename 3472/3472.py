cc5af46c14129d4dfaf5ec153458e1f34ebec567
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-20 09:38:12 -0700

    3472

---
+++ 3472/3472.py
@@ -0,0 +1,6 @@
+class Solution:
+    def longest_palindromic_subsequence_after_at(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
