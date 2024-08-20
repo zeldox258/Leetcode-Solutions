61c9ac1bc08972ae28525e5e81dc1836e0eea80a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-19 19:29:49 -0700

    2240

---
+++ 2240/2240.py
@@ -0,0 +1,10 @@
+class Solution:
+    def number_of_ways_to_buy_pens_and_pencils(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
