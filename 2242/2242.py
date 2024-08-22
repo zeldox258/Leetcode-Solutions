d793349d9db7b260b24fdfc05b4361965ff345d5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-21 20:16:18 -0700

    2242

---
+++ 2242/2242.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_score_of_a_node_sequence(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
