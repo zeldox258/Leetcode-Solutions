09f9fbecaf92bf9c4a512d009b82d8cc98616b1f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-23 18:30:23 -0700

    2244

---
+++ 2244/2244.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_rounds_to_complete_all_tasks(self, n: int) -> int:
+        count = 0
+        while n:
+            count += n & 1
+            n >>= 1
+        return count
