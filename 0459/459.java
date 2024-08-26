61ea18709162adf069530893dd84f05f3c901b11
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-26 12:00:00 -0700

    459

---
+++ 0459/459.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String sub = doubled.substring(1, doubled.length() - 1);
+        return sub.contains(s);
+    }
+}
