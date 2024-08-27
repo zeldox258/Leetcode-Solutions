b6e8cb9cb581012554bba4919ee990c038246d2c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-26 20:04:35 -0700

    2247

---
+++ 2247/2247.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum_cost_of_trip_with_k_highways(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
