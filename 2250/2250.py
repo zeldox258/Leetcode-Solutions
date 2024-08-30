491e2f4628b10d7de2226fd87cb68b8e9218b1f2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-08-29 20:14:13 -0700

    2250

---
+++ 2250/2250.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_number_of_rectangles_containing_ea(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
