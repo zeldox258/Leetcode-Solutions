7d7178bf5224b164ee278921c2686d2a062b0c72
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-03 09:11:17 -0700

    3487

---
+++ 3487/3487.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_unique_subarray_sum_after_deleti(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
