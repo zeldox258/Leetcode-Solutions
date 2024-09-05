8029ec9a36db414ec5a6f7d685e10786ae39af8a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-05 14:36:42 -0700

    2257

---
+++ 2257/2257.py
@@ -0,0 +1,6 @@
+class Solution:
+    def count_unguarded_cells_in_the_grid(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
