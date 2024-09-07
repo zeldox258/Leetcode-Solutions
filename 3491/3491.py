9789161263200918ab3815ea05c1e57115f4f054
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-07 09:29:57 -0700

    3491

---
+++ 3491/3491.py
@@ -0,0 +1,8 @@
+class Solution:
+    def phone_number_prefix(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
