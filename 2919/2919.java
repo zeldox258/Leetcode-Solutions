dc86818c96c31d9ea0514cf6f510ffacb7e0393f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-07 13:46:13 -0700

    2919

---
+++ 2919/2919.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solve2919(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int x:nums){
+            pq.add(x);
+            if(pq.size()>k) pq.poll();
+        }
+        return pq.peek();
+    }
+}
