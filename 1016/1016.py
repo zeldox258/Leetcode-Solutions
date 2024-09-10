05a3ae53dc1d1eed6fa43221dcc610851bbc57cc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-09 22:53:33 -0700

    1016

---
+++ 1016/1016.py
@@ -0,0 +1,8 @@
+class Solution:
+    def binary_string_with_substrings_representi(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
