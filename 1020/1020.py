fe989e626339df2af30344f477b2a548024b1c5a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-13 15:06:29 -0700

    1020

---
+++ 1020/1020.py
@@ -0,0 +1,8 @@
+class Solution:
+    def number_of_enclaves(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
