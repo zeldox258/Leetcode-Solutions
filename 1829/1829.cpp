4f1440fa2a2ae49a1a7219eba41217b6b2d83804
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-13 22:37:37 -0700

    1829

---
+++ 1829/1829.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> maximumXorForEachQuery(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> path;
+        bt(nums,0,path);
+        return res;
+    }
+    void bt(vector<int>& nums,int s,vector<int>& path){
+        res.push_back(path);
+        for(int i=s;i<nums.size();i++){
+            if(i>s&&nums[i]==nums[i-1]) continue;
+            path.push_back(nums[i]);bt(nums,i+1,path);path.pop_back();
+        }
+    }
+};
