b5054740d077b316ab874f256898a87c8d1fd7c0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-16 08:35:13 -0700

    306

---
+++ 0306/306.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var additiveNumber = function(s) {
+    const set = new Set();
+    let l = 0, res = 0;
+    for (let r = 0; r < s.length; r++) {
+        while (set.has(s[r])) set.delete(s[l++]);
+        set.add(s[r]);
+        res = Math.max(res, r - l + 1);
+    }
+    return res;
+};
