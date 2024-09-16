06d78dea3f0fe3380fe0cc209baacfb8f637b957
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-16 15:30:37 -0700

    3500

---
+++ 3500/3500.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimum_cost_to_divide_array_into_subarr(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
