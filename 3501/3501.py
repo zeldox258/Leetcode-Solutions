d3fc0ea05bb76c2238dc7bdc3d93932d9833954a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-17 17:17:22 -0700

    3501

---
+++ 3501/3501.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximize_active_section_with_trade_ii(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
