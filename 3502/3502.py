6db939db23a3af5c122fcf43ecf958a426e5c7d5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-18 20:34:10 -0700

    3502

---
+++ 3502/3502.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_cost_to_reach_every_position(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
