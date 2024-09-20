f00cd4f5901915dfc2a8621a1361efbc9fea84b2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-20 12:00:00 -0700

    434

---
+++ 0434/434.java
@@ -0,0 +1,6 @@
+
+class Solution {
+    public int countSegments(String s) {
+        int cnt=0; for(int i=0;i<s.length();i++) if(s.charAt(i)!=' '&&(i==0||s.charAt(i-1)==' ')) cnt++; return cnt;
+    }
+}
