f2fe376264412c6d47c25d8d5b0b1bf891e8ae67
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-21 11:25:39 -0700

    2273

---
+++ 2273/2273.py
@@ -0,0 +1,9 @@
+class Solution:
+    def find_resultant_array_after_removing_anag(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
