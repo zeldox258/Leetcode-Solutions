fe6074a487482ec6544fb7d34f35a999234d80c3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-21 11:06:18 -0700

    127

---
+++ 0127/127.ts
@@ -0,0 +1,10 @@
+function wordLadder(head: ListNode | null): ListNode | null {
+    let prev: ListNode | null = null, cur = head;
+    while (cur) {
+        const next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
