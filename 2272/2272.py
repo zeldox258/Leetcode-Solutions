6ae6c3591f9442fe8aeac8dcdcbd6e6374e9c636
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-20 19:14:18 -0700

    2272

---
+++ 2272/2272.py
@@ -0,0 +1,6 @@
+class Solution:
+    def substring_with_largest_variance(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
