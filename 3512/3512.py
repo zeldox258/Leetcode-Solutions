0a4893ea4d8852deff59a516471818ae050dff12
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-28 19:43:50 -0700

    3512

---
+++ 3512/3512.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimum_operations_to_make_array_sum_div(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
