77dd562c738603cf591a2a7efce0d395e950813a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-29 22:59:54 -0700

    2281

---
+++ 2281/2281.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sum_of_total_strength_of_wizards(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
