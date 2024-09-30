97c8b85967a0213bb0d67014a1ad0a4d6d940745
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-09-30 09:02:26 -0700

    2282

---
+++ 2282/2282.py
@@ -0,0 +1,12 @@
+class Solution:
+    def number_of_people_that_can_be_seen_in_a_g(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
