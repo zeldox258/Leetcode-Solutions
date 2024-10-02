39069fc9fd39ecd0df15a475a8a328fd9ecbd7ec
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-02 15:40:12 -0700

    2284

---
+++ 2284/2284.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sender_with_largest_word_count(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
