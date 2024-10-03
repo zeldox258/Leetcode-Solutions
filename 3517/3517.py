705a34d811b6ea5f4fe0f27e4de6bb42af5fd13d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-03 16:25:10 -0700

    3517

---
+++ 3517/3517.py
@@ -0,0 +1,6 @@
+class Solution:
+    def smallest_palindromic_rearrangement_i(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
