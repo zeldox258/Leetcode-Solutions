c28621bb2f856e618169b92b7dab939f9d887e8d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-04 11:39:11 -0700

    2088

---
+++ 2088/2088.go
@@ -0,0 +1,11 @@
+func countFertilePyramidsInALand(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := countFertilePyramidsInALand(root.Left)
+	r := countFertilePyramidsInALand(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
