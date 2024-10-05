33eeef386341abd752fd9b3f333c7619840dc4cf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-05 12:55:35 -0700

    2287

---
+++ 2287/2287.py
@@ -0,0 +1,6 @@
+class Solution:
+    def rearrange_characters_to_make_target_stri(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
