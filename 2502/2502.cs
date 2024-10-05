2cdb0c3b4ea56adf5f231f64aad78e545d1add97
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-05 13:13:41 -0700

    2502

---
+++ 2502/2502.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int DesignMemoryAllocator(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => a[1] - b[1]);
+        int count = 1, end = intervals[0][1];
+        for (int i = 1; i < intervals.Length; i++) {
+            if (intervals[i][0] >= end) { count++; end = intervals[i][1]; }
+        }
+        return count;
+    }
+}
