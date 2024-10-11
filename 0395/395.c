b747a05aee1629c3aa2e0781397662c32560f7bc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-11 08:38:54 -0700

    395

---
+++ 0395/395.c
@@ -0,0 +1,13 @@
+bool longestSubstringWithAtLeastKRe(char* s) {
+    char st[10001];
+    int top = -1;
+    for (int i = 0; s[i]; i++) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') st[++top] = s[i];
+        else {
+            if (top < 0) return false;
+            char p = st[top--];
+            if (s[i] == ')' && p != '(' || s[i] == ']' && p != '[' || s[i] == '}' && p != '{') return false;
+        }
+    }
+    return top == -1;
+}
