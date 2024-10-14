db323c5d95127910e95c5fcf76e1015f9ee6a775
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-13 21:51:33 -0700

    2295

---
+++ 2295/2295.py
@@ -0,0 +1,8 @@
+class Solution:
+    def replace_elements_in_an_array(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
