68a227e09ba5bbd2487bd9897c39b360b9d7e5c7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-15 14:14:22 -0700

    3529

---
+++ 3529/3529.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_cells_in_overlapping_horizontal_an(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
