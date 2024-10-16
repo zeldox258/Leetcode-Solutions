a5d8c8cb255ac3f9273f24451d758481cc4560b2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-16 15:41:54 -0700

    1529

---
+++ 1529/1529.java
@@ -0,0 +1,15 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> solve1529(int[] nums) {
+        Arrays.sort(nums);
+        bt(nums,0,new ArrayList<>());
+        return res;
+    }
+    void bt(int[] nums,int s,List<Integer> path){
+        res.add(new ArrayList<>(path));
+        for(int i=s;i<nums.length;i++){
+            if(i>s&&nums[i]==nums[i-1]) continue;
+            path.add(nums[i]);bt(nums,i+1,path);path.remove(path.size()-1);
+        }
+    }
+}
---
+++ 1529/README.md
@@ -0,0 +1,5 @@
+# 1529. Minimum Suffix Flips
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/minimum-suffix-flips/)
