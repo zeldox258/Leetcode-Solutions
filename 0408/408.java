6f7add16aa18aca121bda186a718a5b3c91874c0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-19 12:00:00 -0700

    408

---
+++ 0408/408.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i=0,j=0;
+        while(i<word.length()&&j<abbr.length()){
+            if(Character.isDigit(abbr.charAt(j))){
+                if(abbr.charAt(j)=='0') return false;
+                int num=0; while(j<abbr.length()&&Character.isDigit(abbr.charAt(j))) num=num*10+(abbr.charAt(j++)-'0');
+                i+=num;
+            } else { if(word.charAt(i++)!=abbr.charAt(j++)) return false; }
+        }
+        return i==word.length()&&j==abbr.length();
+    }
+}
