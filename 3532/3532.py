1be1eab6c2ac449087d9b2882e2340ed12702f7c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-18 18:00:02 -0700

    3532

---
+++ 3532/3532.py
@@ -0,0 +1,6 @@
+class Solution:
+    def path_existence_queries_in_a_graph_i(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
