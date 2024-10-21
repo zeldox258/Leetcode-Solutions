a495cbb5722b95b9e52b01557afe126dc1d05eb3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-20 22:40:28 -0700

    156

---
+++ 0156/156.ts
@@ -0,0 +1,10 @@
+function binaryTreeUpsideDown(s: string): number {
+    const set = new Set<string>();
+    let l = 0, res = 0;
+    for (let r = 0; r < s.length; r++) {
+        while (set.has(s[r])) set.delete(s[l++]);
+        set.add(s[r]);
+        res = Math.max(res, r - l + 1);
+    }
+    return res;
+}
