dc2e13a7ee67a19bb3e6536c461a1873df20a7b6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-26 20:16:16 -0700

    3540

---
+++ 3540/3540.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_time_to_visit_all_houses(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
