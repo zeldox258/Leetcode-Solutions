48ffeb8a435a0625e8e4b7056a1fb34519a1269f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-28 18:37:37 -0700

    2310

---
+++ 2310/2310.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum_of_numbers_with_units_digit_k(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
