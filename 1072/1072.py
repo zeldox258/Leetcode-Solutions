092ce778d6a2a53a20ea0b49038533ab4a81a826
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-10-30 22:29:53 -0700

    1072

---
+++ 1072/1072.py
@@ -0,0 +1,6 @@
+class Solution:
+    def flip_columns_for_maximum_number_of_equal(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
