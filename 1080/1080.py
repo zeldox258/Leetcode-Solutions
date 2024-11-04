f11c15256fadbd7020e00f11761eaad0ab9053a0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-04 09:33:00 -0800

    1080

---
+++ 1080/1080.py
@@ -0,0 +1,8 @@
+class Solution:
+    def insufficient_nodes_in_root_to_leaf_paths(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
