2bd52bf0ff5bbed97066bf78ff556023618f11be
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-04 15:40:23 -0800

    2317

---
+++ 2317/2317.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_xor_after_operations(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
