24dd587dee77bb95e73a65bbf31d68f2447b3d6f
zeldox258 <hsynmrgry@gmail.com>
2024-11-04 16:49:38 -0800

    2914

---
+++ 2914/2914.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minChanges(String s) {
+        int cnt = 0;
+        for(int i=0; i<s.length(); i+=2){
+            if(s.charAt(i)!=s.charAt(i+1)) cnt++;
+        }
+        return cnt;
+    }
+
+}
