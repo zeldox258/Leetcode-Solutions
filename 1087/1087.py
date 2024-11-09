069ae1e170e1a03c21889c3e11880bc1e8131129
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-08 18:05:10 -0800

    1087

---
+++ 1087/1087.py
@@ -0,0 +1,6 @@
+class Solution:
+    def brace_expansion(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
