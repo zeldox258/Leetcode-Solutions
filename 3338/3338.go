f10343063cf42ac9fe0acf05ae903bad6507f535
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-13 14:27:47 -0800

    3338

---
+++ 3338/3338.go
@@ -0,0 +1,10 @@
+func secondHighestSalaryIi(nums []int, k int) int {
+	m := map[int]int{0: 1}
+	sum, count := 0, 0
+	for _, x := range nums {
+		sum += x
+		count += m[sum-k]
+		m[sum]++
+	}
+	return count
+}
