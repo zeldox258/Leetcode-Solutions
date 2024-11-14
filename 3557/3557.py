6158dc3f6c6edaf1c8d71b308e3f147daabdb700
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-13 20:35:53 -0800

    3557

---
+++ 3557/3557.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find_maximum_number_of_non_intersecting_(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
