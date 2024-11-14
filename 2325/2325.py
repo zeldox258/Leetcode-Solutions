29bad30f9beb28652a069e79702ae9d6e859f881
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-13 18:25:10 -0800

    2325

---
+++ 2325/2325.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decode_the_message(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
