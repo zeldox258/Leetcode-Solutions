bdf86ef27183df91059f0ff05759590c053bc07d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-16 12:00:00 -0800

    404

---
+++ 0404/404.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null) return 0;
+        int res=0;
+        if(root.left!=null&&root.left.left==null&&root.left.right==null) res+=root.left.val;
+        else res+=sumOfLeftLeaves(root.left);
+        res+=sumOfLeftLeaves(root.right);
+        return res;
+    }
+}
