b814c4192d3301dcc47038016f343bd82f76a9e1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-17 11:23:34 -0800

    2329

---
+++ 2329/2329.py
@@ -0,0 +1,7 @@
+class Solution:
+    def product_sales_analysis_v(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
