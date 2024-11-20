6df62eb5c2f1eef80f6c2805a73ab12ed3e3ee44
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-20 14:09:41 -0800

    1100

---
+++ 1100/1100.py
@@ -0,0 +1,10 @@
+class Solution:
+    def find_klength_substrings_with_no_repeated(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
