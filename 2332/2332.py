d193e204d2c873202f3689e98a12b849b5084611
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-20 21:06:50 -0800

    2332

---
+++ 2332/2332.py
@@ -0,0 +1,6 @@
+class Solution:
+    def the_latest_time_to_catch_a_bus(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
