e9d94112505818f25cd03960f33f2241106ac328
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-22 08:57:35 -0800

    1102

---
+++ 1102/1102.py
@@ -0,0 +1,6 @@
+class Solution:
+    def path_with_maximum_minimum_value(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
