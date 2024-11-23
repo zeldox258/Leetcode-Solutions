2c219e3acea3ed84ce74156a26f5190a57070363
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-22 17:33:40 -0800

    3566

---
+++ 3566/3566.py
@@ -0,0 +1,8 @@
+class Solution:
+    def partition_array_into_two_equal_product_s(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
