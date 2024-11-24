beef4ac1aa7b2cdd782143453b18f5b78e971305
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-24 12:31:47 -0800

    2336

---
+++ 2336/2336.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallest_number_in_infinite_set(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
