cce68320389cecbdef6d640f7c6ba9e2e221d5d5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-11-28 11:23:12 -0800

    2340

---
+++ 2340/2340.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_adjacent_swaps_to_make_a_valid_a(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
