15f8e2995cca75e3392f7e3b28218ce0e5011fd2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-02 08:26:45 -0800

    3577

---
+++ 3577/3577.py
@@ -0,0 +1,6 @@
+class Solution:
+    def count_the_number_of_computer_unlocking_p(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
