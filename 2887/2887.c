78660e86e2d1ad7461c2b70a5c1bf60b7d8de0c7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-04 08:11:44 -0800

    2887

---
+++ 2887/2887.c
@@ -0,0 +1,11 @@
+struct ListNode* fillMissingData(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* cur = head;
+    while (cur) {
+        struct ListNode* next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
