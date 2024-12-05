2de67bbafda88f9c2998a244130b5b9271bf54bf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-05 12:24:44 -0800

    2347

---
+++ 2347/2347.py
@@ -0,0 +1,6 @@
+class Solution:
+    def best_poker_hand(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
