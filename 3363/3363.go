90e5443171202530563c3db5bc62af03794f7e31
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-08 13:38:06 -0800

    3363

---
+++ 3363/3363.go
@@ -0,0 +1,11 @@
+func findTheMaximumNumberOfFruitsCo(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := findTheMaximumNumberOfFruitsCo(root.Left)
+	r := findTheMaximumNumberOfFruitsCo(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
