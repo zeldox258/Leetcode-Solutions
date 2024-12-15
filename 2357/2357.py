a7afb8eb2fb51ff6873cd1f6dd6586fa3fa703bd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-14 21:04:04 -0800

    2357

---
+++ 2357/2357.py
@@ -0,0 +1,12 @@
+class Solution:
+    def make_array_zero_by_subtracting_equal_amo(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
