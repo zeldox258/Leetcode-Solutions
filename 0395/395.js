28bd812d6764338185f9a03192e5f6fd272f560b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-15 12:04:03 -0800

    395

---
+++ 0395/395.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var longestSubstringWithAtLeastKRe = function(s) {
+    const st = [];
+    const pairs = {'(': ')', '[': ']', '{': '}'};
+    for (const c of s) {
+        if (pairs[c]) st.push(c);
+        else {
+            if (!st.length || pairs[st.pop()] !== c) return false;
+        }
+    }
+    return st.length === 0;
+};
