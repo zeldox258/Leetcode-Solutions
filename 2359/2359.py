11c1d13d6884a0a2111772ece12b5d6f4b3a6558
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-16 21:29:27 -0800

    2359

---
+++ 2359/2359.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_closest_node_to_given_two_nodes(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
