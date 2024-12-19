d00fbfc6709d7e7d48778ddc35801e16b451622c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-19 12:04:04 -0800

    2362

---
+++ 2362/2362.py
@@ -0,0 +1,6 @@
+class Solution:
+    def generate_the_invoice(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
