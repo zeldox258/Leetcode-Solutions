f055c065cb5912420032c324c8a0cf0674b6ae9d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-25 20:54:35 -0800

    3600

---
+++ 3600/3600.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximize_spanning_tree_stability_with_up(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
