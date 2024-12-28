72e1d93c0a385d708eace759cc040eba53759843
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-27 21:32:24 -0800

    3382

---
+++ 3382/3382.go
@@ -0,0 +1,11 @@
+func maximumAreaRectangleWithPointC(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
