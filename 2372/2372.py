46eb8be5dea7dedcc99b06fa53cd3ef4bcb91523
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-29 09:32:00 -0800

    2372

---
+++ 2372/2372.py
@@ -0,0 +1,12 @@
+class Solution:
+    def calculate_the_influence_of_each_salesper(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
