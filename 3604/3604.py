615d2a95c039eb70535ad3c3570bd5e50f65ac71
huseyinguray_microsoft <huseyinguray@microsoft.com>
2024-12-29 13:39:15 -0800

    3604

---
+++ 3604/3604.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_time_to_reach_destination_in_dir(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
