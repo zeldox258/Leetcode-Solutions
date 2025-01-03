a963051139a91caacd2d02640e06863783a1f870
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-03 13:11:48 -0800

    2377

---
+++ 2377/2377.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sort_the_olympic_table(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
