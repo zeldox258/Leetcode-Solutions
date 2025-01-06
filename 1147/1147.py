ac7f2bfb007541cf05f30ae1c2820e2912ebd921
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-06 15:21:57 -0800

    1147

---
+++ 1147/1147.py
@@ -0,0 +1,6 @@
+class Solution:
+    def longest_chunked_palindrome_decomposition(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
