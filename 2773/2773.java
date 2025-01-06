eebfeb45d30e350a0a7440fc6aebcd932c52982c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-06 13:11:22 -0800

    2773

---
+++ 2773/2773.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solve2773(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[n];
+        java.util.Arrays.fill(res,-1);
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&&nums[st.peek()]<nums[i]) res[st.pop()]=nums[i];
+            st.push(i);
+        }
+        return res;
+    }
+}
---
+++ 2773/README.md
@@ -0,0 +1,5 @@
+# 2773. Height of Special Binary Tree
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/height-of-special-binary-tree/)
