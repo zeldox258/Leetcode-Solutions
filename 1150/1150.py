cd8a5ff7bd9f54c1ebd034d1e3565d33c3fcba26
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-08 12:09:45 -0800

    1150

---
+++ 1150/1150.py
@@ -0,0 +1,15 @@
+class Solution:
+    def check_if_a_number_is_majority_element_in(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
