ad1402a5addaa003a9f5c2bc317004899090f692
zeldox258 <hsynmrgry@gmail.com>
2025-01-09 17:54:25 -0800

    116

---
+++ 0116/116..java
@@ -0,0 +1,21 @@
+class Solution {
+    public Node connect(Node root) {
+        if(root == null){
+            return root;
+        }
+
+        Node leftMost = root;
+        while(leftMost.left != null){
+            Node curr = leftMost;
+            while(curr != null){
+                curr.left.next = curr.right;
+                if(curr.next != null){
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+}
