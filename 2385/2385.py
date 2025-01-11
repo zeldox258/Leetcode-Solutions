a90d4dc7147e0c71d6c4751e6149c9f7a8135c9f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-11 13:33:42 -0800

    2385

---
+++ 2385/2385.py
@@ -0,0 +1,8 @@
+class Solution:
+    def amount_of_time_for_binary_tree_to_be_inf(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
