598ae935526cfd4058260de771a7b2f98f606dbe
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-13 13:42:58 -0800

    2387

---
+++ 2387/2387.py
@@ -0,0 +1,12 @@
+class Solution:
+    def median_of_a_row_wise_sorted_matrix(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
