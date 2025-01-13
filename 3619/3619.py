c34e1febe0f9ef8815eb70df6339fd7c3701aaa7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-13 14:04:56 -0800

    3619

---
+++ 3619/3619.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_islands_with_total_value_divisible(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
