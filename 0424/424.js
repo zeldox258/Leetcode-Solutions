da1ddac66aed80c182cbe2e77c183275089dfb21
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-13 21:10:04 -0800

    424

---
+++ 0424/424.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestRepeatingCharacterRepla = function(nums) {
+    let max = nums[0], cur = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        cur = Math.max(nums[i], cur + nums[i]);
+        max = Math.max(max, cur);
+    }
+    return max;
+};
