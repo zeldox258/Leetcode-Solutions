3f852d0dbdd42a220f1c9cff9f0c9a13b9ecf399
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-13 17:36:33 -0800

    1155

---
+++ 1155/1155.py
@@ -0,0 +1,8 @@
+class Solution:
+    def number_of_dice_rolls_with_target_sum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
