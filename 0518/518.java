3ffb5abe31c895be5a599f9e988ba76213d44440
zeldox258 <hsynmrgry@gmail.com>
2025-01-14 23:00:08 -0800

    518

---
+++ 0518/518.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int arr[] = new int[amount+1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        for(int c : coins)
+            for(int i = c; i <= amount; i++)
+                arr[i] += arr[i-c];
+        return arr[amount];
+    }
+}
