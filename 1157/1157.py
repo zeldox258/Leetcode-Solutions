dfee0399491747f9fdf71885544e9c5e68028095
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-15 22:15:39 -0800

    1157

---
+++ 1157/1157.py
@@ -0,0 +1,12 @@
+class Solution:
+    def online_majority_element_in_subarray(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
