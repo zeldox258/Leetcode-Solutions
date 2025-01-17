ddbc84906ebb668ab07f0a6daea81a1925a82ca1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-16 21:21:52 -0800

    2390

---
+++ 2390/2390.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removing_stars_from_a_string(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
