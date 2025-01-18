4761976743150923263a316d98ec4be237b91883
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-18 13:30:55 -0800

    1633

---
+++ 1633/1633.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var percentageOfUsersAttendedACont = function(nums, k) {
+    const sorted = [...nums].sort((a, b) => a - b);
+    return sorted[sorted.length - k];
+};
