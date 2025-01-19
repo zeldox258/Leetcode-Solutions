72d42d25829d1ee60b109e6b963f7f89bfa8532e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-18 16:25:54 -0800

    2392

---
+++ 2392/2392.py
@@ -0,0 +1,6 @@
+class Solution:
+    def build_a_matrix_with_conditions(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
