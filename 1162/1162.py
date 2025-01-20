7304cd9dd96f428a76f1356457a881b5c746545f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-20 08:06:27 -0800

    1162

---
+++ 1162/1162.py
@@ -0,0 +1,6 @@
+class Solution:
+    def as_far_from_land_as_possible(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
