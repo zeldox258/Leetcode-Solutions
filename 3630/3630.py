11763f8e1c2d81f12b4b47e117c4fea060a83c8a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-24 15:14:45 -0800

    3630

---
+++ 3630/3630.py
@@ -0,0 +1,8 @@
+class Solution:
+    def partition_array_for_maximum_xor_and_and(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
