06b292e103b7b618ebd07d278b41d52313b73886
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-25 12:36:21 -0800

    2399

---
+++ 2399/2399.py
@@ -0,0 +1,14 @@
+class Solution:
+    def check_distances_between_same_letters(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
