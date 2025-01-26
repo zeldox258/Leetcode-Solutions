b3e73547463036333746176bfb8841ffaff2f0bd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-26 14:56:02 -0800

    2400

---
+++ 2400/2400.py
@@ -0,0 +1,8 @@
+class Solution:
+    def number_of_ways_to_reach_a_position_after(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
