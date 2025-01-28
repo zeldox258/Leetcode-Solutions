5db87a416b03069cb8433de960516a989410cb18
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-28 11:43:09 -0800

    1170

---
+++ 1170/1170.py
@@ -0,0 +1,8 @@
+class Solution:
+    def compare_strings_by_frequency_of_the_smal(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
