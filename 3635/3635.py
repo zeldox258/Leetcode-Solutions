23a08892e0ea292953938493ab971dc7505f1db6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-29 16:13:55 -0800

    3635

---
+++ 3635/3635.py
@@ -0,0 +1,10 @@
+class Solution:
+    def earliest_finish_time_for_land_and_water_(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
