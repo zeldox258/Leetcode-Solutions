2c9483a804c1934fa28ce502b652a03143ae70ee
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-29 17:05:29 -0800

    1171

---
+++ 1171/1171.py
@@ -0,0 +1,10 @@
+class Solution:
+    def remove_zero_sum_consecutive_nodes_from_l(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
