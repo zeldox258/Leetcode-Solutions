85f9e9e205a2b427a7ef4da847c2ddd4f4cfa272
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-01-30 16:50:36 -0800

    1492

---
+++ 1492/1492.ts
@@ -0,0 +1,10 @@
+function theKthFactorOfN(head: ListNode | null): ListNode | null {
+    let prev: ListNode | null = null, cur = head;
+    while (cur) {
+        const next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
