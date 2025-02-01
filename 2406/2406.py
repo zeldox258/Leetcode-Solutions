50de30c45ebbc06b307b7e9e30558c20e013c8f3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-01 15:34:00 -0800

    2406

---
+++ 2406/2406.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divide_intervals_into_minimum_number_of_(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
