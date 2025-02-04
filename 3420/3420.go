38e08130c27c7c3a2eccf3226a182fbb9dfe8eba
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-03 17:41:50 -0800

    3420

---
+++ 3420/3420.go
@@ -0,0 +1,10 @@
+func countNondecreasingSubarraysAft(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, x := range nums {
+		if j, ok := m[target-x]; ok {
+			return []int{j, i}
+		}
+		m[x] = i
+	}
+	return nil
+}
