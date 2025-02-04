5cbc224794cb51bd137771f0f0beba04a12906a4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-04 10:47:46 -0800

    1177

---
+++ 1177/1177.py
@@ -0,0 +1,6 @@
+class Solution:
+    def can_make_palindrome_from_substring(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
