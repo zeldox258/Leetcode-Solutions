e23fbdb6b51664404fb22a3b87a018dd6dc15960
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-08 19:10:48 -0800

    1181

---
+++ 1181/1181.py
@@ -0,0 +1,8 @@
+class Solution:
+    def before_and_after_puzzle(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
