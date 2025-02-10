ccd8dc88fce80dc0fc8e8cefbe50cda4f57b0f68
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-09 18:54:30 -0800

    451

---
+++ 0451/451.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sortCharactersByFrequency = function(nums) {
+    let l = 0, r = nums.length - 1, res = 0;
+    while (l < r) {
+        res = Math.max(res, Math.min(nums[l], nums[r]) * (r - l));
+        if (nums[l] < nums[r]) l++; else r--;
+    }
+    return res;
+};
