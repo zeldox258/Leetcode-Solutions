d0fd7de3202ae44658b9083953dc6779e358149d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-10 17:53:41 -0800

    2415

---
+++ 2415/2415.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse_odd_levels_of_binary_tree(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
