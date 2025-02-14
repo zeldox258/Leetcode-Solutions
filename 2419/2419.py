529dce69121d5cd57a1520ee17f90cdd948bdc6b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-14 08:02:23 -0800

    2419

---
+++ 2419/2419.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longest_subarray_with_maximum_bitwise_an(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
