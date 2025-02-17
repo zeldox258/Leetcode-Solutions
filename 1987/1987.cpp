0119afdd1c748035857269e79035611a446d5465
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-17 08:44:59 -0800

    1987

---
+++ 1987/1987.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* numberOfUniqueGoodSubsequences(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* cur=head;
+        while(cur){
+            ListNode* next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+};
