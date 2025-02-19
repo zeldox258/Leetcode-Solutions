a25d882d6985af72aa0144ce0207265ae436afcc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-19 12:42:14 -0800

    1192

---
+++ 1192/1192.py
@@ -0,0 +1,6 @@
+class Solution:
+    def critical_connections_in_a_network(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
