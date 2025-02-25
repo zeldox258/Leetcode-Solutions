6e83e630263cccf4517589c97190208a655c9ee5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-25 08:02:54 -0800

    2430

---
+++ 2430/2430.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_deletions_on_a_string(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
