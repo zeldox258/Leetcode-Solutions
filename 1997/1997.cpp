aa5b23dc02701481fab19996057da9cdf69e6dce
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-27 13:13:40 -0800

    1997

---
+++ 1997/1997.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstDayWhereYouHaveBeenInAllT(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]<target) l=m+1; else r=m-1;
+        }
+        return -1;
+    }
+};
