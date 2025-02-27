452662c1875344519c9548c370b488653944ea4f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-27 15:18:26 -0800

    1200

---
+++ 1200/1200.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_absolute_difference(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
