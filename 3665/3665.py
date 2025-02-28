566d514e9df0a33b1fcf1f96a2b19b70727f2e77
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-02-28 14:13:03 -0800

    3665

---
+++ 3665/3665.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twisted_mirror_path_count(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
