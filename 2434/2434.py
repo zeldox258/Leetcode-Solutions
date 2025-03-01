c680c1b3de48ae233cbcbd2283f511c244c42b59
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-01 08:23:33 -0800

    2434

---
+++ 2434/2434.py
@@ -0,0 +1,7 @@
+class Solution:
+    def using_a_robot_to_print_the_lexicographic(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
