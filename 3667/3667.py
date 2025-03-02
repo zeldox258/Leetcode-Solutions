f292425dbf6b21a9e465d3d9b2cceae60b2e0c88
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-02 14:11:41 -0800

    3667

---
+++ 3667/3667.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sort_array_by_absolute_value(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
