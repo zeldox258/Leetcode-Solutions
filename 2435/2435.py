301c1d7e697690c58d10f9438b47b80911809598
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-02 21:13:37 -0800

    2435

---
+++ 2435/2435.py
@@ -0,0 +1,10 @@
+class Solution:
+    def paths_in_matrix_whose_sum_is_divisible_b(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
