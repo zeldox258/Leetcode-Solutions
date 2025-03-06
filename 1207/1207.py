51887caac9d99504cee68bb501a40535414d686a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-06 09:54:04 -0800

    1207

---
+++ 1207/1207.py
@@ -0,0 +1,6 @@
+class Solution:
+    def unique_number_of_occurrences(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
