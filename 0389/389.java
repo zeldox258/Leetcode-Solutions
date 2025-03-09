60a739ba08022edb1ba5dfe136888021e6242833
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-09 12:00:00 -0700

    389

---
+++ 0389/389.java
@@ -0,0 +1,6 @@
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char c=0; for(char ch:s.toCharArray())c^=ch; for(char ch:t.toCharArray())c^=ch; return c;
+    }
+}
