856886437d4f6daa193187e4b49f1e7b7bb66421
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-10 19:24:59 -0700

    1211

---
+++ 1211/1211.py
@@ -0,0 +1,8 @@
+class Solution:
+    def queries_quality_and_percentage(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
