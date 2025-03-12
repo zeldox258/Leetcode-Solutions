7bdfce0e83b13836e62b5f041d552f5136a86bd8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-11 22:09:36 -0700

    1446

---
+++ 1446/1446.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int ConsecutiveCharacters(string s) {
+        var set = new HashSet<char>();
+        int l = 0, res = 0;
+        for (int r = 0; r < s.Length; r++) {
+            while (set.Contains(s[r])) set.Remove(s[l++]);
+            set.Add(s[r]);
+            res = Math.Max(res, r - l + 1);
+        }
+        return res;
+    }
+}
