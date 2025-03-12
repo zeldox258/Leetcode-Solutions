0a05687595a38cb38c4001c11b76b9fd4757a898
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-11 22:16:23 -0700

    3457

---
+++ 3457/3457.go
@@ -0,0 +1,11 @@
+func eatPizzas(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
