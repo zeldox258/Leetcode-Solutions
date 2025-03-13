5abc813c4dd26533f98f776b1fb6ca4ab930abcc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-13 12:00:00 -0700

    387

---
+++ 0387/387.java
@@ -0,0 +1,9 @@
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] cnt=new int[26];
+        for(char c:s.toCharArray()) cnt[c-'a']++;
+        for(int i=0;i<s.length();i++) if(cnt[s.charAt(i)-'a']==1) return i;
+        return -1;
+    }
+}
