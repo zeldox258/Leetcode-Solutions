60abb50df023bdb135704808424ec3e3d7270c1e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-14 11:14:37 -0700

    2447

---
+++ 2447/2447.py
@@ -0,0 +1,12 @@
+class Solution:
+    def number_of_subarrays_with_gcd_equal_to_k(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
