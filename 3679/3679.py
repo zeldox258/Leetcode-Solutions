b2ea613d2909abe877415dcf33e322c7971cd254
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-14 17:59:33 -0700

    3679

---
+++ 3679/3679.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_discards_to_balance_inventory(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
