6c73fc9e829539986155ab95391b14973c379778
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-16 15:03:02 -0700

    1217

---
+++ 1217/1217.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimum_cost_to_move_chips_to_the_same_p(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
