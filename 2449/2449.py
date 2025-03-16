9c529357810cf4f4909f868359fa0bd658b27da9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-16 12:37:07 -0700

    2449

---
+++ 2449/2449.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_number_of_operations_to_make_arr(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
