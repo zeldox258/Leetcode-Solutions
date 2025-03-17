489d7aa67c1dcf4a2978b5e513a78c5303cd6faf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-17 12:42:46 -0700

    3682

---
+++ 3682/3682.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_index_sum_of_common_elements(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
