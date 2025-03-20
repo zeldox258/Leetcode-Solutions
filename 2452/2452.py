10651fda401d218187bfb3411d5738d6dd5513ef
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-19 18:15:35 -0700

    2452

---
+++ 2452/2452.py
@@ -0,0 +1,6 @@
+class Solution:
+    def words_within_two_edits_of_dictionary(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
