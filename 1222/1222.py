5a5315f3fa56941411758dbca9521e99038b14df
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-21 14:54:51 -0700

    1222

---
+++ 1222/1222.py
@@ -0,0 +1,6 @@
+class Solution:
+    def queens_that_can_attack_the_king(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
