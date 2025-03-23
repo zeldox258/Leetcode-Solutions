89471a8c59ac0b1582fc47162f31aa463ce55c48
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-23 14:24:11 -0700

    2456

---
+++ 2456/2456.py
@@ -0,0 +1,8 @@
+class Solution:
+    def most_popular_video_creator(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
