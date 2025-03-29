5bf089ae7f44d1e8342eaf4bd82c082aabb97159
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-03-29 14:51:07 -0700

    3694

---
+++ 3694/3694.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distinct_points_reachable_after_substrin(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
