fa6eab2cfb3786d2123289aebc328e886786a4c6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-05 16:52:14 -0700

    1237

---
+++ 1237/1237.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_positive_integer_solution_for_a_giv(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
