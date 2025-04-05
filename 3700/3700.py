edb741a6e8e9168785467ea775c94b38e3aec62f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-04 21:57:13 -0700

    3700

---
+++ 3700/3700.py
@@ -0,0 +1,15 @@
+class Solution:
+    def number_of_zigzag_arrays_ii(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
