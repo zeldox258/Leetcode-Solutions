b472ab3bbabdd5765c55fce0a38aa28a3045675e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-07 18:10:47 -0700

    2471

---
+++ 2471/2471.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_number_of_operations_to_sort_a_b(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
