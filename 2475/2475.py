589a1f5b5ff265892552425f613a560f07667eb1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-11 15:43:24 -0700

    2475

---
+++ 2475/2475.py
@@ -0,0 +1,8 @@
+class Solution:
+    def number_of_unequal_triplets_in_array(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
