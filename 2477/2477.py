1fdd0833287488c8acf25335f7a536fda53300fa
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-13 15:04:20 -0700

    2477

---
+++ 2477/2477.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimum_fuel_cost_to_report_to_the_capit(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
