b0a1365f0e4c44b7d759a22b95def85bee6d3491
zeldox258 <huseyinguray@microsoft.com>
2025-04-14 10:53:38 -0700

    3330

---
+++ 3330/3330.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int count=0;
+        for(int i=1;i<word.length();i++){
+            if(word.charAt(i)==word.charAt(i-1)) count++;
+        }
+        return count+1;
+    }
+}
