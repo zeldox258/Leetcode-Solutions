25c71cf461aa74ffbaec00ab8f0b42b95bc27cef
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-16 10:00:27 -0700

    2509

---
+++ 2509/2509.py
@@ -0,0 +1,7 @@
+class Solution:
+    def cycle_length_queries_in_a_tree(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
