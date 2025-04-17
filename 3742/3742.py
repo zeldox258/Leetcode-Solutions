e0ffd9af1b3319b7e78b23516b3f3f2edc3c3560
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-17 12:44:07 -0700

    3742

---
+++ 3742/3742.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_path_score_in_a_grid(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
