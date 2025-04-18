f3032d96cb44daa1fe772f45515deafb9803f5e5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-18 13:57:20 -0700

    1279

---
+++ 1279/1279.py
@@ -0,0 +1,7 @@
+class Solution:
+    def traffic_light_controlled_intersection(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
