0b041a32dfaefb6b4dda5bad9b97a002033b6130
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-19 09:41:19 -0700

    2512

---
+++ 2512/2512.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reward_top_k_students(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
