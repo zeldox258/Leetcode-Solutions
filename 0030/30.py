49f5b48cd0a8e59bcbc08aa2ce3e9ffe60f75873
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-19 18:04:23 -0700

    30

---
+++ 0030/30.py
@@ -0,0 +1,8 @@
+class Solution:
+    def substring_with_concatenation_of_all_word(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
