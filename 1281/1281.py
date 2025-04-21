f8c4ff11e208f9b0b6512ce3420a05dd691bd76d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-20 21:46:12 -0700

    1281

---
+++ 1281/1281.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subtract_the_product_and_sum_of_digits_o(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
