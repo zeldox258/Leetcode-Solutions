40ac16d7c00e88f57dbfc600a81db35c4034f227
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-23 16:16:55 -0700

    34

---
+++ 0034/34.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_first_and_last_position_of_element_(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
