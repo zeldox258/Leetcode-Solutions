14977ca1334a07fa6263c865646d9803a3231d6c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-23 18:54:30 -0700

    2516

---
+++ 2516/2516.py
@@ -0,0 +1,8 @@
+class Solution:
+    def take_k_of_each_character_from_left_and_r(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
