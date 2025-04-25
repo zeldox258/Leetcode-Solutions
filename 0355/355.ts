d29c32036b78762004b6e0f123b5982210dd8391
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-25 09:07:11 -0700

    355

---
+++ 0355/355.ts
@@ -0,0 +1,10 @@
+function designTwitter(n: number, edges: number[][]): number {
+    const parent = Array.from({length: n}, (_, i) => i);
+    const find = (x: number): number => parent[x] === x ? x : (parent[x] = find(parent[x]));
+    let comp = n;
+    for (const [a, b] of edges) {
+        const ra = find(a), rb = find(b);
+        if (ra !== rb) { parent[ra] = rb; comp--; }
+    }
+    return comp;
+}
