4401eee1af35514f75fcdc37252aa81ebe022321
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-25 14:53:03 -0700

    3750

---
+++ 3750/3750.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_number_of_flips_to_reverse_binar(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
