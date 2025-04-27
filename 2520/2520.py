f2bf7a0874e41d19b44f79cc85e59ea3895dfce2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-27 12:32:47 -0700

    2520

---
+++ 2520/2520.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_the_digits_that_divide_a_number(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
