710584bf702b2553528077e29b9e04a47637bc8c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-26 22:22:10 -0700

    1287

---
+++ 1287/1287.py
@@ -0,0 +1,9 @@
+class Solution:
+    def element_appearing_more_than_25_in_sorted(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
