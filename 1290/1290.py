5bc3af8a7007fa639324a5f22c0d5c5a02237ea8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-29 16:18:11 -0700

    1290

---
+++ 1290/1290.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convert_binary_number_in_a_linked_list_t(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
