82cbec3f7da3e0f31816eca1c647232888de2bc5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-04-30 15:00:56 -0700

    41

---
+++ 0041/41.py
@@ -0,0 +1,8 @@
+class Solution:
+    def first_missing_positive(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
