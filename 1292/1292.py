4945093673f676bb7529334265d229f0d203d53f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-01 08:43:31 -0700

    1292

---
+++ 1292/1292.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum_side_length_of_a_square_with_sum(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
