acf780ce5a8f7cb3a406db9675758cedeb824782
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-01 10:57:15 -0700

    2524

---
+++ 2524/2524.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum_frequency_score_of_a_subarray(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
