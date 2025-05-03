41cca3148b623e02c8177a6f6cfd6a8490dcebb3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-03 08:06:43 -0700

    1294

---
+++ 1294/1294.py
@@ -0,0 +1,7 @@
+class Solution:
+    def weather_type_in_each_country(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
