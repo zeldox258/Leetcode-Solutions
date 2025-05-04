c0c46eb707ef67005ae8181858506d2bcb158a4c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-04 11:36:11 -0700

    45

---
+++ 0045/45.py
@@ -0,0 +1,8 @@
+class Solution:
+    def jump_game_ii(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
