9afe0769b9e75fed28a62d976bf6b20633254abf
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-04 22:41:32 -0700

    2527

---
+++ 2527/2527.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_xorbeauty_of_array(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
