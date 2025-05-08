ea7221f4edde8527e63521a2b95ad690087b5d84
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-08 16:03:28 -0700

    1087

---
+++ 1087/1087.go
@@ -0,0 +1,11 @@
+func braceExpansion(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
