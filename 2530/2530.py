2d5bb779866e30387594af3f169374bf4e6e87e4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-07 20:20:58 -0700

    2530

---
+++ 2530/2530.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximal_score_after_applying_k_operation(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
