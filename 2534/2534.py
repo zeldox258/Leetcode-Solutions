4f59ee3e0fc95208a9c6650f6aba4ba6b6b0d30c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-11 10:41:05 -0700

    2534

---
+++ 2534/2534.py
@@ -0,0 +1,14 @@
+class Solution:
+    def time_taken_to_cross_the_door(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
