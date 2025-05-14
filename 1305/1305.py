0a91d4acbf5d1006b94f063bbf98d94df7efaa58
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-14 08:10:07 -0700

    1305

---
+++ 1305/1305.py
@@ -0,0 +1,8 @@
+class Solution:
+    def all_elements_in_two_binary_search_trees(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
