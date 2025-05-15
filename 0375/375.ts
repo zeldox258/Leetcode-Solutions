8bda6d04914fee66f29de037c62eb9357a3343f1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-15 08:39:44 -0700

    375

---
+++ 0375/375.ts
@@ -0,0 +1,8 @@
+function guessNumberHigherOrLowerIi(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) return [map.get(target - nums[i])!, i];
+        map.set(nums[i], i);
+    }
+    return [];
+}
