234275aead35f04a6bcfeea2e64a55d67efb2ee5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-15 08:48:56 -0700

    56

---
+++ 0056/56.py
@@ -0,0 +1,8 @@
+class Solution:
+    def merge_intervals(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
