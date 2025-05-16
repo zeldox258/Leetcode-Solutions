960c7360e43421acfb8895f7448e758380e75b17
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-16 12:34:25 -0700

    2539

---
+++ 2539/2539.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_the_number_of_good_subsequences(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
