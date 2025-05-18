d875195f60bdb0ee946771c15b0923ef7bb0b07d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-18 16:26:53 -0700

    1309

---
+++ 1309/1309.py
@@ -0,0 +1,7 @@
+class Solution:
+    def decrypt_string_from_alphabet_to_integer_(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
