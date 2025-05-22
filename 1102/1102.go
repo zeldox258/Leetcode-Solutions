f35dff1724776263299f463f5c610dbb03a84ac0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-21 19:16:00 -0700

    1102

---
+++ 1102/1102.go
@@ -0,0 +1,11 @@
+func pathWithMaximumMinimumValue(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
