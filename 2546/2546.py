edcdc0b38eda3008bf5225d5250541874c561b85
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-23 10:51:10 -0700

    2546

---
+++ 2546/2546.py
@@ -0,0 +1,8 @@
+class Solution:
+    def apply_bitwise_operations_to_make_strings(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
