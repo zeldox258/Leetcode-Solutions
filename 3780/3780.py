dc36a80a3cd4de5e06053c89466d71586194f718
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-25 19:18:29 -0700

    3780

---
+++ 3780/3780.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_sum_of_three_numbers_divisible_b(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
