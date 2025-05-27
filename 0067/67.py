58de80491075f1f2669831615657ada94aeb5a45
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-26 19:43:39 -0700

    67

---
+++ 0067/67.py
@@ -0,0 +1,6 @@
+class Solution:
+    def add_binary(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
