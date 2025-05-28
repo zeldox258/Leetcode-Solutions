690c53a5dbcf7affb8e1d9b512fdbe83c0537896
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-28 14:22:34 -0700

    1205

---
+++ 1205/1205.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solve1205(TreeNode root) {
+        if(root==null) return 0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);int depth=0;
+        while(!q.isEmpty()){
+            int sz=q.size();depth++;
+            for(int i=0;i<sz;i++){
+                TreeNode n=q.poll();
+                if(n.left!=null) q.add(n.left);
+                if(n.right!=null) q.add(n.right);
+            }
+        }
+        return depth;
+    }
+}
