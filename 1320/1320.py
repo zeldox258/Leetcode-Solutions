3e35c1b4d7657907889bf2ae67ff22944c710585
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-05-29 18:44:55 -0700

    1320

---
+++ 1320/1320.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_distance_to_type_a_word_using_tw(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
