a6eaa4a69666cfd6a91628558eb1c832a129f695
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-03 14:55:18 -0700

    2557

---
+++ 2557/2557.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_number_of_integers_to_choose_fro(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
