663efd704d167b08e0466ac03940f5b335ec0ecd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-05 10:27:20 -0700

    1327

---
+++ 1327/1327.py
@@ -0,0 +1,6 @@
+class Solution:
+    def list_the_products_ordered_in_a_period(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
