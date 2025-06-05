ebb8a3ea225161442935afd70e7b2fa6b229bd53
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-04 18:57:32 -0700

    2558

---
+++ 2558/2558.py
@@ -0,0 +1,9 @@
+class Solution:
+    def take_gifts_from_the_richest_pile(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
