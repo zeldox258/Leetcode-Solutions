589ca02783ffa8fd2b6231f167c2c3b6c0a877ef
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-07 08:54:33 -0700

    398

---
+++ 0398/398.ts
@@ -0,0 +1,11 @@
+function randomPickIndex(nums: number[], k: number): number {
+    const map = new Map<number, number>();
+    map.set(0, 1);
+    let sum = 0, count = 0;
+    for (const x of nums) {
+        sum += x;
+        count += map.get(sum - k) || 0;
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+    return count;
+}
