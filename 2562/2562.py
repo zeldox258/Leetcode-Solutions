92892477e2cbbd48ae55e55076ebf1380b45c19e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-08 08:47:23 -0700

    2562

---
+++ 2562/2562.py
@@ -0,0 +1,9 @@
+class Solution:
+    def find_the_array_concatenation_value(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
