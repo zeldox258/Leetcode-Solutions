7b7fae8cce6b047d6598896493178051153f7196
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-08 22:35:13 -0700

    2749

---
+++ 2749/2749.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int MinimumOperationsToMakeTheInte(int[] nums) {
+        int max = nums[0], cur = nums[0];
+        for (int i = 1; i < nums.Length; i++) {
+            cur = Math.Max(nums[i], cur + nums[i]);
+            max = Math.Max(max, cur);
+        }
+        return max;
+    }
+}
