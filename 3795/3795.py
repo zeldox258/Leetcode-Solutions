a236077fcb33acbf09ce72ccba603eb5ea231e2b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-09 18:35:57 -0700

    3795

---
+++ 3795/3795.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_subarray_length_with_distinct_su(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
