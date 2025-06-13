14e0b0b308633fb962b6a2dbe472ec4beb34999c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-13 15:14:58 -0700

    1335

---
+++ 1335/1335.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_difficulty_of_a_job_schedule(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
