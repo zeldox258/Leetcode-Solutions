ed2c2e11fc554657dd4b0e47995f2fddb1a25c42
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-14 16:19:49 -0700

    3079

---
+++ 3079/3079.c
@@ -0,0 +1,8 @@
+int findTheSumOfEncryptedIntegers(int* nums, int numsSize) {
+    int cur = nums[0], mx = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        cur = nums[i] > cur + nums[i] ? nums[i] : cur + nums[i];
+        if (cur > mx) mx = cur;
+    }
+    return mx;
+}
