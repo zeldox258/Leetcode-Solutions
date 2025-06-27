c877c3e24d5dd8e9eb2cef08a61462716d8b8307
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-26 18:10:21 -0700

    417

---
+++ 0417/417.ts
@@ -0,0 +1,8 @@
+function pacificAtlanticWaterFlow(intervals: number[][]): number {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let count = 1, end = intervals[0][1];
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] >= end) { count++; end = intervals[i][1]; }
+    }
+    return count;
+}
