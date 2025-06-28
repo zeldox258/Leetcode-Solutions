d6ee76e806334a413294866d32099b7c8cc83436
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-28 15:26:18 -0700

    2105

---
+++ 2105/2105.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solve2105(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(Character.isLetterOrDigit(c)) sb.append(Character.toLowerCase(c));
+        }
+        String t=sb.toString();
+        return t.equals(sb.reverse().toString())?"true":"false";
+    }
+}
---
+++ 2105/README.md
@@ -0,0 +1,5 @@
+# 2105. Watering Plants II
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/watering-plants-ii/)
