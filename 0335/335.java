c541c56100247635eaf97f3eedec56cfd7f9ba5b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-06-30 12:00:00 -0700

    335

---
+++ 0335/335.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+        int n=distance.length;
+        for (int i=3;i<n;i++) {
+            if (distance[i]>=distance[i-2]&&distance[i-1]<=distance[i-3]) return true;
+            if (i>=4&&distance[i-1]==distance[i-3]&&distance[i]+distance[i-4]>=distance[i-2]) return true;
+            if (i>=5&&distance[i-2]>=distance[i-4]&&distance[i]+distance[i-4]>=distance[i-2]&&distance[i-1]<=distance[i-3]&&distance[i-1]+distance[i-5]>=distance[i-3]) return true;
+        }
+        return false;
+    }
+}
