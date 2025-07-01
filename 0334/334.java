ea505347bc04fa6bedee0da17212b5310ed551ff
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-01 12:00:00 -0700

    334

---
+++ 0334/334.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first=Integer.MAX_VALUE, second=Integer.MAX_VALUE;
+        for (int n:nums) {
+            if (n<=first) first=n;
+            else if (n<=second) second=n;
+            else return true;
+        }
+        return false;
+    }
+}
