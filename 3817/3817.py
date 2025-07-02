0742077bd0be3c9c1d1cc8038de933969126baed
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-01 22:54:01 -0700

    3817

---
+++ 3817/3817.py
@@ -0,0 +1,6 @@
+class Solution:
+    def good_indices_in_a_digit_string(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
