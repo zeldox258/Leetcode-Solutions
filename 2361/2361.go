5dc3e998ca2e2005ae6685fed356eb3b71789b48
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-03 15:55:24 -0700

    2361

---
+++ 2361/2361.go
@@ -0,0 +1,15 @@
+func minimumCostsUsingTheTrainLine(s string) int {
+	seen := map[byte]bool{}
+	l, res := 0, 0
+	for r := 0; r < len(s); r++ {
+		for seen[s[r]] {
+			delete(seen, s[l])
+			l++
+		}
+		seen[s[r]] = true
+		if r-l+1 > res {
+			res = r - l + 1
+		}
+	}
+	return res
+}
