d24b6af8db1d75dc60efba993627097decbae436
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-04 10:07:45 -0700

    425

---
+++ 0425/425.ts
@@ -0,0 +1,11 @@
+function wordSquares(s: string): boolean {
+    const st: string[] = [];
+    const pairs: Record<string, string> = {'(': ')', '[': ']', '{': '}'};
+    for (const c of s) {
+        if (pairs[c]) st.push(c);
+        else {
+            if (!st.length || pairs[st.pop()!] !== c) return false;
+        }
+    }
+    return st.length === 0;
+}
