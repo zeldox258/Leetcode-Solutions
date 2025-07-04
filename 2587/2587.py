ae7faa90b2afbe9bc5b1863672b36e22ac035213
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-03 22:48:12 -0700

    2587

---
+++ 2587/2587.py
@@ -0,0 +1,6 @@
+class Solution:
+    def rearrange_array_to_maximize_prefix_score(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
