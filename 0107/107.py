a2fe8bf75fa9be8220e914c4f3fbc0c72d3090ae
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-05 11:22:57 -0700

    107

---
+++ 0107/107.py
@@ -0,0 +1,12 @@
+class Solution:
+    def binary_tree_level_order_traversal_ii(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
