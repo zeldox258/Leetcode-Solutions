f97eafb1e80c4a2d8c60e379e36b6d0246d5c68c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-05 16:16:10 -0700

    1357

---
+++ 1357/1357.py
@@ -0,0 +1,6 @@
+class Solution:
+    def apply_discount_every_n_orders(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
