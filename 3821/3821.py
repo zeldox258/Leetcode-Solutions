c7f81975fb5d986eef3cb664baea7a48f557c78c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-05 19:59:57 -0700

    3821

---
+++ 3821/3821.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_nth_smallest_integer_with_k_one_bit(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
