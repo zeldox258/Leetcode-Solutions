6f5e23788979181fe115f566650519afd7e50a3c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-07 14:34:44 -0700

    109

---
+++ 0109/109.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convert_sorted_list_to_binary_search_tre(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
