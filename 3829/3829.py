8f9cd498936721a0d0297642e2a8545577a2cc84
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-13 08:23:38 -0700

    3829

---
+++ 3829/3829.py
@@ -0,0 +1,7 @@
+class Solution:
+    def design_ride_sharing_system(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
