6beb36a0bcba6024f18ba1cc7e8a3f89ba19367a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-13 12:00:00 -0700

    322

---
+++ 0322/322.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for (int i=1;i<=amount;i++)
+            for (int c:coins) if (c<=i) dp[i]=Math.min(dp[i],dp[i-c]+1);
+        return dp[amount]>amount?-1:dp[amount];
+    }
+}
