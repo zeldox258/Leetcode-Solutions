f8f717feba67100365053b09953fd0e8d8307cc1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-16 10:39:57 -0700

    3832

---
+++ 3832/3832.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_users_with_persistent_behavior_patt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
