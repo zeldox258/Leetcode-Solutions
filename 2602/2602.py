d6c852e8dc7b070cb55a08254515ccfc822deed4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-18 15:08:03 -0700

    2602

---
+++ 2602/2602.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_operations_to_make_all_array_ele(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
