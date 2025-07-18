ecd00dca9293d2e670e50c01512c2b51af6f64f4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-17 17:26:34 -0700

    1369

---
+++ 1369/1369.py
@@ -0,0 +1,7 @@
+class Solution:
+    def get_the_second_most_recent_activity(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
