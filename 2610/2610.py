6ba56d5fda79ee328afca6bad8da2c016f899079
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-26 15:56:07 -0700

    2610

---
+++ 2610/2610.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convert_an_array_into_a_2d_array_with_co(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
