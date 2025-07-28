19d9de855c0e758487bd78428154d18993911f05
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-27 19:32:38 -0700

    1379

---
+++ 1379/1379.py
@@ -0,0 +1,14 @@
+class Solution:
+    def find_a_corresponding_node_of_a_binary_tr(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
