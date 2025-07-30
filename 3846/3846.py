4f486093c0a711ecd975878cb26b250b26e13801
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-30 15:10:24 -0700

    3846

---
+++ 3846/3846.py
@@ -0,0 +1,11 @@
+class Solution:
+    def total_distance_to_type_a_string_using_on(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
