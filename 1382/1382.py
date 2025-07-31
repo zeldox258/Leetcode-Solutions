9126b9225076226a002a02a10329897d31ebbbed
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-07-30 22:14:44 -0700

    1382

---
+++ 1382/1382.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balance_a_binary_search_tree(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
