bffa6abc985be5952a488e85970ff32b9a7de15d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-01 11:14:57 -0700

    1384

---
+++ 1384/1384.py
@@ -0,0 +1,7 @@
+class Solution:
+    def total_sales_amount_by_year(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
