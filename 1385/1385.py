5bd5fb2a91d39486642347031c8924557b51a6a2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-02 12:57:02 -0700

    1385

---
+++ 1385/1385.py
@@ -0,0 +1,10 @@
+class Solution:
+    def find_the_distance_value_between_two_arra(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
