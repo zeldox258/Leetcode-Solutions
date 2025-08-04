df856f1a8f8ecda516a0b1f0c6d32d1020c3b310
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-04 11:20:45 -0700

    1387

---
+++ 1387/1387.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sort_integers_by_the_power_value(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
