84d2daa157812a5fdb64ddd85ae1d20016a579cb
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-15 10:13:02 -0700

    3862

---
+++ 3862/3862.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_the_smallest_balanced_index(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
