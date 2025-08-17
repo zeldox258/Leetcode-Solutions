2beef82343b6547cad0b11472b30c7f782738185
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-16 20:44:56 -0700

    3863

---
+++ 3863/3863.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimum_operations_to_sort_a_string(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
