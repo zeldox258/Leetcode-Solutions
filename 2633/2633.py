dfd96cf9617fb18f67105115f8c21bb863a1a857
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-18 17:53:53 -0700

    2633

---
+++ 2633/2633.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convert_object_to_json_string(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
