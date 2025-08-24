1a8642f008466fa1a35db47faec917ab26e14f17
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-24 12:55:58 -0700

    157

---
+++ 0157/157.py
@@ -0,0 +1,6 @@
+class Solution:
+    def read_n_characters_given_read4(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
