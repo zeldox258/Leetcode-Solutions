1d37d091e7421cdc425cdb93ee84e56a20f21d3d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-24 12:00:00 -0700

    280

---
+++ 0280/280.java
@@ -0,0 +1,7 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            if((i%2==1&&nums[i]<nums[i-1])||(i%2==0&&nums[i]>nums[i-1])){int t=nums[i];nums[i]=nums[i-1];nums[i-1]=t;}
+        }
+    }
+}
