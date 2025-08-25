9c09163bc25068edf526c716ec7147424916ef68
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-25 13:24:40 -0700

    2640

---
+++ 2640/2640.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_the_score_of_all_prefixes_of_an_arr(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
