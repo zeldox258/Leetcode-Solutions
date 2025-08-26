d77f8287646cffe136d64de22f3d2e824f2f98fd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-26 12:00:00 -0700

    278

---
+++ 0278/278.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int firstBadVersion(int n) {
+        int l=1,r=n;
+        while(l<r){int m=l+(r-l)/2;if(isBadVersion(m))r=m;else l=m+1;}
+        return l;
+    }
+    boolean isBadVersion(int v){return false;}
+}
