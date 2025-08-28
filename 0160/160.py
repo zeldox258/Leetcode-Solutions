730dc2be3890ceba2a0554f2bb6d43d20fb5a311
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-27 21:39:47 -0700

    160

---
+++ 0160/160.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intersection_of_two_linked_lists(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
