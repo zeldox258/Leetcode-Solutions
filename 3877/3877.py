5fb9cf6290efbd748a08a16de49e1c70bba259ce
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-30 08:39:31 -0700

    3877

---
+++ 3877/3877.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_removals_to_achieve_target_xor(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
