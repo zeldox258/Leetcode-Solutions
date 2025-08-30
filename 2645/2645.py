a2523769683a1250d9e856a9c8306101291f1147
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-30 14:32:50 -0700

    2645

---
+++ 2645/2645.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimum_additions_to_make_valid_string(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
