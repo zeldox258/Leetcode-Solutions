6f5f25248dd6daceecd1b43bd11251bca3157543
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-30 12:00:00 -0700

    274

---
+++ 0274/274.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);int n=citations.length;
+        for(int i=0;i<n;i++) if(citations[i]>=n-i) return n-i;
+        return 0;
+    }
+}
