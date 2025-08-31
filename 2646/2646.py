609ac4b3ea96bc2d9aebd56aceb99b19f564e718
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-31 08:58:07 -0700

    2646

---
+++ 2646/2646.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimize_the_total_price_of_the_trips(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
