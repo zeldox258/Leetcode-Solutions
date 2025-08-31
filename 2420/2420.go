6b1bd89de38f08aa91146526b6164b7530e180ef
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-08-31 15:11:37 -0700

    2420

---
+++ 2420/2420.go
@@ -0,0 +1,15 @@
+func findAllGoodIndices(s string) bool {
+	st := []rune{}
+	pairs := map[rune]rune{'(': ')', '[': ']', '{': '}'}
+	for _, c := range s {
+		if _, ok := pairs[c]; ok {
+			st = append(st, c)
+		} else {
+			if len(st) == 0 || pairs[st[len(st)-1]] != c {
+				return false
+			}
+			st = st[:len(st)-1]
+		}
+	}
+	return len(st) == 0
+}
