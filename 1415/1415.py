2f0c99ff22e4e2a3ff62b49d02767ab199bfc8a3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-01 14:33:42 -0700

    1415

---
+++ 1415/1415.py
@@ -0,0 +1,10 @@
+class Solution:
+    def the_kth_lexicographical_string_of_all_ha(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
