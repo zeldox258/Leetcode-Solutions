ef8296718c4912e64134ec8c3ad12950cf06da5d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-01 19:15:45 -0700

    2647

---
+++ 2647/2647.py
@@ -0,0 +1,6 @@
+class Solution:
+    def color_the_triangle_red(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
