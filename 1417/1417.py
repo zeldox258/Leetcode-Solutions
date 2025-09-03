a5b1e29860109e6e113ba4c08d306516ee1a39aa
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-03 08:41:29 -0700

    1417

---
+++ 1417/1417.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reformat_the_string(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
