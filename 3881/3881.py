c91b5f300d9ba70d3613b26b57021c30072971a9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-03 10:21:17 -0700

    3881

---
+++ 3881/3881.py
@@ -0,0 +1,8 @@
+class Solution:
+    def direction_assignments_with_exactly_k_vis(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
