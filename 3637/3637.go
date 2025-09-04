a0f63c12cbc602d835533e9e09e7d0c3f3b49bea
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-04 13:40:13 -0700

    3637

---
+++ 3637/3637.go
@@ -0,0 +1,11 @@
+func trionicArrayI(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
