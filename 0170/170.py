31273009c38bd6da03aa17363b8ca4e8ef5713be
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-06 22:52:16 -0700

    170

---
+++ 0170/170.py
@@ -0,0 +1,10 @@
+class Solution:
+    def two_sum_iii_data_structure_design(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
