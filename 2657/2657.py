ed1c4d6b1ac6bbbdaf5803d190e1e17b4b0a017c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-10 08:51:43 -0700

    2657

---
+++ 2657/2657.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find_the_prefix_common_array_of_two_arra(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
