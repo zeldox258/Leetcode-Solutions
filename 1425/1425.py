5ad7f02c71429d0573096502683048a558213374
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-11 13:55:15 -0700

    1425

---
+++ 1425/1425.py
@@ -0,0 +1,8 @@
+class Solution:
+    def constrained_subsequence_sum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
