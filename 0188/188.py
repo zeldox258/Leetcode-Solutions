7de008218075c5c0ff569bb3e3932628d57bde2e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-14 15:09:29 -0700

    188

---
+++ 0188/188.py
@@ -0,0 +1,9 @@
+class Solution:
+    def best_time_to_buy_and_sell_stock_iv(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
