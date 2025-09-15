4e822e04132c01b21bec06a9a372439c08286348
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-14 17:51:11 -0700

    3892

---
+++ 3892/3892.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minimum_operations_to_achieve_at_least_k(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
