a88535157c4f3a60c492db9db7a17103a84ce605
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-18 14:55:46 -0700

    3896

---
+++ 3896/3896.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimum_operations_to_transform_array_in(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
