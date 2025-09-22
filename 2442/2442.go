6819e0755fc6bd18b3d935f8b6e42c6cbbb40d11
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-22 12:12:59 -0700

    2442

---
+++ 2442/2442.go
@@ -0,0 +1,13 @@
+func countNumberOfDistinctIntegersA(intervals [][]int) int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][1] < intervals[j][1]
+	})
+	count, end := 1, intervals[0][1]
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] >= end {
+			count++
+			end = intervals[i][1]
+		}
+	}
+	return count
+}
