a2653b9a95258e3d5b11da50dcfcb40d3adffc93
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-23 11:12:46 -0700

    1437

---
+++ 1437/1437.py
@@ -0,0 +1,9 @@
+class Solution:
+    def check_if_all_1s_are_at_least_length_k_pl(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
