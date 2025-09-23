83c7855529de6b1a55c8fa4ad046fc55ae9bc572
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-23 14:06:04 -0700

    199

---
+++ 0199/199.py
@@ -0,0 +1,7 @@
+class Solution:
+    def binary_tree_right_side_view(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
