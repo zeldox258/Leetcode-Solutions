6741105f62609bdbe266030667802c9c2f7fa864
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-24 13:58:33 -0700

    2672

---
+++ 2672/2672.py
@@ -0,0 +1,12 @@
+class Solution:
+    def number_of_adjacent_elements_with_the_sam(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
