17bdf91eb646f7368cbe7c89c37e032ce240ab55
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-24 21:36:12 -0700

    200

---
+++ 0200/200.py
@@ -0,0 +1,10 @@
+class Solution:
+    def number_of_islands(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
