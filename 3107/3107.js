1b0b3f5ee1467ca74bb45ee1686213076ffe8889
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-26 19:51:09 -0700

    3107

---
+++ 3107/3107.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var minimumOperationsToMakeMedianO = function(nums, target) {
+    let l = 0, r = nums.length - 1;
+    while (l <= r) {
+        const m = Math.floor(l + (r - l) / 2);
+        if (nums[m] === target) return m;
+        if (nums[m] < target) l = m + 1; else r = m - 1;
+    }
+    return -1;
+};
