f99e151eaae53948d92d5f7acb969e2823c93322
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-27 12:00:00 -0700

    243

---
+++ 0243/243.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int min=Integer.MAX_VALUE,i1=-1,i2=-1;
+        for(int i=0;i<wordsDict.length;i++){
+            if(wordsDict[i].equals(word1))i1=i;else if(wordsDict[i].equals(word2))i2=i;
+            if(i1!=-1&&i2!=-1)min=Math.min(min,Math.abs(i1-i2));
+        }
+        return min;
+    }
+}
