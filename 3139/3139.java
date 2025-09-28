483726e15ed889a3508309ddc1f397ffb0852067
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-27 18:32:23 -0700

    3139

---
+++ 3139/3139.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solve3139(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++) dp[i]=Math.max(dp[i-1]+nums[i],nums[i]);
+        int max=dp[0];
+        for(int x:dp) max=Math.max(max,x);
+        return max;
+    }
+}
---
+++ 3139/README.md
@@ -0,0 +1,5 @@
+# 3139. Minimum Cost to Equalize Array
+
+**Difficulty:** Hard
+
+[LeetCode Link](https://leetcode.com/problems/minimum-cost-to-equalize-array/)
