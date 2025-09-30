916103f474c0586c63972ba373a352e16b228949
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-30 16:22:54 -0700

    2238

---
+++ 2238/2238.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solve2238(TreeNode root) {
+        if(root==null) return 0;
+        return 1+Math.max(solve2238(root.left),solve2238(root.right));
+    }
+}
---
+++ 2238/README.md
@@ -0,0 +1,5 @@
+# 2238. Number of Times a Driver Was a Passenger
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/number-of-times-a-driver-was-a-passenger/)
