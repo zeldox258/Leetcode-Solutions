45434e15cc9a61ed793a13e2aa05feac0b6a4b47
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-30 12:00:00 -0700

    239

---
+++ 0239/239.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;int[] res=new int[n-k+1];
+        Deque<Integer> dq=new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty()&&dq.peekFirst()<i-k+1) dq.pollFirst();
+            while(!dq.isEmpty()&&nums[dq.peekLast()]<nums[i]) dq.pollLast();
+            dq.addLast(i);
+            if(i>=k-1) res[i-k+1]=nums[dq.peekFirst()];
+        }
+        return res;
+    }
+}
