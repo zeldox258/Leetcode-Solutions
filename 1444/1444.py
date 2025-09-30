ef0e75990c156a68ad2af9d1205507333c72ed14
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-09-30 12:31:28 -0700

    1444

---
+++ 1444/1444.py
@@ -0,0 +1,7 @@
+class Solution:
+    def number_of_ways_of_cutting_a_pizza(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
