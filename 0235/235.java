7760970decf6edaf0394d6d1adbd7c5bb7cd8a02
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-03 12:00:00 -0700

    235

---
+++ 0235/235.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root!=null){
+            if(p.val<root.val&&q.val<root.val) root=root.left;
+            else if(p.val>root.val&&q.val>root.val) root=root.right;
+            else return root;
+        }
+        return null;
+    }
+}
