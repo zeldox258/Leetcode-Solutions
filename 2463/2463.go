8112c01e88f1fd909d4aabd76a8aaefcaf3861da
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-04 12:38:27 -0700

    2463

---
+++ 2463/2463.go
@@ -0,0 +1,11 @@
+func minimumTotalDistanceTraveled(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := minimumTotalDistanceTraveled(root.Left)
+	r := minimumTotalDistanceTraveled(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
