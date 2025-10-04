b3c46a30916e24775faac5e76ab9cb3072a39e86
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-03 18:22:04 -0700

    1447

---
+++ 1447/1447.py
@@ -0,0 +1,6 @@
+class Solution:
+    def simplified_fractions(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
