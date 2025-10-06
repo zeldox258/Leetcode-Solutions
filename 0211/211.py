d666eacaae5112de5012af8dcc8a0db7a9d42109
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-05 21:49:54 -0700

    211

---
+++ 0211/211.py
@@ -0,0 +1,10 @@
+class Solution:
+    def design_add_and_search_words_data_structu(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
