b93452411d4baf9cb73766f7b90f8b7896108243
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-06 08:17:35 -0700

    3678

---
+++ 3678/3678.go
@@ -0,0 +1,11 @@
+func smallestAbsentPositiveGreaterT(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := smallestAbsentPositiveGreaterT(root.Left)
+	r := smallestAbsentPositiveGreaterT(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
