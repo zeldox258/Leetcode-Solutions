dd9e0d37e71a997afc34f584f27d75f0519d22c3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-11 19:40:25 -0700

    1455

---
+++ 1455/1455.py
@@ -0,0 +1,8 @@
+class Solution:
+    def check_if_a_word_occurs_as_a_prefix_of_an(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
