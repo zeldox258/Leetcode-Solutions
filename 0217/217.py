c22fbf401f7ebe0f4f974b50e75d63540fd4a8fc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-11 22:32:26 -0700

    217

---
+++ 0217/217.py
@@ -0,0 +1,6 @@
+class Solution:
+    def contains_duplicate(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
