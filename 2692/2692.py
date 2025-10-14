e77d1cca30e60f9f7fca4b8d4dee8e2d08364fc7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-14 13:34:11 -0700

    2692

---
+++ 2692/2692.py
@@ -0,0 +1,6 @@
+class Solution:
+    def make_object_immutable(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
