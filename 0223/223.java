bfb675bf2c29ab0e786a54824a8214a0c468676b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-14 12:00:00 -0700

    223

---
+++ 0223/223.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int a=(ax2-ax1)*(ay2-ay1),b=(bx2-bx1)*(by2-by1);
+        int overlapX=Math.max(0,Math.min(ax2,bx2)-Math.max(ax1,bx1));
+        int overlapY=Math.max(0,Math.min(ay2,by2)-Math.max(ay1,by1));
+        return a+b-overlapX*overlapY;
+    }
+}
