31a63d19fe84727474d98efef14122e7d0cb396d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-16 09:06:20 -0700

    1460

---
+++ 1460/1460.py
@@ -0,0 +1,10 @@
+class Solution:
+    def make_two_arrays_equal_by_reversing_subar(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
