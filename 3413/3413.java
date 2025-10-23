101e8d51a321288bfbaee354f6bb8cea2b9c5d40
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-23 12:19:43 -0700

    3413

---
+++ 3413/3413.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void solve3413(int[][] matrix) {
+        int m=matrix.length,n=matrix[0].length;
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++)
+            if(matrix[i][j]==0){
+                for(int k=0;k<n;k++) if(matrix[i][k]!=0) matrix[i][k]=-1;
+                for(int k=0;k<m;k++) if(matrix[k][j]!=0) matrix[k][j]=-1;
+            }
+    }
+}
