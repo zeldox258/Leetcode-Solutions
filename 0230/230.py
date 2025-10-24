12548805ca102905d6cf49c55d66404009500061
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-24 08:45:48 -0700

    230

---
+++ 0230/230.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kth_smallest_element_in_a_bst(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
