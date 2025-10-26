44cb5d886a0c65f8f2179761ee6b1ad78ad09991
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-26 15:44:14 -0700

    232

---
+++ 0232/232.py
@@ -0,0 +1,6 @@
+class Solution:
+    def implement_queue_using_stacks(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
