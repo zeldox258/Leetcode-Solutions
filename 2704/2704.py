ab59faff4947204a9b61dbe47147a435b88295ac
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-26 19:51:08 -0700

    2704

---
+++ 2704/2704.py
@@ -0,0 +1,7 @@
+class Solution:
+    def to_be_or_not_to_be(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
