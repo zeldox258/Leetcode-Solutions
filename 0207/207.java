75b36a35490a43e078236916f8dc81a76c5ef285
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-30 12:00:00 -0700

    207

---
+++ 0207/207.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree=new int[numCourses];
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
+        for(int[] p:prerequisites){graph.get(p[1]).add(p[0]);indegree[p[0]]++;}
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++) if(indegree[i]==0) q.offer(i);
+        int count=0;
+        while(!q.isEmpty()){int c=q.poll();count++;for(int nb:graph.get(c)){if(--indegree[nb]==0)q.offer(nb);}}
+        return count==numCourses;
+    }
+}
