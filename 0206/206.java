da034aee287507fdc12c656f32a645af198a8a44
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-31 12:00:00 -0700

    206

---
+++ 0206/206.java
@@ -0,0 +1,7 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null,cur=head;
+        while(cur!=null){ListNode next=cur.next;cur.next=prev;prev=cur;cur=next;}
+        return prev;
+    }
+}
