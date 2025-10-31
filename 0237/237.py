dd875d17f7c4aa7f99b163ec15dad9a762473f6d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-31 13:12:49 -0700

    237

---
+++ 0237/237.py
@@ -0,0 +1,9 @@
+class Solution:
+    def delete_node_in_a_linked_list(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
