fe6bb06e897f5bf923730bf53adc2532937cef38
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-10-31 21:57:13 -0700

    1475

---
+++ 1475/1475.py
@@ -0,0 +1,10 @@
+class Solution:
+    def final_prices_with_a_special_discount_in_(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
