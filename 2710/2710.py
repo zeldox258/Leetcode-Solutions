d168f03fa9106a4aec907aa4fd870adb594b494d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-01 20:45:42 -0700

    2710

---
+++ 2710/2710.py
@@ -0,0 +1,15 @@
+class Solution:
+    def remove_trailing_zeros_from_a_string(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
