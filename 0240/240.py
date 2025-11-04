7525d34ea4da0c5de368598080627c16ca5a68c2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-03 22:37:21 -0800

    240

---
+++ 0240/240.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search_a_2d_matrix_ii(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
