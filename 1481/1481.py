49927377cea1c7158b8dd439c42316920c52c5ed
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-06 14:22:33 -0800

    1481

---
+++ 1481/1481.py
@@ -0,0 +1,8 @@
+class Solution:
+    def least_number_of_unique_integers_after_k_(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
