19b9a9e6980e8942d1d94c37eff9810af1189dc9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-08 12:00:00 -0800

    198

---
+++ 0198/198.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++) for(int j=0;j<grid[0].length;j++) if(grid[i][j]=='1'){count++;dfs(grid,i,j);}
+        return count;
+    }
+    void dfs(char[][] g,int i,int j){
+        if(i<0||i>=g.length||j<0||j>=g[0].length||g[i][j]!='1') return;
+        g[i][j]='0';dfs(g,i+1,j);dfs(g,i-1,j);dfs(g,i,j+1);dfs(g,i,j-1);
+    }
+}
