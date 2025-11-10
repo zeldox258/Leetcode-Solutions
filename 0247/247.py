f6fc524d1ee443a431ee980adca23443b10e567a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-10 12:14:11 -0800

    247

---
+++ 0247/247.py
@@ -0,0 +1,6 @@
+class Solution:
+    def strobogrammatic_number_ii(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
