6e205f6b54a687f707cf36570a1346723d47373d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-11 13:26:26 -0800

    248

---
+++ 0248/248.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strobogrammatic_number_iii(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
