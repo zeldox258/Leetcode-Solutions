f70dead378d4325b883d1defdcf164db05eb2c86
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-11 17:49:28 -0800

    2721

---
+++ 2721/2721.py
@@ -0,0 +1,11 @@
+class Solution:
+    def execute_asynchronous_functions_in_parall(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
