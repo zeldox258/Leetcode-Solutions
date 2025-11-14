e102bb990fb3c6035a3844f290530bdb0619ea24
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-14 12:13:54 -0800

    1489

---
+++ 1489/1489.py
@@ -0,0 +1,7 @@
+class Solution:
+    def find_critical_and_pseudocritical_edges_i(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
