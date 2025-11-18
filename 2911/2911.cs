392854b0364f3eb7f1678c6c07490c629b62cf9e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-17 22:19:47 -0800

    2911

---
+++ 2911/2911.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int MinimumChangesToMakeKSemipalin(int[] nums) {
+        int l = 0, r = nums.Length - 1, res = 0;
+        while (l < r) {
+            res = Math.Max(res, Math.Min(nums[l], nums[r]) * (r - l));
+            if (nums[l] < nums[r]) l++; else r--;
+        }
+        return res;
+    }
+}
