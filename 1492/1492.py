1abae3d170cbc44b27101e619189736a36e87396
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-17 20:23:16 -0800

    1492

---
+++ 1492/1492.py
@@ -0,0 +1,6 @@
+class Solution:
+    def the_kth_factor_of_n(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
