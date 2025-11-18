ead2377182783249c084607f07bc410d00196560
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-18 14:46:15 -0800

    255

---
+++ 0255/255.py
@@ -0,0 +1,8 @@
+class Solution:
+    def verify_preorder_sequence_in_binary_searc(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
