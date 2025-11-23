d771e1807988af3981c47b12fbb5c728251b30fc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-22 18:00:40 -0800

    2732

---
+++ 2732/2732.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find_a_good_subset_of_the_matrix(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
