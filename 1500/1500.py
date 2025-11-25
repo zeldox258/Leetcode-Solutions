b53bcd7df9c6e0baa3f44c45f0d80c50bd45c088
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-25 15:47:45 -0800

    1500

---
+++ 1500/1500.py
@@ -0,0 +1,8 @@
+class Solution:
+    def design_a_file_sharing_system(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
