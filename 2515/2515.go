4d60c39d0f9a65cb43cbdc82ef0345c2ca008aaa
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-25 10:48:32 -0800

    2515

---
+++ 2515/2515.go
@@ -0,0 +1,22 @@
+func shortestDistanceToTargetString(n int, edges [][]int) int {
+	parent := make([]int, n)
+	for i := range parent {
+		parent[i] = i
+	}
+	var find func(int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+	comp := n
+	for _, e := range edges {
+		a, b := find(e[0]), find(e[1])
+		if a != b {
+			parent[a] = b
+			comp--
+		}
+	}
+	return comp
+}
