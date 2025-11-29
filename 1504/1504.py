30fad2d06bc54e2940746c1be9c75596fd9e0345
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-29 14:28:50 -0800

    1504

---
+++ 1504/1504.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_submatrices_with_all_ones(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
