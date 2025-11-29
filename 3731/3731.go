3bedb7c1768cfcc0f1e01cd47f34a71c7c03af18
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-28 17:57:40 -0800

    3731

---
+++ 3731/3731.go
@@ -0,0 +1,16 @@
+func findMissingElements(nums []int) []int {
+	n := len(nums)
+	res := make([]int, n)
+	for i := range res {
+		res[i] = -1
+	}
+	st := []int{}
+	for i, x := range nums {
+		for len(st) > 0 && nums[st[len(st)-1]] < x {
+			res[st[len(st)-1]] = x
+			st = st[:len(st)-1]
+		}
+		st = append(st, i)
+	}
+	return res
+}
