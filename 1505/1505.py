3fe119f52e023aa6ab3f4058f25b7a624771e2f3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-11-30 17:33:13 -0800

    1505

---
+++ 1505/1505.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimum_possible_integer_after_at_most_k(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
