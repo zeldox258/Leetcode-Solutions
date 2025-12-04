2398a0a32773d0da3820d717968876b948a537f8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-03 20:28:35 -0800

    270

---
+++ 0270/270.py
@@ -0,0 +1,8 @@
+class Solution:
+    def closest_binary_search_tree_value(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
