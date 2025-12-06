8e72d8a3f87707ed4ff51d7ad88c89e6fbdb0686
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-05 16:56:39 -0800

    2745

---
+++ 2745/2745.py
@@ -0,0 +1,8 @@
+class Solution:
+    def construct_the_longest_new_string(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
