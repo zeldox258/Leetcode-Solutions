71a9575b6a9dafa06e0a7bf2f4c19e716579199f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-10 15:05:57 -0800

    277

---
+++ 0277/277.py
@@ -0,0 +1,6 @@
+class Solution:
+    def find_the_celebrity(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
