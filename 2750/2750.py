6c08b17273a2caace152a75eb91a1f3029960092
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-10 20:23:40 -0800

    2750

---
+++ 2750/2750.py
@@ -0,0 +1,10 @@
+class Solution:
+    def ways_to_split_array_into_good_subarrays(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
