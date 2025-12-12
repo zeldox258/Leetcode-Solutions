f44f582d8c5a4eb79101681e6cfc76e0cf81c8c6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-12 10:36:58 -0800

    2752

---
+++ 2752/2752.py
@@ -0,0 +1,6 @@
+class Solution:
+    def customers_with_maximum_number_of_transac(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
