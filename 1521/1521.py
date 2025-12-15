677c638438bfff8401275af852747b7a0a4ade21
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-15 13:16:46 -0800

    1521

---
+++ 1521/1521.py
@@ -0,0 +1,11 @@
+class Solution:
+    def find_a_value_of_a_mysterious_function_cl(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
