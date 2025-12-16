85936e608573739933179376b82ef3e6127e2152
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-16 12:00:00 -0800

    160

---
+++ 0160/160.java
@@ -0,0 +1,7 @@
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a=headA,b=headB;
+        while(a!=b){a=a==null?headB:a.next;b=b==null?headA:b.next;}
+        return a;
+    }
+}
