f3fc9567c41f99bf82b34e3a732c27cf13d4d198
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-17 18:05:01 -0800

    509

---
+++ 0509/509.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public IList<IList<int>> FibonacciNumber(int[] nums) {
+        var res = new List<IList<int>>();
+        Array.Sort(nums);
+        Bt(nums, 0, new List<int>(), res);
+        return res;
+    }
+    void Bt(int[] nums, int s, List<int> path, List<IList<int>> res) {
+        res.Add(new List<int>(path));
+        for (int i = s; i < nums.Length; i++) {
+            if (i > s && nums[i] == nums[i-1]) continue;
+            path.Add(nums[i]); Bt(nums, i+1, path, res); path.RemoveAt(path.Count-1);
+        }
+    }
+}
