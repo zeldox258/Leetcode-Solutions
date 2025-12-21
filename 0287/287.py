c21925c66b4854eadbf460d64af101e6cacfc262
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-20 17:52:30 -0800

    287

---
+++ 0287/287.py
@@ -0,0 +1,12 @@
+class Solution:
+    def find_the_duplicate_number(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
