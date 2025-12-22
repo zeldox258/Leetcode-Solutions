e4f9387786888d89ca4451902900636e1aed083a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-22 12:00:00 -0800

    154

---
+++ 0154/154.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l=0,r=nums.length-1;
+        while(l<r){int m=(l+r)/2;if(nums[m]>nums[r])l=m+1;else if(nums[m]<nums[r])r=m;else r--;}
+        return nums[l];
+    }
+}
