6f672abdba8d8a75ab3bf2792562bfc022d50d19
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-24 22:56:13 -0800

    2764

---
+++ 2764/2764.py
@@ -0,0 +1,7 @@
+class Solution:
+    def is_array_a_preorder_of_some_binary_tree(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
