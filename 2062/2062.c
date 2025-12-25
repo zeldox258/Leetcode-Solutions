72098204eaf5a835b0ce1a6839a3b6a97a51d92e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-24 16:27:13 -0800

    2062

---
+++ 2062/2062.c
@@ -0,0 +1,11 @@
+struct ListNode* countVowelSubstringsOfAString(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* cur = head;
+    while (cur) {
+        struct ListNode* next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
