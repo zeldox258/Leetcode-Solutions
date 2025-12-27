269841714604e393e972abbe37bf38d0ce1fa2f1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-27 09:13:22 -0800

    2767

---
+++ 2767/2767.py
@@ -0,0 +1,6 @@
+class Solution:
+    def partition_string_into_minimum_beautiful_(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
