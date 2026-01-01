a4e45a62a9663126850ef8b236d3130c58e88f9a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2025-12-31 19:02:15 -0800

    1537

---
+++ 1537/1537.py
@@ -0,0 +1,6 @@
+class Solution:
+    def get_the_maximum_score(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
