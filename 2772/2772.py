8162bccdabfd7ef3e1649bb8dab0bb215682f361
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-01 10:19:35 -0800

    2772

---
+++ 2772/2772.py
@@ -0,0 +1,9 @@
+class Solution:
+    def apply_operations_to_make_all_array_eleme(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
