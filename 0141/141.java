073a7aad58ffd29147a38912c46a1f3872749741
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-04 12:00:00 -0800

    141

---
+++ 0141/141.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null&&fast.next!=null){slow=slow.next;fast=fast.next.next;if(slow==fast)return true;}
+        return false;
+    }
+}
