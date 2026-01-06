e3b7dca4ccf37dd163f07b5a0570be4e75886561
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-05 22:15:01 -0800

    108

---
+++ 0108/108.go
@@ -0,0 +1,11 @@
+func convertSortedArrayToBinarySear(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := convertSortedArrayToBinarySear(root.Left)
+	r := convertSortedArrayToBinarySear(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
