43fe2fb82bd3b3d1432d5bb19910fb39ebc9de49
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-06 18:10:26 -0800

    304

---
+++ 0304/304.py
@@ -0,0 +1,7 @@
+class Solution:
+    def range_sum_query_2d_immutable(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
