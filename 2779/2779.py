592ebc2943ef18822b43bb17c3f1833cee6d2170
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-08 14:01:19 -0800

    2779

---
+++ 2779/2779.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum_beauty_of_an_array_after_applyin(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
