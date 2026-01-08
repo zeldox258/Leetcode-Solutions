c79e64c5804bf66ad31faf66a0f30a497c0c3710
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-08 08:35:52 -0800

    1545

---
+++ 1545/1545.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_kth_bit_in_nth_binary_string(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
