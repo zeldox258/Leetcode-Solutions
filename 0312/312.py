142fa5630977cd205b87ca17c9998d9c1786a7d3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-14 11:03:51 -0800

    312

---
+++ 0312/312.py
@@ -0,0 +1,9 @@
+class Solution:
+    def burst_balloons(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
