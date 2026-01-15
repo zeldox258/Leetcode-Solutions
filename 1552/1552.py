ebd75358c363018a37d6a5fe8a23dc1dc4b7b4a8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-15 09:46:57 -0800

    1552

---
+++ 1552/1552.py
@@ -0,0 +1,6 @@
+class Solution:
+    def magnetic_force_between_two_balls(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
