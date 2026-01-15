63c1898f6e62712a5a5b08b3c8b9d472ed030992
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-14 20:51:38 -0800

    2785

---
+++ 2785/2785.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sort_vowels_in_a_string(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
