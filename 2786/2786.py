d3eb77a1cbb534725de71a9a68d2d998b662ee74
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-15 11:33:10 -0800

    2786

---
+++ 2786/2786.py
@@ -0,0 +1,8 @@
+class Solution:
+    def visit_array_positions_to_maximize_score(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
