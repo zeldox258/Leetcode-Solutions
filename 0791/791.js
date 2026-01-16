55b36f761a61153d411f6c0ad7d4628e2e34ac8b
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-15 16:31:20 -0800

    791

---
+++ 0791/791.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var customSortString = function(intervals) {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let count = 1, end = intervals[0][1];
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] >= end) {
+            count++;
+            end = intervals[i][1];
+        }
+    }
+    return count;
+};
