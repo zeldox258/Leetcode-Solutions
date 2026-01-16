80374aee3e21b572df4f26be457ea3b396a5bc87
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-16 11:51:45 -0800

    2787

---
+++ 2787/2787.py
@@ -0,0 +1,9 @@
+class Solution:
+    def ways_to_express_an_integer_as_sum_of_pow(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        count, end = 1, intervals[0][1]
+        for s, e in intervals[1:]:
+            if s >= end:
+                count += 1
+                end = e
+        return count
