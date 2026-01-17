776b08b50dc7ad00b15aef5322a56970ed036d5e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-17 10:18:14 -0800

    315

---
+++ 0315/315.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_of_smaller_numbers_after_self(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
