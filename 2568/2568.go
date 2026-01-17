4882d019ed30e89089fe8a6211948ecd03f3685e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-17 10:04:16 -0800

    2568

---
+++ 2568/2568.go
@@ -0,0 +1,11 @@
+func minimumImpossibleOr(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := minimumImpossibleOr(root.Left)
+	r := minimumImpossibleOr(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
