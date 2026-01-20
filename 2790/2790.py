fa971afcce234523fcf83ca90df54a1419b6b98f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-19 21:55:14 -0800

    2790

---
+++ 2790/2790.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum_number_of_groups_with_increasing(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
