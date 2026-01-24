cab1207df5dd19667d009c7d8447c2a793132e23
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-24 15:03:03 -0800

    2795

---
+++ 2795/2795.py
@@ -0,0 +1,10 @@
+class Solution:
+    def parallel_execution_of_promises_for_indiv(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
