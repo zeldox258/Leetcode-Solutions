55ce8e020122af0db6e5f7ef8d497d855682a911
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-25 12:00:00 -0800

    119

---
+++ 0119/119.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row=new ArrayList<>();
+        row.add(1);
+        for(int i=1;i<=rowIndex;i++){
+            for(int j=i-1;j>=1;j--) row.set(j,row.get(j)+row.get(j-1));
+            row.add(1);
+        }
+        return row;
+    }
+}
