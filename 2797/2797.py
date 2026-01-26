54bc8c6377ddab627a0d330bc0810996e0ad7ba8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-26 10:12:24 -0800

    2797

---
+++ 2797/2797.py
@@ -0,0 +1,6 @@
+class Solution:
+    def partial_function_with_placeholders(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
