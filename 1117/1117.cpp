b81bc7f49aa8257ee14f5298142df3a2eeaf54df
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-27 20:02:17 -0800

    1117

---
+++ 1117/1117.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* buildingH2o(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* cur=head;
+        while(cur){
+            ListNode* next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next;
+        }
+        return prev;
+    }
+};
