55933326046004d056088f04423e159b805ecd43
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-28 12:00:00 -0800

    115

---
+++ 0115/115.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m=s.length(),n=t.length();
+        long[][] dp=new long[m+1][n+1];
+        for(int i=0;i<=m;i++) dp[i][0]=1;
+        for(int i=1;i<=m;i++) for(int j=1;j<=n;j++){
+            dp[i][j]=dp[i-1][j];
+            if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]+=dp[i-1][j-1];
+        }
+        return (int)dp[m][n];
+    }
+}
