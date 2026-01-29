7a2934b40d76c559e567079f96342e77de6286bc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-28 18:11:58 -0800

    1565

---
+++ 1565/1565.py
@@ -0,0 +1,10 @@
+class Solution:
+    def unique_orders_and_customers_per_month(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
