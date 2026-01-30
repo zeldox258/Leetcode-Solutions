dcb19ef271fd6492d875da0c3146d37047eab134
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-29 18:21:30 -0800

    1566

---
+++ 1566/1566.py
@@ -0,0 +1,11 @@
+class Solution:
+    def detect_pattern_of_length_m_repeated_k_or(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
