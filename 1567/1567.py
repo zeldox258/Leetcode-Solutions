2b3b6f47a7a243a92814daee175b0d9525bbf3dc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-30 19:10:35 -0800

    1567

---
+++ 1567/1567.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_length_of_subarray_with_positive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
