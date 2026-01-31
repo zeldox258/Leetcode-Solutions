63fd863ab06a52e79048886695d2999101b2a6a1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-31 12:00:00 -0800

    112

---
+++ 0112/112.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        if(root.left==null&&root.right==null) return root.val==targetSum;
+        return hasPathSum(root.left,targetSum-root.val)||hasPathSum(root.right,targetSum-root.val);
+    }
+}
