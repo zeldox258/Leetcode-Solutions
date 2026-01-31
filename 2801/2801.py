3c6157d44010955e9fdeff304c1205161d6f4f78
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-30 22:32:03 -0800

    2801

---
+++ 2801/2801.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_stepping_numbers_in_range(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
