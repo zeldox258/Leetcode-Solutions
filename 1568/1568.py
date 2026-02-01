dd88e3ad25936671353ebd0f531a3bdf78415694
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-01-31 17:45:58 -0800

    1568

---
+++ 1568/1568.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimum_number_of_days_to_disconnect_isl(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
