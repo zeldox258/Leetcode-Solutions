c6f78b7c19c28576962ad73b3742a150bdc1962d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-05 09:42:56 -0800

    334

---
+++ 0334/334.py
@@ -0,0 +1,7 @@
+class Solution:
+    def increasing_triplet_subsequence(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
