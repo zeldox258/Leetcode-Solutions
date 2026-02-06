999b0bb960a5d509b763e2244054660f96dbda57
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-06 09:32:36 -0800

    1574

---
+++ 1574/1574.py
@@ -0,0 +1,14 @@
+class Solution:
+    def shortest_subarray_to_be_removed_to_make_(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def bt(s, path):
+            res.append(path[:])
+            for i in range(s, len(nums)):
+                if i > s and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                bt(i + 1, path)
+                path.pop()
+        bt(0, [])
+        return res
