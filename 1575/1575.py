18e43c7ccf3ac8272812b718dace2c68b71cc6d3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-07 09:27:42 -0800

    1575

---
+++ 1575/1575.py
@@ -0,0 +1,8 @@
+class Solution:
+    def count_all_possible_routes(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
