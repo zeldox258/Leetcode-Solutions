7107e703d9334e552767398424c8f23c4a05fae8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-07 12:20:38 -0800

    2809

---
+++ 2809/2809.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_time_to_make_array_sum_at_most_x(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
