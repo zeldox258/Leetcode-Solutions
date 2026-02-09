5bc80bdee7fbc939f0f82d96b8f34a72be04927d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-08 18:18:16 -0800

    337

---
+++ 0337/337.py
@@ -0,0 +1,6 @@
+class Solution:
+    def house_robber_iii(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
