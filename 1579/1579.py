3a0b6db8a98d6d5ebcfd78c0d30a8394f13327e6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-11 10:13:59 -0800

    1579

---
+++ 1579/1579.py
@@ -0,0 +1,7 @@
+class Solution:
+    def remove_max_number_of_edges_to_keep_graph(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
