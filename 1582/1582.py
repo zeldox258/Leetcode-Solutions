3c911327f7b00f8d202593f1278631e03a310455
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-14 20:44:40 -0800

    1582

---
+++ 1582/1582.py
@@ -0,0 +1,6 @@
+class Solution:
+    def special_positions_in_a_binary_matrix(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
