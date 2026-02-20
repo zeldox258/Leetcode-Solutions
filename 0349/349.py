322e65d07399a42a856436e3b8f6a77c6e5742e1
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-20 14:28:36 -0800

    349

---
+++ 0349/349.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intersection_of_two_arrays(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
