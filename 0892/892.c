b7c691bb62a9ce86859752b494c0bde11a17a21d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-20 18:39:54 -0800

    892

---
+++ 0892/892.c
@@ -0,0 +1,11 @@
+struct ListNode* surfaceAreaOf3dShapes(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* cur = head;
+    while (cur) {
+        struct ListNode* next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
