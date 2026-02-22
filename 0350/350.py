8717a8a706db3b1a9e9f5d631906e4978afe9a86
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-21 20:37:26 -0800

    350

---
+++ 0350/350.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intersection_of_two_arrays_ii(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
