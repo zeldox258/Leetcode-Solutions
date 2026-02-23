6c3c5a513148a92c944c020337f8fc0672428824
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-22 21:54:43 -0800

    1590

---
+++ 1590/1590.py
@@ -0,0 +1,8 @@
+class Solution:
+    def make_sum_divisible_by_p(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
