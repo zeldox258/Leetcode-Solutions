e7b2e11691b66ccfd32c2ce7df05ffc270124863
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-22 17:47:10 -0800

    2824

---
+++ 2824/2824.py
@@ -0,0 +1,7 @@
+class Solution:
+    def count_pairs_whose_sum_is_less_than_targe(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
