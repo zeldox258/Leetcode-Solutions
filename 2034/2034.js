0ebe4d8967e1c54842a14b608ad6384b8ceef2a9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-23 20:18:21 -0800

    2034

---
+++ 2034/2034.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var stockPriceFluctuation = function(n) {
+    let count = 0;
+    while (n) {
+        count += n & 1;
+        n >>>= 1;
+    }
+    return count;
+};
