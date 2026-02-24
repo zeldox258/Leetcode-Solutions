ab8832f5a2ec7e51c4e4964bd617543f806e5499
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-23 19:35:06 -0800

    2825

---
+++ 2825/2825.py
@@ -0,0 +1,10 @@
+class Solution:
+    def make_string_a_subsequence_using_cyclic_i(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
