0a8164c7d4f5585d4cb720d28fae1ecd9a60059d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-23 17:49:57 -0800

    352

---
+++ 0352/352.py
@@ -0,0 +1,6 @@
+class Solution:
+    def data_stream_as_disjoint_intervals(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
