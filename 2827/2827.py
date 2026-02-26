df5b70098756f33386625f1395d1f15addc3d913
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-25 17:56:45 -0800

    2827

---
+++ 2827/2827.py
@@ -0,0 +1,6 @@
+class Solution:
+    def number_of_beautiful_integers_in_the_rang(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
