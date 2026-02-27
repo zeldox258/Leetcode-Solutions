e3cae856159322f0febeb0ef762709739eaa35ed
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-26 17:00:38 -0800

    1746

---
+++ 1746/1746.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solve1746(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='['||c=='{') st.push(c);
+            else{
+                if(st.isEmpty()) return false;
+                char p=st.pop();
+                if(c==')'&&p!='('||c==']'&&p!='['||c=='}'&&p!='{') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
---
+++ 1746/README.md
@@ -0,0 +1,5 @@
+# 1746. Maximum Subarray Sum After One Operation
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/maximum-subarray-sum-after-one-operation/)
