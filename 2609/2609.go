ec386eef07082f58fe51878a052fa284c23c56c4
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-27 16:18:01 -0800

    2609

---
+++ 2609/2609.go
@@ -0,0 +1,18 @@
+func findTheLongestBalancedSubstrin(nums []int) [][]int {
+	res := [][]int{}
+	sort.Ints(nums)
+	var bt func(int, []int)
+	bt = func(s int, path []int) {
+		tmp := make([]int, len(path))
+		copy(tmp, path)
+		res = append(res, tmp)
+		for i := s; i < len(nums); i++ {
+			if i > s && nums[i] == nums[i-1] {
+				continue
+			}
+			bt(i+1, append(path, nums[i]))
+		}
+	}
+	bt(0, []int{})
+	return res
+}
