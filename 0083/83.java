0ad1b918528775ec46e9d572505ea25164cf61a0
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-02-28 12:00:00 -0800

    83

---
+++ 0083/83.java
@@ -0,0 +1,7 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur=head;
+        while(cur!=null&&cur.next!=null){if(cur.val==cur.next.val)cur.next=cur.next.next;else cur=cur.next;}
+        return head;
+    }
+}
