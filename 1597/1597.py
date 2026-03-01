5b23fdc940fcc088cc8fd829274b52419aedc8f6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-01 14:48:54 -0800

    1597

---
+++ 1597/1597.py
@@ -0,0 +1,6 @@
+class Solution:
+    def build_binary_expression_tree_from_infix_(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
