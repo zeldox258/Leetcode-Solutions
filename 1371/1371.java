3f4bddf46f150881b16a1046cb8637175262ac8c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-01 22:46:05 -0800

    1371

---
+++ 1371/1371.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solve1371(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        int count=1,end=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]>=end){count++;end=intervals[i][1];}
+        }
+        return count;
+    }
+}
