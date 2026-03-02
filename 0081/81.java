d367709b0618fee6a6bbce299fc4ca639401b9a2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-02 12:00:00 -0800

    81

---
+++ 0081/81.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(nums[m]==target) return true;
+            if(nums[l]==nums[m]) l++;
+            else if(nums[l]<nums[m]){
+                if(target>=nums[l]&&target<nums[m]) r=m-1; else l=m+1;
+            } else {
+                if(target>nums[m]&&target<=nums[r]) l=m+1; else r=m-1;
+            }
+        }
+        return false;
+    }
+}
