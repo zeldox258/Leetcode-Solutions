b8dba0885412ee9c1f3e695ea9bab1a7bde3a896
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-03 22:22:01 -0800

    2613

---
+++ 2613/2613.go
@@ -0,0 +1,11 @@
+func beautifulPairs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := beautifulPairs(root.Left)
+	r := beautifulPairs(root.Right)
+	if l > r {
+		return 1 + l
+	}
+	return 1 + r
+}
