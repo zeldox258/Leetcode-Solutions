13a273f8eb4daf01e04735f8a1f65aed2e4120fe
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-06 20:04:34 -0800

    2112

---
+++ 2112/2112.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solve2112(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int x:nums){
+            pq.add(x);
+            if(pq.size()>k) pq.poll();
+        }
+        return pq.peek();
+    }
+}
---
+++ 2112/README.md
@@ -0,0 +1,5 @@
+# 2112. The Airport With the Most Traffic
+
+**Difficulty:** Medium
+
+[LeetCode Link](https://leetcode.com/problems/the-airport-with-the-most-traffic/)
