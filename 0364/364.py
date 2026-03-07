8da745506f896225ebb4f74543ecfa1c0be51cf9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-07 12:55:51 -0800

    364

---
+++ 0364/364.py
@@ -0,0 +1,7 @@
+class Solution:
+    def nested_list_weight_sum_ii(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
