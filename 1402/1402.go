ba3d8e647b9fd17841c7fb478ec3a250513125d7
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-07 19:17:48 -0800

    1402

---
+++ 1402/1402.go
@@ -0,0 +1,11 @@
+func reducingDishes(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
