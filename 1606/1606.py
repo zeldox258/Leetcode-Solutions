066ef39f142e6d74f80a8b61057da1375618a8b6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-10 14:04:00 -0700

    1606

---
+++ 1606/1606.py
@@ -0,0 +1,10 @@
+class Solution:
+    def find_servers_that_handled_most_number_of(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, 0
+        while l < r:
+            res = max(res, min(nums[l], nums[r]) * (r - l))
+            if nums[l] < nums[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
