e26bf933ee5acb797ced82df883fa40f8fa82643
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-09 21:12:13 -0700

    1605

---
+++ 1605/1605.py
@@ -0,0 +1,8 @@
+class Solution:
+    def find_valid_matrix_given_row_and_column_s(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
+        return []
