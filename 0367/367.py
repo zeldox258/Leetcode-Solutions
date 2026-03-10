3790a9f6c33850d21d713c2366585ca119401363
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-10 16:10:28 -0700

    367

---
+++ 0367/367.py
@@ -0,0 +1,6 @@
+class Solution:
+    def valid_perfect_square(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
