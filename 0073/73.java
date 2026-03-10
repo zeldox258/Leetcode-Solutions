d688bb6be0746e6f12b035b198b1290e2848a106
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-10 12:00:00 -0700

    73

---
+++ 0073/73.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean fr=false,fc=false;
+        int m=matrix.length,n=matrix[0].length;
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++) if(matrix[i][j]==0){if(i==0)fr=true;if(j==0)fc=true;matrix[i][0]=0;matrix[0][j]=0;}
+        for(int i=1;i<m;i++) for(int j=1;j<n;j++) if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
+        if(fr) for(int j=0;j<n;j++) matrix[0][j]=0;
+        if(fc) for(int i=0;i<m;i++) matrix[i][0]=0;
+    }
+}
