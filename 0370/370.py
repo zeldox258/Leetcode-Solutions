ed621a1dfbe047fe14cdc5dae16f4ff2312b9e47
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-13 10:35:34 -0700

    370

---
+++ 0370/370.py
@@ -0,0 +1,15 @@
+class Solution:
+    def range_addition(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        comp = n
+        for a, b in edges:
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[ra] = rb
+                comp -= 1
+        return comp
