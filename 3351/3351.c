ffc3cb88032889c77fc32659ecb0853a5b201d1d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-13 18:39:36 -0700

    3351

---
+++ 3351/3351.c
@@ -0,0 +1,10 @@
+int sumOfGoodSubsequences(char* s) {
+    int freq[128] = {0};
+    int l = 0, res = 0, len = strlen(s);
+    for (int r = 0; r < len; r++) {
+        while (freq[(int)s[r]]) freq[(int)s[l++]]--;
+        freq[(int)s[r]]++;
+        if (r - l + 1 > res) res = r - l + 1;
+    }
+    return res;
+}
