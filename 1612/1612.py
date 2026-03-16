4fcf55d2f48cc127c353c78e18eead9ec1c85e70
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-16 09:15:41 -0700

    1612

---
+++ 1612/1612.py
@@ -0,0 +1,6 @@
+class Solution:
+    def check_if_two_expression_trees_are_equiva(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
