c00aed4cbf30b58b68588eb365935954563bd0da
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-16 12:00:00 -0700

    67

---
+++ 0067/67.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb=new StringBuilder();
+        int i=a.length()-1,j=b.length()-1,carry=0;
+        while(i>=0||j>=0||carry>0){
+            int sum=carry;
+            if(i>=0)sum+=a.charAt(i--)-'0';
+            if(j>=0)sum+=b.charAt(j--)-'0';
+            sb.append(sum%2);carry=sum/2;
+        }
+        return sb.reverse().toString();
+    }
+}
