c49b12c43dcfbc35a042333623eca425dcd81b7c
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-19 16:47:28 -0700

    3357

---
+++ 3357/3357.c
@@ -0,0 +1,7 @@
+int cmp(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+int minimizeTheMaximumAdjacentElem(int* nums, int numsSize, int k) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    return nums[numsSize - k];
+}
