b491b4e2dbe591a0ec855b157601b6320a4a572f
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-25 12:00:00 -0700

    58

---
+++ 0058/58.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();int len=0;
+        for(int i=s.length()-1;i>=0;i--){if(s.charAt(i)==' ')break;len++;}
+        return len;
+    }
+}
