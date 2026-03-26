ff045437ab9a012c5ecfbbff42accdaaf3f41c37
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-25 18:51:41 -0700

    382

---
+++ 0382/382.py
@@ -0,0 +1,6 @@
+class Solution:
+    def linked_list_random_node(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
