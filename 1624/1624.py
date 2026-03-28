f4c69d1f222dcfbadf6f7e6f8cea0c9e0dd39dc9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-28 16:12:56 -0700

    1624

---
+++ 1624/1624.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largest_substring_between_two_equal_char(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
