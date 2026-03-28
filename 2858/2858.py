adb4f0b64e56615a4b2fd2b449d2f9f439e4c609
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-28 11:55:57 -0700

    2858

---
+++ 2858/2858.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimum_edge_reversals_so_every_node_is_(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        s = count = 0
+        for x in nums:
+            s += x
+            count += d.get(s - k, 0)
+            d[s] = d.get(s, 0) + 1
+        return count
