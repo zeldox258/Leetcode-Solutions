e82bc7351ca6b98e3af3f1dae651785e6767d415
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-27 19:10:53 -0700

    2857

---
+++ 2857/2857.py
@@ -0,0 +1,6 @@
+class Solution:
+    def count_pairs_of_points_with_distance_k(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
