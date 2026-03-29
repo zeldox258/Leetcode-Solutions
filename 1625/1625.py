51cc3debd341398c239b8aa88793692837373133
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-29 08:37:19 -0700

    1625

---
+++ 1625/1625.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lexicographically_smallest_string_after_(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
