ccfe6f668b37615f959a1b22145a7ef5e539c08e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-03-31 14:16:44 -0700

    1627

---
+++ 1627/1627.py
@@ -0,0 +1,6 @@
+class Solution:
+    def graph_connectivity_with_threshold(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
