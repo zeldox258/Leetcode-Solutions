d2cf1a7ad8c126226587e44e4ca93b42b4100e32
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-06 12:00:00 -0700

    24

---
+++ 0024/24.java
@@ -0,0 +1,9 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+}
