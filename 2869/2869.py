565db71b52d3f5fd2a136545e1b9f98ae2ee262e
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-08 18:44:37 -0700

    2869

---
+++ 2869/2869.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimum_operations_to_collect_elements(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
