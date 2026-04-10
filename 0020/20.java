a43fd8f999f2cdfb3584e7fb9822a21522080fbd
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-10 12:00:00 -0700

    20

---
+++ 0020/20.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c=='(' || c=='{' || c=='[') stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (c==')' && top!='(') return false;
+                if (c=='}' && top!='{') return false;
+                if (c==']' && top!='[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
