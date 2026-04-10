f51db883256eed6394efd7dc6193da829120a033
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-10 12:00:00 -0700

    41

---
+++ 0041/41.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++) while(nums[i]>0&&nums[i]<=n&&nums[nums[i]-1]!=nums[i]){int t=nums[nums[i]-1];nums[nums[i]-1]=nums[i];nums[i]=t;}
+        for(int i=0;i<n;i++) if(nums[i]!=i+1) return i+1;
+        return n+1;
+    }
+}
