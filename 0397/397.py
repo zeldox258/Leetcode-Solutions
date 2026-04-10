eed87e970b2ef98c88b87cc5f637003da46e7d16
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-09 18:43:21 -0700

    397

---
+++ 0397/397.py
@@ -0,0 +1,6 @@
+class Solution:
+    def integer_replacement(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
