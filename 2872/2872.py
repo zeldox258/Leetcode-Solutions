487f74dc9fc7fca5551e716e0d1c6e1ddd44f2b6
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-11 22:32:11 -0700

    2872

---
+++ 2872/2872.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_number_of_kdivisible_components(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
