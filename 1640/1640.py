f55df5e4a36c1b3f27d085aa9f96e4bfcafe7690
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-12 22:53:17 -0700

    1640

---
+++ 1640/1640.py
@@ -0,0 +1,10 @@
+class Solution:
+    def check_array_formation_through_concatenat(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
