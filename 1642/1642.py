c55531f91e4dd851702a4d502aada5fe0d6d35f9
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-14 13:40:27 -0700

    1642

---
+++ 1642/1642.py
@@ -0,0 +1,6 @@
+class Solution:
+    def furthest_building_you_can_reach(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
