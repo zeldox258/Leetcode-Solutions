55fb9202464c228394aeeb50eedda40354379f3a
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 09:24:38 -0700

    3727

---
+++ 3727/3727.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_alternating_sum_of_squares(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
