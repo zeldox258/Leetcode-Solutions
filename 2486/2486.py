eb5c6c9e1f71cee5309398e05431730abbe2e0fc
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 14:07:57 -0700

    2486

---
+++ 2486/2486.py
@@ -0,0 +1,8 @@
+class Solution:
+    def append_characters_to_string_to_make_subs(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
