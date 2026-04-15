4f8c353dbc889ace36b477c8681169fb52f7e0b8
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 13:23:09 -0700

    1252

---
+++ 1252/1252.py
@@ -0,0 +1,6 @@
+class Solution:
+    def cells_with_odd_values_in_a_matrix(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
