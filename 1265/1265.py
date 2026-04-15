2012bb5da460ae5c0c7dcb80d6ca601824bddc8d
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 11:51:14 -0700

    1265

---
+++ 1265/1265.py
@@ -0,0 +1,10 @@
+class Solution:
+    def print_immutable_linked_list_in_reverse(self, s: str) -> bool:
+        st = []
+        pairs = {'(': ')', '[': ']', '{': '}' }
+        for c in s:
+            if c in pairs:
+                st.append(c)
+            elif not st or pairs[st.pop()] != c:
+                return False
+        return not st
