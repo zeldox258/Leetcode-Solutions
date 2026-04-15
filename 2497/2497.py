4567c660f443175b41b05f84635d0073376677a2
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 11:03:45 -0700

    2497

---
+++ 2497/2497.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximum_star_sum_of_a_graph(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
