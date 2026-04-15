0793f461f86508dfa978f7b9b258c59d4a085a50
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 11:00:48 -0700

    7

---
+++ 0007/7.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverse_integer(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
