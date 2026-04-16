ba1799b79f1acc8bed57a71fa21b49ee1d713af3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 18:48:23 -0700

    2494

---
+++ 2494/2494.py
@@ -0,0 +1,7 @@
+class Solution:
+    def merge_overlapping_events_in_the_same_hal(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
