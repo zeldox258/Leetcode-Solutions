7ea7d481552b98587e6e4278fdd5ad7c60167826
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 22:38:29 -0700

    4

---
+++ 0004/4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def median_of_two_sorted_arrays(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
