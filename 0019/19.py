854d6b839b30777b9454026bf61f7e8434838cb3
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 19:59:35 -0700

    19

---
+++ 0019/19.py
@@ -0,0 +1,7 @@
+class Solution:
+    def remove_nth_node_from_end_of_list(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
