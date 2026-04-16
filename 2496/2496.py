d3bf3254ee83388674e15452afafc6cc2df57bb5
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 22:55:13 -0700

    2496

---
+++ 2496/2496.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximum_value_of_a_string_in_an_array(self, s: str) -> int:
+        seen = set()
+        l = res = 0
+        for r, c in enumerate(s):
+            while c in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(c)
+            res = max(res, r - l + 1)
+        return res
