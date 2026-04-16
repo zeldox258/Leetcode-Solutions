54050e1f0ce16a059ed2e73e363d1d0498e7c026
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-15 17:55:31 -0700

    11

---
+++ 0011/11.py
@@ -0,0 +1,8 @@
+class Solution:
+    def container_with_most_water(self, nums: List[int]) -> List[int]:
+        res, st = [-1] * len(nums), []
+        for i, x in enumerate(nums):
+            while st and nums[st[-1]] < x:
+                res[st.pop()] = x
+            st.append(i)
+        return res
