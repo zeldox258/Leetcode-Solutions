7d90b1d2488f2dfcc0623ca141b41509d1146525
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-16 22:52:48 -0700

    2479

---
+++ 2479/2479.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum_xor_of_two_nonoverlapping_subtre(self, nums: List[int]) -> int:
+        cur = mx = nums[0]
+        for x in nums[1:]:
+            cur = max(x, cur + x)
+            mx = max(mx, cur)
+        return mx
