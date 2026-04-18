ed7fcbe65ed4bfb3ebaf862fa9f5ff8a5961a496
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-17 20:12:29 -0700

    22

---
+++ 0022/22.py
@@ -0,0 +1,6 @@
+class Solution:
+    def generate_parentheses(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
