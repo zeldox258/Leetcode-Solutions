9e399f9c15bcc1379c966e3f36ef43505eba0869
huseyinguray_microsoft <huseyinguray@microsoft.com>
2026-04-20 13:36:53 -0700

    1237

---
+++ 1237/1237.c
@@ -0,0 +1,11 @@
+struct ListNode* findPositiveIntegerSolutionFor(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* cur = head;
+    while (cur) {
+        struct ListNode* next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
